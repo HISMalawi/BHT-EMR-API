b4f73423b481d7f95b9800693ca5592af84b4d80
phundi <kennethkapundi1@gmail.com>
2018-08-23 11:17:05 +0200

    Added APIs for locations; districts, tas, villages, regions and foreign country

--- app/models/traditional_authority.rb
+++ app/models/traditional_authority.rb
@@ -1,7 +1,7 @@
 class TraditionalAuthority < ActiveRecord::Base
-    set_table_name  "traditional_authority"
-    set_primary_key "traditional_authority_id"
+    self.table_name  = "traditional_authority"
+    self.primary_key = "traditional_authority_id"
 
-	belongs_to :district
-
+	  belongs_to :district
+    has_many :villages, :foreign_key => :traditional_authority_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,18 @@ Rails.application.routes.draw do
 
     namespace :v1 do
 
-      post '/create_user/:token'						         		 =>	'user#create_user'
-  		get	 '/authenticate/:username/:password' 				 		 =>	'user#authenticate_user'
-  		get	 '/re_authenticate/:username/:password'						 =>	'user#re_authenticate'
-  		get	 '/check_token_validity/:token' 							 =>	'user#check_token_validity'
+      #User APIs
+      post '/create_user/:token'						       =>	'user#create_user'
+  		get	 '/authenticate/:username/:password' 		 =>	'user#authenticate_user'
+  		get	 '/re_authenticate/:username/:password'	 =>	'user#re_authenticate'
+  		get	 '/check_token_validity/:token' 				 =>	'user#check_token_validity'
+
+      #Location APIs
+      get '/get_regions/:token'                           => 'location#regions'
+      get '/get_facilities/:district_id/:token'           => 'location#facilities'
+      get '/get_districts/:region_id/:token'              => 'location#districts'
+      get '/get_tas/:district_id/:token'                  => 'location#tas'
+      get '/get_villages/:ta_id/:token'                   => 'location#villages'
 
     end
 
