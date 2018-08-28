2ecbbdd56ff92c0daeabc5ab5629933b3c187135
Walter Kaunda <walterkaunda@live.co.uk>
2018-08-28 04:00:14 +0200

    Fixed bug in Auditable behaviour.
    before_save callback was not checking whether object being saved has
    the auditable interface.

--- lib/auditable.rb
+++ lib/auditable.rb
@@ -20,11 +20,16 @@ module Auditable
 
   # Saves current user after every save
   def update_audit_trail
-    user = User.current
+    return unless auditable?
 
+    user = User.current
     Rails.logger.warn 'update_audit_trail called outside login' unless user
 
     self.changed_by = user ? user.id : nil
     self.date_changed = Time.now
+  end
+
+  def auditable?
+    respond_to?(:changed_by) && respond_to?(:date_changed)
   end
 end
