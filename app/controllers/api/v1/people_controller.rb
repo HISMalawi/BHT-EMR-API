811f27204bc0136ec87b5724d0830679f0200fac
Walter Kaunda <walterkaunda@live.co.uk>
2018-08-28 04:44:42 +0200

    Implemented person voiding.

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::PeopleController < ApplicationController
   extend PersonService
 
   def index
-    render json: Person.all
+    # TODO: Add pagination
+    render json: Person.all.limit(10)
   end
 
   def show
@@ -23,8 +24,8 @@ class Api::V1::PeopleController < ApplicationController
     create_params, errors = required_params required: PERSON_FIELDS
     return render json: create_params, status: :bad_request if errors
 
-    person = create_person(create_params[:gender], create_params[:birth_date],
-                           create_params[:birth_date_estimated])
+    person = create_person(create_params[:gender], create_params[:birthdate],
+                           create_params[:birthdate_estimated])
 
     create_person_name(person, given_name: create_params[:given_name],
                                family_name: create_params[:family_name],
@@ -37,14 +38,20 @@ class Api::V1::PeopleController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+    person = Person.find(params[:id])
+    if person.void
+      render status: :no_content
+    else
+      render json: { errors: "Failed to void person ##{person_id}" }
+    end
   end
 
   private
 
   PERSON_FIELDS = %i[
-    given_name middle_name family_name gender birth_date
-    birth_date_estimated home_district home_village
+    given_name middle_name family_name gender birthdate
+    birthdate_estimated home_district home_village
     home_traditional_authority current_district current_village
     current_traditional_authority
   ].freeze
