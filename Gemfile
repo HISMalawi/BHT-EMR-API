1f2605d4d6f4d31275b0ca5f1a6a1191455a07d7
Walter Kaunda <walterkaunda@live.co.uk>
2018-08-31 10:25:32 +0200

    Fixed DDEClient crash that was caused by expired session token.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # gem 'rack-cors'
 
 gem 'composite_primary_keys'
+gem 'rest-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- lib/dde_client.rb
+++ lib/dde_client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'logger'
-require 'net/http'
+require 'restclient'
 
 class DDEClient
   def initialize
@@ -17,41 +17,32 @@ class DDEClient
   def connect(config: nil, connection: nil)
     raise ArgumentError, 'config or connection required' unless config || connection
     @connection = reload_connection connection if connection
-    @connection = establish_connection config if config && !@connection
+    @connection = establish_connection config: config if config && !@connection
     @connection
   end
 
   def get(resource)
-    uri = build_uri resource
-    request = Net::HTTP::Get.new(uri)
-    prepare_request request
-    exec_request request, uri
+    exec_request resource do |url, headers|
+      RestClient.get url, headers
+    end
   end
 
   def post(resource, data)
-    uri = build_uri resource
-    LOGGER.debug uri
-    request = Net::HTTP::Post.new(uri)
-    prepare_request request
-    request['Content-type'] = JSON_CONTENT_TYPE
-    request.body = JSON.dump(data)
-    exec_request request, uri
+    exec_request resource do |url, headers|
+      RestClient.post url, data.to_json, headers
+    end
   end
 
   def put(resource, data)
-    uri = build_uri resource
-    request = Net::HTTP::Put.new(uri)
-    prepare_request request
-    request['Content-type'] = JSON_CONTENT_TYPE
-    request.body = JSON.dump(data)
-    exec_request request, uri
+    exec_request resource do |url, headers|
+      RestClient.put url, data.to_json, headers
+    end
   end
 
   def delete(resource)
-    uri = build_uri resource
-    request = Net::HTTP::Delete.new(uri)
-    prepare_request request
-    exec_request request, uri
+    exec_request resource do |url, headers|
+      RestClient.delete url, headers: headers
+    end
   end
 
   private
@@ -66,29 +57,28 @@ class DDEClient
     LOGGER.debug 'Loading DDE connection'
     if connection[:expires] < Time.now
       LOGGER.debug 'DDE connection expired'
-      establish_connection connection[:config]
+      establish_connection connection
     else
-      # HACK: Globally save base_url as a connection object may not currently
-      # be available to the expand_url method right now
       @base_url = connection[:config][:base_url]
       connection
     end
   end
 
-  # Establish a connection to DDE using config
+  # Establish a connection to DDE
   #
   # NOTE: This simply involves logging into DDE
-  def establish_connection(config)
+  def establish_connection(connection)
     LOGGER.debug 'Establishing new connection to DDE from configuration'
+    config = connection[:config]
 
     # Block any automatic logins when processing request to avoid infinite loop
     # in request execution below... Under normal circumstances request execution
-    # will attempt a login if 404 is met. Not pretty, I know but it does the job
+    # will attempt a login if 401 is met. Not pretty, I know but it does the job
     # for now!!!
     @auto_login = false
 
     # HACK: Globally save base_url as a connection object may not currently
-    # be available to the expand_url method right now
+    # be available to the build_url method right now
     @base_url = config[:base_url]
 
     response, status = post 'login', {
@@ -98,52 +88,41 @@ class DDEClient
 
     @auto_login = true
 
-    raise 'Unable to establish connection to DDE :(' if status != 200
+    raise "Unable to establish connection to DDE: #{response}" if status != 200
 
     LOGGER.info 'Connection to DDE established :)'
-
-    # Return our connection object...
-    {
-      key: response['access_token'],
-      expires: Time.now + DDE_API_KEY_VALIDITY_PERIOD,
-      config: config
-    }
+    connection[:key] = response['access_token']
+    connection[:expires] = Time.now + DDE_API_KEY_VALIDITY_PERIOD
+    connection
   end
 
   # Returns a URI object with API host attached
   def build_uri(resource)
-    URI("#{@base_url}/#{DDE_VERSION}/#{resource}")
+    "#{@base_url}/#{DDE_VERSION}/#{resource}"
   end
 
-  # This just sets the API key
-  def prepare_request(request)
-    authorization = @connection[:key] if @connection
-    request['Authorization'] = @connection[:key] if authorization
-    request
+  def headers
+    {
+      'Content-type' => JSON_CONTENT_TYPE,
+      'Authorization' => @connection ? @connection[:key] : nil
+    }
   end
 
-  def exec_request(request, uri, ignore_auto_login: false)
-    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      LOGGER.debug 'Making a request to DDE API @ ' + uri.to_s
-      http.request(request)
-    end
-
+  def exec_request(resource, &request)
+    LOGGER.debug "Executing DDE request using: #{@connection}"
+    response = yield build_uri(resource), headers
     LOGGER.debug "Handling DDE response:\n\tStatus - #{response.code}\n\tBody - #{response.body}"
-
-    status = response.code.to_i
-
-    if status == 401 && @auto_login && !ignore_auto_login
-      LOGGER.debug 'Auto-logging into DDE...'
-      @connection[:key] = nil
-      new_connection = establish_connection @connection[:config]
-      return { 'error' => 'Failed to login into DDE' }, 401 unless new_connection
-      @connection = new_connection
-      # Login successful, lets retry the last request and make sure we
-      # don't retry an auto-login
-      exec_request(request, uri, ignore_auto_login: true)
-    end
-
     handle_response response
+  rescue RestClient::Unauthorized => e
+    LOGGER.error "DDEClient suppressed exception: #{e}"
+    return handle_response e.response unless @auto_login
+    LOGGER.debug 'Auto-logging into DDE...'
+    establish_connection @connection
+    LOGGER.debug "Reset connection: #{@connection}"
+    retry # Retry last request...
+  rescue RestClient::BadRequest => e
+    LOGGER.error "DDEClient suppressed exception: #{e}"
+    handle_response e.response
   end
 
   def handle_response(response)
