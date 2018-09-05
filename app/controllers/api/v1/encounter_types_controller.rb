acc94918f41ce33a0e37b8ea9f16190add60a74c
Walter Kaunda <walterkaunda@live.co.uk>
2018-09-05 16:13:21 +0200

    Added encounter_type name search

--- app/controllers/api/v1/encounter_types_controller.rb
+++ app/controllers/api/v1/encounter_types_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::EncounterTypesController < ApplicationController
   def index
-    render json: paginate(EncounterType)
+    search_params, = required_params optional: %i[name]
+
+    if search_params.empty?
+      render json: paginate(EncounterType)
+    else
+      render json: paginate(EncounterType.where(search_params))
+    end
   end
 end
