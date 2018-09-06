1c467878de96443ba007c97d8b4f4002bf8a3119
Walter Kaunda <walterkaunda@live.co.uk>
2018-09-06 15:59:35 +0200

    Extended Auditable concern to automatically set creator and date_created.

--- app/models/concerns/auditable.rb
+++ app/models/concerns/auditable.rb
@@ -15,18 +15,29 @@ module Auditable
   extend ActiveSupport::Concern
 
   included do
-    before_save :update_audit_trail
+    before_save :update_change_trail
+    before_create :update_create_trail
   end
 
   # Saves current user after every save
-  def update_audit_trail
-    return unless auditable?
+  def update_change_trail
+    return unless respond_to?(:changed_by) && respond_to?(:date_changed)
 
     user = User.current
-    Rails.logger.warn 'update_audit_trail called outside login' unless user
+    Rails.logger.warn 'Auditable::update_change_trail called outside login' unless user
 
     self.changed_by = user ? user.id : nil
     self.date_changed = Time.now
+  end
+
+  def update_create_trail
+    return unless respond_to?(:date_created) && respond_to?(:creator)
+
+    user = User.current
+    Rails.logger.warn 'Auditable::update_create_trail called outside login' unless user
+
+    self.creator = user ? user.id : nil
+    self.date_created = Time.now
   end
 
   def auditable?
