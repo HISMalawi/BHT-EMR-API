2fd639859ddd4b32732d3d0b8ad0f29083d38958
Walter Kaunda <walterkaunda@live.co.uk>
2018-09-06 13:18:23 +0200

    Added people names search.
    Closes #10.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      get '/people/_names' => 'person_names#index'
       resources :people
       resources :roles
       resources :patients
