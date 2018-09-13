b003d3c05b1d52c95aea81802e64465ba3a2ccbd
Walter Kaunda <walterkaunda@live.co.uk>
2018-09-13 11:51:35 +0200

    Removed dead code in obs controller.

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -1,5 +1,3 @@
-require 'utils/remappable_hash'
-
 class Api::V1::ObservationsController < ApplicationController
   # Retrieve specific observation
   #
@@ -42,10 +40,7 @@ class Api::V1::ObservationsController < ApplicationController
   #   person_id - Person (not patient?) the observation is for
   #   encounter_id - The encounter this observation belongs to
   #   concept_id - The observation's concept
-  #   value - This observations value
-  #   value_type - Type of value above which must be one of boolean, coded,
-  #                drug, datetime, numeric, modifier, text, complex.
-  #   value_coded_name_id* - Only required if value_type above is coded
+  #   value_* - This observations value
   #
   # Optional parameters
   #   order_id, comments
@@ -86,13 +81,7 @@ class Api::V1::ObservationsController < ApplicationController
   #   value_coded_name_id* - Only required if value_type above is coded
   #   order_id, comments
   def update
-    update_params, errors = required_params required: %i[
-      person_id concept_id encounter_id concept_id value value_type
-      order_id comments
-    ]
-    return render json: update_params, status: :bad_request if errors
-
-    return unless remap_value_to_typed_value update_params
+    update_params = params.permit! # FIX-ME: This is highly unsafe
 
     observation = Observation.find(params[:id])
     if observation.update(update_params)
@@ -113,44 +102,5 @@ class Api::V1::ObservationsController < ApplicationController
       error = { errors: "Failed to delete observation ##{params[:id]}" }
       render json: error, status: :internal_server_error
     end
-  end
-
-  private
-
-  # A map of value_types (see methods create and update) to their database
-  # fields and dependent fields.
-  # Map structure: value_type => [field_name, dependent_fields_list]
-  OBS_VALUE_MAP = {
-    'boolean' => [:value_boolean, []],
-    'coded' => [:value_coded, [:value_coded_name_id]],
-    'drug' => [:value_drug, []],
-    'datetime' => [:value_datetime, []],
-    'numeric' => [:value_numeric, []],
-    'modifier' => [:value_modifier, []],
-    'text' => [:value_text, []],
-    'complex' => [:value_complex, []]
-  }.freeze
-
-  def obs_typed_value_field(param_name)
-    OBS_VALUE_MAP.fetch param_name
-  end
-
-  # Remap plain `value` to typed `value_*`
-  def remap_value_to_typed_value(params)
-    typed_value, required_extras = OBS_VALUE_MAP[params[:value_type]]
-
-    params.remap_field! :value, typed_value # Remap `value` to typed name
-    params.delete :value_type # Remove value_type field
-
-    return true if required_extras.empty?
-
-    extra_params, errors = required_params required: required_extras
-    if errors
-      render json: extra_params, status: :bad_request
-      return false
-    end
-
-    params.merge! extra_params
-    true
   end
 end
