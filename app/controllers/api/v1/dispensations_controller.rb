a46fea5f3ed856590aa2155e1d087502dacd756c
Walter Kaunda <walterkaunda@live.co.uk>
2018-09-21 10:13:01 +0200

    Added dispensation search

--- app/controllers/api/v1/dispensations_controller.rb
+++ app/controllers/api/v1/dispensations_controller.rb
@@ -11,4 +11,22 @@ class Api::V1::DispensationsController < ApplicationController
       render json: obs_list, status: :created
     end
   end
+
+  def index
+    patient_id = params.require %i[patient_id]
+
+    if params[:date]
+      date = params[:date] ? Date.strptime(params[:date]) : Time.now
+      dispensing = EncounterService.recent_encounter encounter_type_name: 'Dispensing',
+                                                     patient_id: patient_id,
+                                                     date: date
+      obs = dispensing ? dispensing.observations : []
+    else
+      obs = Observation.where(patient_id: patient_id).order(date_created: :desc)
+    end
+
+    drug_orders = obs.map(&:drug_order).reject(&:nil?)
+
+    render json: drug_orders
+  end
 end
--- app/services/dispensation_service.rb
+++ app/services/dispensation_service.rb
@@ -4,16 +4,18 @@ module DispensationService
       obs_list = plain_dispensations.map do |dispensation|
         order_id = dispensation[:drug_order_id]
         quantity = dispensation[:quantity]
-        obs = dispense_drug order_id, quantity
+        date = dispensation[:date] ? Time.strptime(dispensation[:date]) : nil
+        obs = dispense_drug order_id, quantity, date: date
         unless obs.errors.empty?
-          return  ["Failed to dispense order ##{order_id}", obs.errors], true
+          return ["Failed to dispense order ##{order_id}", obs.errors], true
         end
         obs
       end
 
       [obs_list, false]
     end
-    def dispense_drug(order_id, quantity)
+    def dispense_drug(order_id, quantity, date: nil)
+      date ||= Time.now
       drug_order = DrugOrder.find(order_id)
       # NOTE: Some caching below would be helpful
       patient = drug_order.order.patient
@@ -26,7 +28,7 @@ module DispensationService
         encounter_id: encounter.encounter_id,
         value_drug: drug_order.drug_inventory_id,
         value_numeric: quantity,
-        obs_datetime: Time.now # TODO: Prefer date passed by user
+        obs_datetime: date # TODO: Prefer date passed by user
       )
     end
 
