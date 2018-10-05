7bf7ac3fd8d2568227d5718a81ef2003dfc478f8
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-05 12:01:23 +0200

    Initial database setup bug fixes

--- bin/initial_database_setup.sh
+++ bin/initial_database_setup.sh
@@ -18,29 +18,28 @@ fi
 
 set -x # turns on stacktrace mode which gives useful debug information
 
+export RAILS_ENV=$ENV
+rails db:environment:set RAILS_ENV=$ENV
 
 #Creat Database
-bundle exec rake db:drop db:create db:schema:load
+bundle exec rake db:drop db:create # db:schema:load
 
 USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
 PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
 DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
 HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
 
-echo "DROP DATABASE $DATABASE;" | mysql --host=$HOST --user=$USERNAME --password=$PASSWORD
-echo "CREATE DATABASE $DATABASE;" | mysql --host=$HOST --user=$USERNAME --password=$PASSWORD
-
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/openmrs_1_7_2_concept_server_full_db.sql
 
-#echo "schema additions"
+echo "Schema additions ----------------------------------------------------------------------------------"
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/schema_bart2_additions.sql
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/bart2_views_schema_additions.sql
-#echo "defaults"
+mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/bart2_views_schema_additions.sql
+echo "Defaults ------------------------------------------------------------------------------------"
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/defaults.sql
-#echo "user schema modifications"
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/user_schema_modifications.sql
+echo "User schema modifications -------------------------------------------------------------------------"
+mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/user_schema_modifications.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/malawi_regions.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/mysql_functions.sql
+# mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/mysql_functions.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/drug_ingredient.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/pharmacy.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/national_id.sql
@@ -48,7 +47,7 @@ mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/${SITE}.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/tasks.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/moh_regimens_only.sql
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/regimen_indexes.sql
+mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/regimen_indexes.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/retrospective_station_entries.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/create_dde_server_connection.sql
 
@@ -65,11 +64,9 @@ mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_
 #rake openmrs:bootstrap:load:defaults RAILS_ENV=$ENV
 #rake openmrs:bootstrap:load:site SITE=$SITE RAILS_ENV=production#
 
-export RAILS_ENV=$ENV
+# bundle exec rake db:migrate
 
-bundle exec rake db:migrate
-
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/bart2_views_schema_additions.sql
+# mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/bart2_views_schema_additions.sql
 mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/revised_regimens.sql
 
 echo "After completing database setup, you are advised to run the following:"
