427931292ef5887dd0084ef196dcfc10bac1b72c
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-09 18:25:47 +0200

    Added missing relationship to patient states in program workflow state.

--- app/models/program_workflow_state.rb
+++ app/models/program_workflow_state.rb
@@ -6,6 +6,7 @@ class ProgramWorkflowState < RetirableRecord
 
   belongs_to :program_workflow
   belongs_to :concept
+  has_many :patient_states, foreign_key: :state
 
   # def self.find_state(state_id)
   #   self.find_by_sql(["SELECT * FROM `program_workflow_state` WHERE (`program_workflow_state`.`program_workflow_state_id` = ?)", state_id]).first
