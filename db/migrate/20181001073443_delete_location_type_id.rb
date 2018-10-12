771dafdecedd35b67d79d151e0df6e6b7b4f0175
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-12 13:03:19 +0200

    Updated migrations to run on MySQL 5.5

--- db/migrate/20181001073443_delete_location_type_id.rb
+++ db/migrate/20181001073443_delete_location_type_id.rb
@@ -1,6 +1,6 @@
 class DeleteLocationTypeId < ActiveRecord::Migration[5.2]
   def change
-    execute 'ALTER TABLE location MODIFY COLUMN date_created DATETIME NOT NULL DEFAULT NOW()'
+    execute 'ALTER TABLE location MODIFY COLUMN date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()'
     remove_column :location, :location_type_id
   end
 end
