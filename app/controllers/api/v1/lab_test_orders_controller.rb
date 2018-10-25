fa31e8f89d8f5426c078cc2b588ca60ac5359c93
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-25 11:29:29 +0200

    Added date parameter to POST /programs/:id/lab_tests/orders.

--- app/controllers/api/v1/lab_test_orders_controller.rb
+++ app/controllers/api/v1/lab_test_orders_controller.rb
@@ -4,9 +4,16 @@ class Api::V1::LabTestOrdersController < ApplicationController
   def create
     lab_test_type_id, encounter_id = params.require %i[test_type_id encounter_id]
 
-    type = LabTestType.find(lab_test_type_id)
-    encounter = Encounter.find(encounter_id)
-    order = engine.create_order type: type, encounter: encounter
+    begin
+      date = params[:date] ? params[:date].to_date : nil
+    rescue ArgumentError => e
+      error = "Failed to parse date(#{params[:date]}): #{e}"
+      return render json: { error: error }, status: :bad_request
+    end
+
+    type = LabTestType.find lab_test_type_id
+    encounter = Encounter.find encounter_id
+    order = engine.create_order type: type, encounter: encounter, date: date
 
     render json: order
   end
