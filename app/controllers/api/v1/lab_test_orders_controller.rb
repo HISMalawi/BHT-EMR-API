566321b1b29f12a53179bb09d15eb75eaca6c849
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-25 11:57:54 +0200

    Added stubs for retrieving lab orders for a patient.

--- app/controllers/api/v1/lab_test_orders_controller.rb
+++ app/controllers/api/v1/lab_test_orders_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Api::V1::LabTestOrdersController < ApplicationController
+  def index
+    if params[:accession_number]
+      render json: engine.find_orders_by_accession_number(params[:accession_number])
+    elsif params[:patient_id]
+      render json: engine.find_orders_by_patient_id(params[:patient_id])
+    else
+      render json: { errors: ['accession_number or patient_id required'] },
+             status: :bad_request
+    end
+  end
+
   def create
     lab_test_type_id, encounter_id = params.require %i[test_type_id encounter_id]
 
@@ -8,19 +19,20 @@ class Api::V1::LabTestOrdersController < ApplicationController
       date = params[:date] ? params[:date].to_date : nil
     rescue ArgumentError => e
       error = "Failed to parse date(#{params[:date]}): #{e}"
-      return render json: { error: error }, status: :bad_request
+      return render json: { errors: [error] }, status: :bad_request
     end
 
     type = LabTestType.find lab_test_type_id
     encounter = Encounter.find encounter_id
     order = engine.create_order type: type, encounter: encounter, date: date
 
-    render json: order
+    render json: order, status: :created
   end
 
   private
 
   def engine
-    ARTService::LabTestsEngine.new program: nil
+    # TODO: Access the engine from a factory not directly like this
+    @engine ||= ARTService::LabTestsEngine.new program: nil
   end
 end
