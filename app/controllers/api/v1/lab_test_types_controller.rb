0fdca8610bd85ace212c0a62eb89865d7ba0dbe4
Walter Kaunda <walterkaunda@live.co.uk>
2018-10-26 13:35:05 +0200

    Added automatic escaping of '_' with spaces on lab test types search.

--- app/controllers/api/v1/lab_test_types_controller.rb
+++ app/controllers/api/v1/lab_test_types_controller.rb
@@ -4,7 +4,19 @@ class Api::V1::LabTestTypesController < ApplicationController
   def index
     query = engine.types search_string: params[:search_string],
                          panel_id: params[:panel_id]
-    render json: paginate(query)
+
+    # RANT: The following should really be happening on the frontend
+    # when rendering. The backend should not be dictating presentation
+    # of the data on the frontend. Besides we are unneccessarily wasting
+    # cycles by doing it here as we have to loop through the `types` array
+    # twice, first here and secondly in the following `render` call.
+    types = paginate(query).collect do |type|
+      type = type.as_json
+      type['TestName'].gsub!(/_+/, ' ')
+      type
+    end
+
+    render json: types
   end
 
   def panels
