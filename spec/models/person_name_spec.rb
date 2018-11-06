27603f0b7a296a422833e12cb35b4efc07f416c2
Walter Kaunda <walterkaunda@live.co.uk>
2018-11-06 13:22:54 +0200

    Added person name validations for long validations.

--- spec/models/person_name_spec.rb
+++ spec/models/person_name_spec.rb
@@ -12,6 +12,14 @@ RSpec.describe PersonName, type: :model do
       expect(name.errors).to include :given_name
     end
 
+    it 'must not be more than 20 characters long' do
+      name = create :person_name
+      name.given_name = 'AVeryLongNameWithMoreThan20CharactersInIt'
+      expect(name.save).to be false
+      expect(name.errors.size).to eq 1
+      expect(name.errors).to include :given_name
+    end
+
     it 'can not be nil' do
       name = create :person_name
       name.given_name = nil
@@ -25,6 +33,14 @@ RSpec.describe PersonName, type: :model do
     it 'must not be less than 2 characters long' do
       name = create :person_name
       name.family_name = 'a'
+      expect(name.save).to be false
+      expect(name.errors.size).to eq 1
+      expect(name.errors).to include :family_name
+    end
+
+    it 'must not be more than 20 characters long' do
+      name = create :person_name
+      name.family_name = 'AVeryLongNameWithMoreThan20CharactersInIt'
       expect(name.save).to be false
       expect(name.errors.size).to eq 1
       expect(name.errors).to include :family_name
@@ -44,6 +60,22 @@ RSpec.describe PersonName, type: :model do
       name = create :person_name
       name.middle_name = nil
       expect(name.save).to be true
+    end
+
+    it 'must not be less than 2 characters long' do
+      name = create :person_name
+      name.middle_name = 'a'
+      expect(name.save).to be false
+      expect(name.errors.size).to eq 1
+      expect(name.errors).to include :middle_name
+    end
+
+    it 'must not be more than 20 characters long' do
+      name = create :person_name
+      name.middle_name = 'AVeryLongNameWithMoreThan20CharactersInIt'
+      expect(name.save).to be false
+      expect(name.errors.size).to eq 1
+      expect(name.errors).to include :middle_name
     end
   end
 end
