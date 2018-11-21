4e07c11961e5ef1b6c9796db207ad9fa54cf455e
kwalter <walterkaunda@live.co.uk>
2018-11-21 14:31:09 +0200

    Fixed bug in report serialisation.

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,5 +4,11 @@ class Report < RetirableRecord
   self.table_name = :reporting_report_design
 
   belongs_to :type, foreign_key: :report_definition_id, class_name: 'ReportType'
-  has_many :values, class_name: 'ReportValue'
+  has_many :values, class_name: 'ReportValue', foreign_key: :report_design_id
+
+  def as_json(options = {})
+    super(options.merge(
+      include: { type: {}, values: {} }
+    ))
+  end
 end
