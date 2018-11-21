78487608b60d5f8ba58ed28e03291884871a9ea6
kwalter <walterkaunda@live.co.uk>
2018-11-21 21:41:41 +0200

    Added a rudimentary locking mechanism for report generation.
    - A new report can not be queued when a report of the same type is on the queue.

--- app/services/report_service.rb
+++ app/services/report_service.rb
@@ -18,13 +18,39 @@ class ReportService
     LOGGER.debug "Retrieving report, #{name}, for period #{start_date} to #{end_date}"
     type = report_type(type)
 
-    report = find_report(type, name, start_date, end_date)
-    return report if report && !@overwrite_mode
+    report = @overwrite_mode ? nil : find_report(type, name, start_date, end_date)
+    return report if report
+
+    lock = self.class.acquire_report_lock(type.name, start_date, end_date)
+    return nil unless lock
 
     LOGGER.debug("#{name} report not found... Queueing one...")
     queue_report(name: name, type: type, start_date: start_date,
-                 end_date: end_date, **kwargs)
+                 end_date: end_date, lock: lock, **kwargs)
     nil
+  end
+
+  def self.acquire_report_lock(report_type_name, start_date, end_date)
+    path = lock_file_path(report_type_name, start_date, end_date)
+
+    if path.exist? && (File.stat(path).mtime + 12.hours) > Time.now
+      LOGGER.debug("Report is locked: #{path}")
+      return nil
+    end
+
+    File.open(path, 'w') do |fout|
+      fout << "Locked by #{User.current.username} @ #{Time.now}"
+    end
+
+    LOGGER.debug("Report lock file created: #{path}")
+    path
+  end
+
+  def self.release_report_lock(path)
+    path = Pathname.new(path)
+    return unless path.exist?
+
+    File.unlink(path)
   end
 
   private
@@ -53,11 +79,12 @@ class ReportService
           .first
   end
 
-  def queue_report(start_date:, end_date:, type:, **kwargs)
+  def queue_report(start_date:, end_date:, type:, lock:, **kwargs)
     kwargs[:start_date] = start_date.to_s
     kwargs[:end_date] = end_date.to_s
     kwargs[:type] = type.id
     kwargs[:user] = User.current.user_id
+    kwargs[:lock] = lock
 
     LOGGER.debug("Queueing #{type.name} report with arguments: #{kwargs}")
     if @immediate_mode
@@ -65,5 +92,9 @@ class ReportService
     else
       ReportJob.perform_later(engine(@program).to_s, **kwargs)
     end
+  end
+
+  def self.lock_file_path(report_type_name, start_date, end_date)
+    Rails.root.join('tmp', "#{report_type_name}-report-#{start_date}-to-#{end_date}.lock")
   end
 end
