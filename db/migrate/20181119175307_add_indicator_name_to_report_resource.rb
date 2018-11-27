bedd2e277156eedcc96e28f7ad542543ef2cc94a
kwalter <walterkaunda@live.co.uk>
2018-11-27 10:16:06 +0200

    Removed duplicate migrations

--- db/migrate/20181119175307_add_indicator_name_to_report_resource.rb
+++
@@ -1,6 +0,0 @@
-class AddIndicatorNameToReportResource < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reporting_report_design_resource, :indicator_name, :string, null: false
-    add_column :reporting_report_design_resource, :indicator_short_name, :string, null: true
-  end
-end
