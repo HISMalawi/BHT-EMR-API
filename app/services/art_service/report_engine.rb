bb3753b9ab7700543a600f012bf2b57ae8f8cfef
kwalter <walterkaunda@live.co.uk>
2018-11-27 13:55:29 +0200

    Updated ART report_engine interface to match new ReportService requirements.
    - Also registered Cohort disaggregated report

--- app/services/art_service/report_engine.rb
+++ app/services/art_service/report_engine.rb
@@ -7,14 +7,24 @@ module ARTService
     LOGGER = Rails.logger
 
     REPORTS = {
-      'COHORT' => ARTService::Reports::Cohort
+      'COHORT' => ARTService::Reports::Cohort,
+      'COHORT_DISAGGREGATED' => ARTService::Reports::CohortDisaggregated
     }.freeze
 
     def generate_report(type:, **kwargs)
-      LOGGER.debug("Generating report(#{kwargs})")
-      type = ReportType.find(type)
-      report_builder = REPORTS[type.name.upcase].new(type: type, **kwargs)
-      report_builder.build_report
+      call_report_manager(:build_report, type: type, **kwargs)
+    end
+
+    def find_report(type:, **kwargs)
+      call_report_manager(:find_report, type: type, **kwargs)
+    end
+
+    private
+
+    def call_report_manager(method, type:, **kwargs)
+      report_manager = REPORTS[type.name.upcase].new(type: type, **kwargs)
+      method = report_manager.method(method)
+      method.call
     end
   end
 end
