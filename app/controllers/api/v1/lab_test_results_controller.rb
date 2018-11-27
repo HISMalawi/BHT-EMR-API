c0c1345ed25628c955193a973b6eb003268fcd0d
kwalter <walterkaunda@live.co.uk>
2018-11-27 14:46:00 +0200

    Update lab test result controller to use result save method instead of create method.
    Done this as the result container (ie LabParameter) is now available immediately when
    the test is ordered.

--- app/controllers/api/v1/lab_test_results_controller.rb
+++ app/controllers/api/v1/lab_test_results_controller.rb
@@ -8,13 +8,10 @@ class Api::V1::LabTestResultsController < ApplicationController
   end
 
   def create
-    accession_number, test_type_id, test_value = params.require %i[
-      accession_number test_type_id test_value
-    ]
-    test_type = engine.type test_type_id
-    result = engine.create_result accession_number: accession_number,
-                                  test_type: test_type,
-                                  test_value: test_value
+    accession_number, test_value = params.require %i[accession_number test_value]
+
+    result = engine.save_result accession_number: accession_number,
+                                test_value: test_value
     render json: result, status: :created
   end
 end
