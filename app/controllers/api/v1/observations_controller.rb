12c1ecfdffec990518e61afbd6fcb34358125ea9
kwalter <walterkaunda@live.co.uk>
2018-12-06 14:46:11 +0200

    Added missing search filters to observations#index
    - Basically all value_* fields were excluded

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -23,7 +23,9 @@ class Api::V1::ObservationsController < ApplicationController
   # NOTE: When multiple parameters are specified they are
   #       AND-d together.
   def index
-    filters = params.permit(%i[person_id concept_id encounter_id order_id])
+    filters = params.permit(%i[person_id concept_id encounter_id order_id
+                               value_coded value_datetime value_numeric
+                               value_text])
 
     query = filters.empty? ? Observation : Observation.where(filters)
 
