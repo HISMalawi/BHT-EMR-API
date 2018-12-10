619280f92726112237b8682f5753cece728aa1f2
kwalter <walterkaunda@live.co.uk>
2018-12-10 15:54:45 +0200

    Added DrugController#show

--- app/controllers/api/v1/drugs_controller.rb
+++ app/controllers/api/v1/drugs_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::DrugsController < ApplicationController
+  def show
+    render json: Drug.find(params[:id])
+  end
+
   def index
     name = params.permit(:name)[:name]
     query = name ? Drug.where('name like ?', "%#{name}%") : Drug
