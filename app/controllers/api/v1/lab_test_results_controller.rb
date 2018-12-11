63a7a34a4d2e3f309fe1353e0736c5f1cdf326e2
kwalter <walterkaunda@live.co.uk>
2018-12-11 09:14:54 +0200

    Made specifying of time optional on lab result create

--- app/controllers/api/v1/lab_test_results_controller.rb
+++ app/controllers/api/v1/lab_test_results_controller.rb
@@ -8,11 +8,11 @@ class Api::V1::LabTestResultsController < ApplicationController
   end
 
   def create
-    accession_number, test_value = params.require %i[accession_number test_value time]
+    accession_number, test_value = params.require %i[accession_number test_value]
 
     result = engine.save_result accession_number: accession_number,
                                 test_value: test_value,
-                                time: time&.to_datetime || Time.now
+                                time: params[:time]&.to_datetime || Time.now
     render json: result, status: :created
   end
 end
