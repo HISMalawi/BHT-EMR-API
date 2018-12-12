4aaef34d8bfa71d8a67d7ed7553483b70ceeabaa
kwalter <walterkaunda@live.co.uk>
2018-12-12 02:30:30 +0200

    Bug fix iin patient_identifiers_controller

--- spec/controllers/api/v1/patient_identifiers_controller_spec.rb
+++ spec/controllers/api/v1/patient_identifiers_controller_spec.rb
@@ -23,7 +23,7 @@ require 'rails_helper'
 # removed from Rails core in Rails 5, but can be added back in via the
 # `rails-controller-testing` gem.
 
-RSpec.describe PatientIdentifiersController, type: :controller do
+RSpec.describe Api::V1::PatientIdentifiersController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # PatientIdentifier. As you add validations to PatientIdentifier, be sure to
