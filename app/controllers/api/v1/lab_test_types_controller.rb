9e1e1606c4f3032ed5db1269134df9932ab45f56
kwalter <walterkaunda@live.co.uk>
2018-12-24 15:42:47 +0200

    Refactored endpoint for lab test types to use lims

--- app/controllers/api/v1/lab_test_types_controller.rb
+++ app/controllers/api/v1/lab_test_types_controller.rb
@@ -5,20 +5,9 @@ class Api::V1::LabTestTypesController < ApplicationController
 
   def index
     query = engine.types search_string: params[:search_string],
-                         panel_id: params[:panel_id]
+                         specimen_type: params[:specimen_type]
 
-    # RANT: The following should really be happening on the frontend
-    # when rendering. The backend should not be dictating presentation
-    # of the data on the frontend. Besides we are unneccessarily wasting
-    # cycles by doing it here as we have to loop through the `types` array
-    # twice, first here and secondly in the following `render` call.
-    types = paginate(query).collect do |type|
-      type = type.as_json
-      type['TestName'].gsub!(/_+/, ' ')
-      type
-    end
-
-    render json: types
+    render json: query
   end
 
   def panels
