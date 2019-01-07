b9416d7e20196127c2eeb2fcfacd4f66e119a252
kwalter <walterkaunda@live.co.uk>
2019-01-07 17:26:38 +0200

    Fixed bug in  person#update
    - Parameters were not being extracted  from the posted parameters.

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -30,32 +30,32 @@ class Api::V1::PeopleController < ApplicationController
   end
 
   def create
-    create_params, errors = required_params required: PersonService::PERSON_FIELDS,
+    create_params, errors = required_params required: person_service::PERSON_FIELDS,
                                             optional: [:middle_name]
     return render json: create_params, status: :bad_request if errors
 
-    person = PersonService.create_person create_params
-    PersonService.create_person_name person, create_params
-    PersonService.create_person_address person, create_params
-    PersonService.create_person_attributes person, params.permit!
+    person = person_service.create_person create_params
+    person_service.create_person_name person, create_params
+    person_service.create_person_address person, create_params
+    person_service.create_person_attributes person, params.permit!
 
     render json: person, status: :created
   end
 
   def update
-    person = People.find(params[:id])
+    person = Person.find(params[:id])
     update_params = params.permit!
 
-    PersonService.update_person person, update_params
-    PersonService.update_person_name person, update_params
-    PersonService.update_person_address person, update_params
-    PersonService.update_person_attributes person, update_params
+    person_service.update_person person, update_params
+    person_service.update_person_name person, update_params
+    person_service.update_person_address person, update_params
+    person_service.update_person_attributes person, update_params
 
     # ASIDE: Person we just updated may be linked to DDE, if this is the
     # case, do we notify DDE of the update right now or do we force client
     # to trigger an update in DDE by calling POST /patient/:patient_id?
 
-    render json: person, status: :created
+    render json: person, status: :ok
   end
 
   def destroy
@@ -78,5 +78,9 @@ class Api::V1::PeopleController < ApplicationController
     PERSON_ATTRIBUTES.each_with_object({}) do |field, attrs|
       attrs[field] = params[field]
     end
+  end
+
+  def person_service
+    PersonService.new
   end
 end
