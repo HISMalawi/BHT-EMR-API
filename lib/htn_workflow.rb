cb2b6e64364d2c84212e4de62e954ab0189b4f1b
kwalter <walterkaunda@live.co.uk>
2019-01-18 16:50:42 +0200

    Bug in HTN user roles check

--- lib/htn_workflow.rb
+++ lib/htn_workflow.rb
@@ -136,7 +136,7 @@ class HtnWorkflow
   end
 
   def current_user_roles
-    user_roles = UserRole.where(["user_id = ?", current_user.id]).collect{|r|r.role}
+    user_roles = UserRole.where(["user_id = ?", User.current.id]).collect{|r|r.role}
     RoleRole.where(["child_role IN (?)", user_roles]).collect{|r|user_roles << r.parent_role}
     return user_roles.uniq
   end
