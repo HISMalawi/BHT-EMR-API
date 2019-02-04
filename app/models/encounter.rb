b363536b61906ac136134701597405d028826d8b
kwalter <walterkaunda@live.co.uk>
2019-02-04 09:38:49 +0200

    Added program_id to encounter

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -14,6 +14,7 @@ class Encounter < VoidableRecord
   belongs_to :provider, class_name: 'Person', foreign_key: :provider_id
   belongs_to :patient
   belongs_to :location, optional: true
+  belongs_to :program
 
   validates_presence_of :encounter_datetime
 
