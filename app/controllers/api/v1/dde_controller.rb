3d47a45851e3a835a790f3eac317b51a96d091a0
kwalter <walterkaunda@live.co.uk>
2019-02-04 14:06:46 +0200

    Added end point for searching patients by name and gender

---
+++ app/controllers/api/v1/dde_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::DdeController < ApplicationController
+  # GET /api/v1/dde/patients
+  def find_patients_by_npid
+    npid = params.require(:npid)
+    render json: service.find_patients_by_npid(npid)
+  end
+
+  def find_patients_by_name_and_gender
+    given_name, family_name, gender = params.require(%i[given_name family_name gender])
+    render json: service.find_patients_by_name_and_gender(given_name, family_name, gender)
+  end
+
+  # GET /api/v1/dde/match
+  #
+  # Returns DDE patients matching demographics passed
+  def matching_patients
+    render json: service.find_matching_patients(match_params)
+  end
+
+  private
+
+  MATCH_PARAMS = %i[given_name family_name gender birthdate home_village
+                    home_traditional_authority home_district].freeze
+
+  def match_params
+    MATCH_PARAMS.each_with_object({}) do |param, params_hash|
+      raise "param #{param} is required" if params[param].empty?
+
+      params_hash[param] = params[param]
+    end
+  end
+
+  def service
+    DDEService.new
+  end
+end
--- app/services/patient_service.rb
+++ app/services/patient_service.rb
@@ -43,6 +43,14 @@ class PatientService
     dde_service.find_patients_by_npid(npid)
   end
 
+  def find_patients_by_name_and_gender(given_name, family_name, gender)
+    Patient.joins(:person).merge(
+      Person.joins(:names).where('gender like ?', "#{gender}%").merge(
+        PersonName.where(given_name: given_name, family_name: family_name)
+      )
+    )
+  end
+
   def find_patient_median_weight_and_height(patient)
     median_weight_height(patient.age_in_months, patient.person.gender)
   end
