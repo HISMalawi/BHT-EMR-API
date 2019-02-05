50c1d3ea42ed19446f601851187671c3c845a6b0
kwalter <walterkaunda@live.co.uk>
2019-02-05 15:45:51 +0200

    Fixed bug in appointment service.
    - Appointment recommendation was limited to atmost 6 days before drug
      run out date. This was resulting in the application falling back to
      the drug run out date which in some cases can be a non-clinic day.

--- app/services/appointment_service.rb
+++ app/services/appointment_service.rb
@@ -71,29 +71,29 @@ class AppointmentService
 
   def booked_appointments(ref_date)
     ref_date ||= @retro_date
-    appointments_made(ref_date) 
+    appointments_made(ref_date)
   end
 
   private
 
   def appointments_made(date)
-    clients = ActiveRecord::Base.connection.select_all("SELECT 
-    i.identifier, p.birthdate, p.gender, n.given_name, 
+    clients = ActiveRecord::Base.connection.select_all("SELECT
+    i.identifier, p.birthdate, p.gender, n.given_name,
     n.family_name, obs.person_id, p.birthdate_estimated
-    FROM obs 
+    FROM obs
     INNER JOIN encounter e ON e.encounter_id = obs.encounter_id
-    AND e.voided = 0 AND obs.voided = 0 
+    AND e.voided = 0 AND obs.voided = 0
     AND e.encounter_type = #{encounter_type('APPOINTMENT').id}
     RIGHT JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
     RIGHT JOIN person_address a ON a.person_id = e.patient_id AND a.voided = 0
     RIGHT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
-    RIGHT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0 
+    RIGHT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
     AND i.identifier_type IN(2,3)
     WHERE obs.concept_id = #{concept('Appointment date').concept_id}
     AND value_datetime BETWEEN '#{date.strftime('%Y-%m-%d 00:00:00')}'
     AND '#{date.strftime('%Y-%m-%d 23:59:59')}'
-    GROUP BY i.identifier, p.birthdate, p.gender, 
-    n.given_name, n.family_name, 
+    GROUP BY i.identifier, p.birthdate, p.gender,
+    n.given_name, n.family_name,
     obs.person_id, p.birthdate_estimated;")
 
     clients_formatted = [];
@@ -228,7 +228,7 @@ class AppointmentService
     appointments = {}
     sdate = (end_date.to_date + 1.day)
 
-    1.upto(4).each do |num|
+    1.upto(10).each do |num|
       appointments[(sdate - num.day)] = 0
     end
 
