0906cabe27b16454ff0e355eaab8d419bc9997af
James Mwantisi <jamesmwantisi@gmail.com>
2019-02-08 13:52:49 +0200

    Lab Test Service Refactored

--- app/services/lab_test_service.rb
+++ app/services/lab_test_service.rb
@@ -9,7 +9,7 @@ class LabTestService
 
     def load_engine(program_id)
       program = Program.find program_id
-      engine = ENGINES[program.concept.concept_names[0].name.upcase]
+      engine = ENGINES[program.name.upcase]
       engine.new program: program
     end
   end
