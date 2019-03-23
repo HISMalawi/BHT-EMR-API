89de6d54e7444f975623b5b82fccc000e94443ef
kwalter <walterkaunda@live.co.uk>
2019-03-24 01:36:40 +0200

    Bug fix for lab test types

--- app/controllers/api/v1/lab_test_types_controller.rb
+++ app/controllers/api/v1/lab_test_types_controller.rb
@@ -10,8 +10,8 @@ class Api::V1::LabTestTypesController < ApplicationController
   end
 
   def panels
-    test_type = params.require %i[test_type]
-    response = engine.panels search_string: test_type
+    test_type = params.require(:test_type)
+    response = engine.panels test_type
     if response
       render json: response
     else
