77522349c8a8f3cea690f137bd3c466418818ef9
James Mwantisi <jamesmwantisi@gmail.com>
2019-03-26 10:36:49 +0200

    created ntp regimens seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,43 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# insert into drug (concept_id, name, combination, dose_strength, units, creator, uuid)
+# VALUES
+# (768, 'RHZ (R75/H50/Z150)', 1, 1, 'mg', 1, 'a'),
+# (1194, 'RH (R75/H50)', 1, 1, 'mg', 1, 'b'),
+# (1131, 'RHZE (R150/H75/Z400/E275)', 1, 1, 'mg', 1, 'c');
+
+#TB Drugs
+Drug.create(concept_id: 768, name: 'RHZ (R75/H50/Z150)', combination: 1, dosage_form: 4020, route: 3327, dose_strength: 1, units: 'mg', creator: 1, uuid: SecureRandom.uuid )
+Drug.create(concept_id: 1194, name: 'RH (R75/H50)', combination: 1, dosage_form: 4020, route: 3327, dose_strength: 1, units: 'mg', creator: 1, uuid: SecureRandom.uuid )
+Drug.create(concept_id: 1131, name: 'RHZE (R150/H75/Z400/E275)', combination: 1, dosage_form: 4020, route: 3327, dose_strength: 1, units: 'mg', creator: 1, uuid: SecureRandom.uuid )
+
+#Regimens: RHZE (R150/H75/Z400/E275) && RH (R150/H75)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZ (R75/H50/Z150)'), am_dose: 1, min_weight: 4, max_weight: 7, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZ (R75/H50/Z150)'), am_dose: 1, min_weight: 8, max_weight: 11, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZ (R75/H50/Z150)'), am_dose: 1, min_weight: 12, max_weight: 15, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZ (R75/H50/Z150)'), am_dose: 1, min_weight: 16, max_weight: 24, creator: 1)
+
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R75/H50)'), am_dose: 1, min_weight: 4, max_weight: 7, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R75/H50)'), am_dose: 1, min_weight: 8, max_weight: 11, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R75/H50)'), am_dose: 1, min_weight: 12, max_weight: 15, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R75/H50)'), am_dose: 1, min_weight: 16, max_weight: 24, creator: 1)
+
+NtpRegimen.create(drug: Drug.find_by(name: 'E (Ethambutol 100mg tablet)'), am_dose: 1, min_weight: 4, max_weight: 7, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'E (Ethambutol 100mg tablet)'), am_dose: 1, min_weight: 8, max_weight: 11, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'E (Ethambutol 100mg tablet)'), am_dose: 1, min_weight: 12, max_weight: 15, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'E (Ethambutol 100mg tablet)'), am_dose: 1, min_weight: 16, max_weight: 24, creator: 1)
+
+#Adult Regimens: RHZE (R150/H75/Z400/E275) && RH (R150/H75)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZE (R150/H75/Z400/E275)'), am_dose: 1, min_weight: 30, max_weight: 37, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZE (R150/H75/Z400/E275)'), am_dose: 1, min_weight: 38, max_weight: 54, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZE (R150/H75/Z400/E275)'), am_dose: 1, min_weight: 55, max_weight: 74, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RHZE (R150/H75/Z400/E275)'), am_dose: 1, min_weight: 75, max_weight: 1000, creator: 1)
+
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R150/H75)'), am_dose: 1, min_weight: 30, max_weight: 37, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R150/H75)'), am_dose: 1, min_weight: 38, max_weight: 54, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R150/H75)'), am_dose: 1, min_weight: 55, max_weight: 74, creator: 1)
+NtpRegimen.create(drug: Drug.find_by(name: 'RH (R150/H75)'), am_dose: 1, min_weight: 75, max_weight: 1000, creator: 1)
+
+
