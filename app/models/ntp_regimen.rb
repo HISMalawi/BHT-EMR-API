3ffce208c40ddc99a8e21bb4d66c7c35a5d9642e
James Mwantisi <jamesmwantisi@gmail.com>
2019-03-26 10:35:57 +0200

    Refactored NtpRegimen

--- app/models/ntp_regimen.rb
+++ app/models/ntp_regimen.rb
@@ -1,8 +1,8 @@
 class NtpRegimen < VoidableRecord
   self.table_name = 'ntp_regimens'
-  self.primary_key = 'regimen_id'
+  self.primary_key = 'id'
   #Drug Name, Units, Concept name
-  belongs_to :drug, foreign_key: :drug_inventory_id
+  belongs_to :drug, foreign_key: :drug_id
 
 
   def as_json(options = {})
