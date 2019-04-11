7aa84165c2814d58b62e474310435d18e7e24b1d
kwalter <walterkaunda@live.co.uk>
2019-04-11 16:57:51 +0200

    Started implementing rds sync

--- bin/rds_push.rb
+++ bin/rds_push.rb
@@ -2,11 +2,10 @@
 
 require 'logger'
 require 'rest-client'
-require 'securerandom'
 
-Rails.logger = Logger.new(STDOUT)
-ActiveRecord::Base.logger = Rails.logger
-RestClient.log = Rails.logger
+LOGGER = Logger.new(STDOUT)
+ActiveRecord::Base.logger = LOGGER
+RestClient.log = LOGGER
 
 APPLICATION_CONFIG_PATH = Rails.root.join('config/application.yml')
 DATABASE_CONFIG_PATH = Rails.root.join('config/database.yml').to_s
@@ -23,14 +22,12 @@ TIME_EPOCH = '1970-01-01'.to_time
 IMMUTABLE_MODELS = [PersonAddress, PatientIdentifier, Observation, Order].freeze
 
 def main
-  config # Load configuration early to ensure it's sanity before doing anything
-
   MODELS.each do |model|
-    Rails.logger.debug("Scanning model: #{model}")
+    LOGGER.debug("Scanning model: #{model}")
     last_update_time = delta(model)
 
     recent_records(model, last_update_time).each do |record|
-      Rails.logger.debug("#{last_update_time} - #{model}: #{record}")
+      LOGGER.debug("#{last_update_time} - #{model}: #{record}")
 
       update_time = record_update_time(record)
       last_update_time = update_time if update_time > last_update_time
@@ -42,10 +39,22 @@ def main
 
       save_record_sync_status(sync_status, record, record_doc_id)
     rescue StandardError => e
-      Rails.logger.error("Failed to write `#{model}` record: #{record} due to exception: #{e}")
+      LOGGER.error("Failed to write `#{model}` record: #{record} due to exception: #{e}")
     end
 
     save_delta(model, last_update_time)
+    initiate_couch_sync
+  end
+end
+
+# Attempts to execute passed block with a lock file
+def with_lock
+  File.open('/tmp/rds_push.lock', File::RDWR | File::CREAT) do |lock_file|
+    unless lock_file.flock(File::LOCK_EX | File::LOCK_NB)
+      raise 'Another instance is already is running'
+    end
+
+    yield
   end
 end
 
@@ -76,7 +85,7 @@ def save_delta(model, time)
 
   Dir.mkdir(DELTA_STATE_PATH.parent) unless DELTA_STATE_PATH.parent.exist?
 
-  Rails.logger.debug("Saving delta, #{time}, for model: #{model}")
+  LOGGER.debug("Saving delta, #{time}, for model: #{model}")
 
   File.open(DELTA_STATE_PATH, 'w') do |fin|
     fin.write(@delta.to_yaml)
@@ -153,7 +162,7 @@ end
 def push_record(record, doc_id = nil)
   record = serialize_record(record)
 
-  Rails.logger.debug("Pushing record (#{doc_id}) to couch db: #{record}")
+  LOGGER.debug("Pushing record (#{doc_id}) to couch db: #{record}")
 
   if doc_id
     push_existing_record(record, doc_id)
@@ -164,8 +173,32 @@ end
 
 # Convert record to JSON
 def serialize_record(record)
+  # HACK: Temporarily transform id on the record for the serialization
+  # process as we do not know what the id field actually maps to
+  # (eg id on Person maps to person_id and on PersonAttribute to person_attribute_type)
+  site_id = GlobalProperty.find_by_property('current_health_center_id').property_value
+  record_id = record.id
+  record.id = "#{record_id}00#{site_id}".to_i
+
   serialized_record = record.as_json(ignore_includes: true)
   serialized_record['record_type'] = record.class.to_s
+
+  record.id = record_id # Restore original id
+
+  if record.class == Encounter && record.program.nil?
+    # HACK: Apparently this script may be run on old applications
+    # that use the old openmrs standard that has no program
+    # specific encounters. Thus we manually have to set the program
+    # id using the value specified in the config file.
+    program_name = config['couchdb']['local']['program']
+    raise 'program_name not found in couch config: application.yml' unless program_name
+
+    program = Program.find_by_name(program_name)
+    raise 'Invalid program name in couch config: application.yml' unless program
+
+    serialized_record['program_id'] = program.id
+  end
+
   serialized_record.to_json
 end
 
@@ -174,30 +207,74 @@ end
 # @see push_record
 def push_new_record(record)
   handle_couch_response do
-    RestClient.post(couch_url, record, content_type: :json)
+    RestClient.post(local_couch_url, record, content_type: :json)
   end
 end
 
 def push_existing_record(record, doc_id)
   handle_couch_response do
-    RestClient.put("#{couch_url}/#{doc_id}", record, content_type: :json)
+    RestClient.put("#{local_couch_url}/#{doc_id}", record, content_type: :json)
   end
 end
 
 def create_couch_database
-  response = RestClient.put(couch_url, {})
-  Rails.logger.debug(response)
+  response = RestClient.put(local_couch_url, {})
+  LOGGER.debug(response)
 end
 
-def couch_url
-  protocol = config['couchdb']['protocol']
-  username = config['couchdb']['username']
-  password = config['couchdb']['password']
-  host = config['couchdb']['host']
-  port = config['couchdb']['port']
-  database = config['couchdb']['database']
+def local_couch_url
+  couch_config = config['couchdb']['local']
+  protocol = couch_config['protocol']
+  username = couch_config['username']
+  password = couch_config['password']
+  host = couch_config['host']
+  port = couch_config['port']
+  database = couch_config['database']
 
   "#{protocol}://#{username}:#{password}@#{host}:#{port}/#{database}"
+end
+
+def local_bare_couch_url
+  couch_config = config['couchdb']['local']
+  protocol = couch_config['protocol']
+  host = couch_config['host']
+  port = couch_config['port']
+  database = couch_config['database']
+
+  "#{protocol}://#{host}:#{port}/#{database}"
+end
+
+def master_couch_url
+  couch_config = config['couchdb']['master']
+  protocol = couch_config['protocol']
+  username = couch_config['username']
+  password = couch_config['password']
+  host = couch_config['host']
+  port = couch_config['port']
+  database = couch_config['database']
+
+  "#{protocol}://#{username}:#{password}@#{host}:#{port}/#{database}"
+end
+
+def master_bare_couch_url
+  couch_config = config['couchdb']['master']
+  protocol = couch_config['protocol']
+  host = couch_config['host']
+  port = couch_config['port']
+  database = couch_config['database']
+
+  "#{protocol}://#{host}:#{port}/#{database}"
+end
+
+def initiate_couch_sync
+  request = {
+    source: local_bare_couch_url,
+    master: master_bare_couch_url
+  }
+
+  RestClient.post("#{local_couch_url}/_replicate", request.to_json, content_type: :json, referer: 'http://127.0.0.1:5984')
+rescue StandardError => e
+  raise e.response.body.inspect
 end
 
 # Handle response from couch db
@@ -211,4 +288,8 @@ rescue RestClient::NotFound => e
   raise e
 end
 
-main
+with_lock do
+  config # Load configuration early to ensure it's sanity before doing anything
+
+  main
+end
