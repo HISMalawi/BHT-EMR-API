a33f23f7a0fc2c172f66b9cc4fb71f93ab097eaa
kwalter <walterkaunda@live.co.uk>
2019-05-06 14:53:56 +0200

    Added OPD LA report.

--- app/services/opd_service/report_engine.rb
+++ app/services/opd_service/report_engine.rb
@@ -5,7 +5,15 @@ module OPDService
   class ReportEngine
     include ModelUtils
 
+    REPORTS = {
+      'LA_PRESCRIPTIONS' => OPDService::Reports::LaPrescriptions
+    }
+
     def initialize
+    end
+
+    def find_report(type:, **kwargs)
+      report_engine(type).find_report(**kwargs)
     end
 
     # Retrieves the next encounter for bound patient
@@ -13,15 +21,15 @@ module OPDService
       @date = date.to_date
       stats = {}
       stats[:top] = {
-        registered_today: registered_today('New patient'), 
+        registered_today: registered_today('New patient'),
         returning_today: registered_today('Revisiting'),
-        referred_today: registered_today('Referral') 
+        referred_today: registered_today('Referral')
       }
 
       stats[:down] = {
-        registered: monthly_registration('New patient'), 
+        registered: monthly_registration('New patient'),
         returning: monthly_registration('Revisiting'),
-        referred: monthly_registration('Referral') 
+        referred: monthly_registration('Referral')
       }
 
       return stats
@@ -35,11 +43,11 @@ module OPDService
         RIGHT JOIN person_address a ON a.person_id = person.person_id
         RIGHT JOIN person_name n ON n.person_id = person.person_id').\
         select('person.*, a.state_province district, i.identifier nid,
-        a.township_division ta, a.city_village village, 
+        a.township_division ta, a.city_village village,
         n.given_name, n.family_name').order('n.date_created DESC')
-    
+
       stats = []
-      (data || []).each do |record| 
+      (data || []).each do |record|
         person = Person.find record['person_id']
         stats << {
           given_name: record['given_name'],
@@ -56,20 +64,20 @@ module OPDService
 
     def diagnosis_by_address(start_date, end_date)
       type = EncounterType.find_by_name 'Outpatient diagnosis'
-       
+
       data = Encounter.where('encounter_datetime BETWEEN ? AND ?
         AND encounter_type = ? AND value_coded IS NOT NULL
-        AND concept_id IN(6543, 6542)', 
-        start_date.to_date.strftime('%Y-%m-%d 00:00:00'), 
+        AND concept_id IN(6543, 6542)',
+        start_date.to_date.strftime('%Y-%m-%d 00:00:00'),
         end_date.to_date.strftime('%Y-%m-%d 23:59:59'),type.id).\
         joins('INNER JOIN obs ON obs.encounter_id = encounter.encounter_id
         INNER JOIN person p ON p.person_id = encounter.patient_id
         RIGHT JOIN person_address a ON a.person_id = encounter.patient_id').\
-        select('encounter.encounter_type, obs.value_coded, p.*, 
+        select('encounter.encounter_type, obs.value_coded, p.*,
         a.state_province district, a.township_division ta, a.city_village village')
-    
+
       stats = {}
-      
+
       (data || []).each do |record|
         concept = ConceptName.find_by_concept_id record['value_coded']
         district  = record['district']
@@ -96,7 +104,7 @@ module OPDService
 
       data = Encounter.where('encounter_datetime BETWEEN ? AND ?
         AND encounter_type = ? AND value_coded IS NOT NULL
-        AND obs.concept_id = ?', start_date.to_date.strftime('%Y-%m-%d 00:00:00'), 
+        AND obs.concept_id = ?', start_date.to_date.strftime('%Y-%m-%d 00:00:00'),
         end_date.to_date.strftime('%Y-%m-%d 23:59:59'),type.id, visit_type.concept_id).\
         joins('INNER JOIN obs ON obs.encounter_id = encounter.encounter_id
         INNER JOIN person p ON p.person_id = encounter.patient_id
@@ -107,7 +115,7 @@ module OPDService
         order('n.date_created DESC').group('n.person_id, encounter.encounter_id')
 
       stats = []
-      (data || []).each do |record| 
+      (data || []).each do |record|
         stats << {
           given_name: record['given_name'],
           family_name: record['family_name'],
@@ -123,22 +131,22 @@ module OPDService
 
     def diagnosis(start_date, end_date)
       type = EncounterType.find_by_name 'Outpatient diagnosis'
-       
+
       data = Encounter.where('encounter_datetime BETWEEN ? AND ?
         AND encounter_type = ? AND value_coded IS NOT NULL
-        AND concept_id IN(6543, 6542)', 
-        start_date.to_date.strftime('%Y-%m-%d 00:00:00'), 
+        AND concept_id IN(6543, 6542)',
+        start_date.to_date.strftime('%Y-%m-%d 00:00:00'),
         end_date.to_date.strftime('%Y-%m-%d 23:59:59'),type.id).\
         joins('INNER JOIN obs ON obs.encounter_id = encounter.encounter_id
         INNER JOIN person p ON p.person_id = encounter.patient_id').\
         select('encounter.encounter_type, obs.value_coded, p.*')
-      
+
       stats = {}
-      (data || []).each do |record| 
+      (data || []).each do |record|
         age_group = get_age_group(record['birthdate'], end_date)
         gender = record['gender']
         concept = ConceptName.find_by_concept_id record['value_coded']
-        
+
         next if gender.blank?
 
         if stats[concept.name].blank?
@@ -176,8 +184,8 @@ module OPDService
           stats[concept.name][:female_unknowns] += 1
         elsif age_group == 'Unknown' && gender == 'M'
           stats[concept.name][:male_unknowns] += 1
-        end  
-      
+        end
+
       end
 
       return stats
@@ -188,19 +196,19 @@ module OPDService
        visit_type = ConceptName.find_by_name 'Given drugs'
 
       data = Encounter.where('encounter_datetime BETWEEN ? AND ?
-        AND encounter_type = ? AND obs.concept_id = ?', 
-        start_date.to_date.strftime('%Y-%m-%d 00:00:00'), 
+        AND encounter_type = ? AND obs.concept_id = ?',
+        start_date.to_date.strftime('%Y-%m-%d 00:00:00'),
         end_date.to_date.strftime('%Y-%m-%d 23:59:59'),type.id, visit_type.concept_id).\
         joins('INNER JOIN obs ON obs.encounter_id = encounter.encounter_id
         INNER JOIN person p ON p.person_id = encounter.patient_id
         INNER JOIN drug d ON d.drug_id = obs.value_drug
         LEFT JOIN person_name n ON n.person_id = encounter.patient_id AND n.voided = 0').\
-        select('encounter.patient_id, obs.value_numeric, 
+        select('encounter.patient_id, obs.value_numeric,
         d.drug_id, obs.obs_datetime,n.given_name, n.family_name, p.*, d.name drug_name').\
         order('n.date_created DESC').group('n.person_id, encounter.encounter_id')
 
       stats = []
-      (data || []).each do |record| 
+      (data || []).each do |record|
         stats << {
           given_name: record['given_name'],
           family_name: record['family_name'],
@@ -220,8 +228,8 @@ module OPDService
       type = EncounterType.find_by_name 'TREATMENT'
 
       data = Encounter.where('encounter_datetime BETWEEN ? AND ?
-        AND encounter_type = ? AND i.quantity > 0', 
-        start_date.to_date.strftime('%Y-%m-%d 00:00:00'), 
+        AND encounter_type = ? AND i.quantity > 0',
+        start_date.to_date.strftime('%Y-%m-%d 00:00:00'),
         end_date.to_date.strftime('%Y-%m-%d 23:59:59'),type.id).\
         joins('INNER JOIN orders o ON o.encounter_id = encounter.encounter_id
         INNER JOIN person p ON p.person_id = encounter.patient_id
@@ -233,7 +241,7 @@ module OPDService
         order('n.date_created DESC').group('n.person_id, o.order_id')
 
       stats = []
-      (data || []).each do |record| 
+      (data || []).each do |record|
         stats << {
           given_name: record['given_name'],
           family_name: record['family_name'],
@@ -250,16 +258,20 @@ module OPDService
     end
 
     private
-    
+
+    def report_engine(type)
+      REPORTS[type.upcase].new
+    end
+
     def get_age_group(birthdate, end_date)
       begin
         birthdate = birthdate.to_date
         end_date  = end_date.to_date
-        months = age_in_months(birthdate, end_date) 
+        months = age_in_months(birthdate, end_date)
       rescue
         months = 'Unknown'
       end
-         
+
       if months == 'Unknown'
         return 'Unknown'
       elsif months < 6
@@ -291,23 +303,23 @@ module OPDService
       value_coded = ConceptName.find_by_name visit_type
 
       count = Encounter.where('encounter_datetime BETWEEN ? AND ?
-        AND encounter_type = ? AND concept_id = ? 
-        AND value_coded = ?', *TimeUtils.day_bounds(@date), type.id, 
+        AND encounter_type = ? AND concept_id = ?
+        AND value_coded = ?', *TimeUtils.day_bounds(@date), type.id,
         concept.concept_id, value_coded.concept_id).\
         joins('INNER JOIN obs USING(encounter_id)').\
         select('count(*) AS total')
-      
+
       return count[0]['total'].to_i
     end
 
     def monthly_registration(visit_type)
       start_date = (@date - 12.month)
       dates = []
-            
+
       start_date = start_date.beginning_of_month
       end_date  = start_date.end_of_month
       dates << [start_date, end_date]
-    
+
       1.upto(11) do |m|
         sdate = start_date + m.month
         edate = sdate.end_of_month
@@ -319,16 +331,16 @@ module OPDService
       value_coded = ConceptName.find_by_name visit_type
 
       months = {}
-  
+
       (dates || []).each_with_index do |(date1, date2), i|
         count = Encounter.where('encounter_datetime BETWEEN ? AND ?
-          AND encounter_type = ? AND concept_id = ? 
-          AND value_coded = ?', date1.strftime('%Y-%m-%d 00:00:00'), 
-          date2.strftime('%Y-%m-%d 23:59:59'), type.id, 
+          AND encounter_type = ? AND concept_id = ?
+          AND value_coded = ?', date1.strftime('%Y-%m-%d 00:00:00'),
+          date2.strftime('%Y-%m-%d 23:59:59'), type.id,
           concept.concept_id, value_coded.concept_id).\
           joins('INNER JOIN obs USING(encounter_id)').\
           select('count(*) AS total')
-      
+
         months[(i+1)]= {
           start_date: date1, end_date: date2,
           count: count[0]['total'].to_i
