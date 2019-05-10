8eb4faf82f80a06d6a62e0eab579ce91da8cabe3
kwalter <walterkaunda@live.co.uk>
2019-05-10 02:02:00 +0200

    Refactored pharmacy batch and items models.
    - Replaced timestamps with openmrs compatible timestamps.

--- db/migrate/20190508125042_create_pharmacy_batch_items.rb
+++ db/migrate/20190508125042_create_pharmacy_batch_items.rb
@@ -7,12 +7,13 @@ class CreatePharmacyBatchItems < ActiveRecord::Migration[5.2]
       t.float :current_quantity
       t.date :delivery_date
       t.date :expiry_date
+      t.integer :creator, null: false
+      t.datetime :date_created, null: false, default: -> { 'NOW()' }
+      t.datetime :date_changed, default: -> { 'NOW()' }
       t.boolean :voided
       t.integer :voided_by
       t.string :void_reason
-      t.date :date_voided
-
-      t.timestamps
+      t.datetime :date_voided
     end
   end
 end
