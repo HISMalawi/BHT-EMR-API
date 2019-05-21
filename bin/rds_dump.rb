7509f5c46be363d79550c6c909ec27d7e4ca3337
kwalter <walterkaunda@live.co.uk>
2019-05-21 15:55:19 +0200

    Added rds_dump script that dumps an SQL file instead of pushing to Couch DB.
    - Dumps SQL insert statements into 'log/rds_dump.sql'

---
+++ bin/rds_dump.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require_relative 'rds_push'
+
+def main
+  File.open(Rails.root.join('log', 'rds_dump.sql'), 'w') do |fout|
+    config['databases'].each do |database, database_config|
+      dump(database, database_config['program_name'], fout)
+    end
+  end
+end
+
+# Dump database in an RDS compatible format into given file.
+def dump(database, program_name, file)
+  MODELS.each do |model|
+    records = recent_records(model, TIME_EPOCH, database)
+
+    # Chunk retrieved records while converting them to JSON at the same time.
+    record_chunks = chunk_records(records, RECORDS_BATCH_SIZE) do |record|
+      record = serialize_record(record, program_name)
+      record.delete('record_type')
+      record
+    end
+
+    # Convert the chunks to SQL INSERT statements and write them to file
+    record_chunks.each do |chunk|
+      record_fields = chunk.first.keys
+
+      sql_values = chunk.map do |record|
+        values = record_fields.map do |field|
+          record[field] ? sql_quote(record[field]) : 'NULL'
+        end
+
+        "(#{values.join(', ')})"
+      end
+
+      sql_statement = <<~SQL
+        INSERT INTO #{model.table_name} (#{record_fields.join(', ')})
+        VALUES #{sql_values.join(', ')};
+      SQL
+
+      file.write(sql_statement)
+    end
+  end
+end
+
+# Converts an enumerator (generator) of records into an enumerator
+# of arrays of records of the given batch_size.
+def chunk_records(records, batch_size)
+  Enumerator.new do |enum|
+    chunked_records = []
+
+    records.each do |record|
+      if chunked_records.size == batch_size
+        enum.yield(chunked_records)
+        chunked_records = []
+      end
+
+      transformed_record = block_given? ? yield(record) : record
+      chunked_records.push(transformed_record)
+    end
+
+    # Yield the remaining records if any
+    enum.yield(chunked_records) unless chunked_records.empty?
+  end
+end
+
+def sql_quote(text)
+  ActiveRecord::Base.connection.quote(text)
+end
+
+main
