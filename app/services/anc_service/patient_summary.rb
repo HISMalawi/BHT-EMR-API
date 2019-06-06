e8769cf3d138692056372cf8fd6a7bb7320e117e
Pachawo Bisani <phbisan@gmail.com>
2019-06-06 13:44:17 +0200

    Added function to get patient's current outcome

--- app/services/anc_service/patient_summary.rb
+++ app/services/anc_service/patient_summary.rb
@@ -5,25 +5,25 @@ module ANCService
   class PatientSummary
     NPID_TYPE = 'National id'
     ARV_NO_TYPE = 'ARV Number'
-  
+
     SECONDS_IN_MONTH = 2_592_000
-  
+
     include ModelUtils
-  
+
     attr_reader :patient
     attr_reader :date
-  
+
     def initialize(patient, date)
       @patient = patient
       @date = date
       @art_service = ARTService::PatientSummary
     end
-  
+
     def full_summary
-        
+
       {
         patient_id: patient.patient_id,
-        current_outcome: "On treatment",
+        current_outcome: getCurrentPatientOutcome,
         residence: "",
         date_of_lnmp: date_of_lnmp,
         anc_visits: number_of_visits,
@@ -36,25 +36,28 @@ module ANCService
         .where(['encounter_type = ? AND obs.concept_id = ?',
           EncounterType.find_by_name('Current pregnancy').id,
           ConceptName.find_by_name('Last menstrual period').concept_id])
-        .last.observations.collect { 
-          |o| o.value_datetime 
+        .last.observations.collect {
+          |o| o.value_datetime
         }.compact.last.to_date rescue nil
-      
+
     end
 
     def number_of_visits
       lmp_date = date_of_lnmp
+
+      visits = []
 
       anc_visits = patient.encounters.joins([:observations])
         .where(['encounter_type = ? AND obs.concept_id = ?
             AND encounter_datetime > ?',
           EncounterType.find_by_name('ANC Visit Type').id,
           ConceptName.find_by_name('Reason for visit').concept_id,
-          lmp_date])
-        .last.observations.collect { 
-          |o| o.value_numeric 
-        }.compact.length rescue nil
-      
+          lmp_date]).each do |e|
+        e.observations.each do |o|
+          visits << o.value_numeric unless o.value_numeric.blank?
+        end
+      end
+      return visits.length
     end
 
     def fundus
@@ -68,6 +71,24 @@ module ANCService
         .last.observations.collect {|o|
           o.value_numeric
         }.compact.last.to_i rescue nil
+    end
+
+    def getCurrentPatientOutcome
+      state = ActiveRecord::Base.connection.select_one(
+        "SELECT state FROM patient_state INNER JOIN patient_program p ON p.patient_program_id = patient_state.patient_program_id
+        AND p.program_id = 12 WHERE (patient_state.voided = 0 AND p.voided = 0
+          AND p.program_id = program_id AND DATE(start_date) <= '#{@date}'
+          AND p.patient_id = #{@patient.id})
+          AND (patient_state.voided = 0)
+          ORDER BY start_date DESC, patient_state.patient_state_id DESC,
+          patient_state.date_created DESC LIMIT 1;"
+      )["state"]
+
+      outcome = ActiveRecord::Base.connection.select_one(
+        "SELECT name FROM program_workflow_state INNER JOIN concept_name ON concept_name.concept_id = program_workflow_state.concept_id
+        WHERE program_workflow_state_id = '#{state}';"
+      )["name"]
+
     end
 
   end
