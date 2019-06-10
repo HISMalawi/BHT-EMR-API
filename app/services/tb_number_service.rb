f8e6b5dbeeba97102390cdea10ce02cf5131237b
James Mwantisi <jamesmwantisi@gmail.com>
2019-06-10 14:07:16 +0500

    Assogn session on creation of tb/ipt number

--- app/services/tb_number_service.rb
+++ app/services/tb_number_service.rb
@@ -7,23 +7,23 @@ class TbNumberService
   IPT_NUMBER_IDENTIFIER_NAME = 'District IPT Number'
   FACILITY_CODE_GLOBAL_PROPERTY_NAME = 'site_prefix'
 
-  def assign_tb_number(patient_id)
+  def assign_tb_number(patient_id, date)
     PatientIdentifier.create(
-      identifier: generate_tb_number(),
+      identifier: generate_tb_number(date),
       type: patient_identifier_type(TB_NUMBER_IDENTIFIER_NAME),
       patient_id: patient_id,
       location_id: Location.current.location_id,
-      date_created: Time.now
+      date_created: date
     )
   end
 
-  def assign_ipt_number(patient_id)
+  def assign_ipt_number(patient_id, date)
     PatientIdentifier.create(
-      identifier: generate_tb_number(),
+      identifier: generate_ipt_number(date),
       type: patient_identifier_type(IPT_NUMBER_IDENTIFIER_NAME),
       patient_id: patient_id,
       location_id: Location.current.location_id,
-      date_created: Time.now
+      date_created: date
     )
   end
 
@@ -46,14 +46,14 @@ class TbNumberService
   end
 
   private
-  def generate_tb_number()
+  def generate_tb_number(date)
     id = retrieve_recent_id_number(TB_NUMBER_IDENTIFIER_NAME) + 1
-    "#{get_facility_code}/TB/#{id}/#{Time.now.year}"
+    "#{get_facility_code}/TB/#{id}/#{date.year}"
   end
 
-  def generate_ipt_number()
+  def generate_ipt_number(date)
     id = retrieve_recent_id_number(IPT_NUMBER_IDENTIFIER_NAME) + 1
-    "#{get_facility_code}/IPT/#{id}/#{Time.now.year}"
+    "#{get_facility_code}/IPT/#{id}/#{date.year}"
   end
 
   def retrieve_recent_id_number(id_type)
