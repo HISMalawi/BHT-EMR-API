a1bfba45c48625a63b023dbba2c7de84057e0448
James Mwantisi <jamesmwantisi@gmail.com>
2019-06-11 15:53:11 +0500

    Add Isoniazid concept to tb drug

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -37,7 +37,7 @@ class Drug < ActiveRecord::Base
   end
 
   def self.tb_drugs
-    tb_concept = Concept.joins(:concept_names).where(concept_name: { name: ['Rifampicin isoniazid and pyrazinamide', 'Ethambutol', 'Rifampicin and isoniazid', 'Rifampicin Isoniazid Pyrazinamide Ethambutol'] } )
+    tb_concept = Concept.joins(:concept_names).where(concept_name: { name: ['Rifampicin isoniazid and pyrazinamide', 'Ethambutol', 'Rifampicin and isoniazid', 'Rifampicin Isoniazid Pyrazinamide Ethambutol', 'Isoniazid'] } )
     Drug.where(concept: tb_concept)
   end
 
