a284b4fa2b6180c31fc966d38dbf650cc929d5ec
kwalter <walterkaunda@live.co.uk>
2019-06-12 14:19:50 +0200

    Added pharmacy tables to rds_push list.
    - Also added missing record type for User model.

--- app/models/pharmacy_batch_item_reallocation.rb
+++ app/models/pharmacy_batch_item_reallocation.rb
@@ -1,4 +1,4 @@
-class PharmacyBatchItemReallocation < ApplicationRecord
+class PharmacyBatchItemReallocation < VoidableRecord
   belongs_to :item, class_name: 'PharmacyBatchItem', foreign_key: :batch_item_id
   belongs_to :location, optional: true
 
