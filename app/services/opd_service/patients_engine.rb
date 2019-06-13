ad00ca503f063eafe42c278a84f014cf717c139a
Mwatha Bwanali <mwathabwanali@gmail.com>
2019-06-13 11:21:40 +0200

    adding PatientService:OPD

--- app/services/opd_service/patients_engine.rb
+++ app/services/opd_service/patients_engine.rb
@@ -8,4 +8,16 @@ class OPDService::PatientsEngine
   def visit_summary_label(patient, date)
     OPDService::VisitLabel.new(patient, date)
   end
+  
+  # Retrieves given patient's status info.
+  #
+  # The info is just what you would get on a patient information
+  # confirmation page in an ART application.
+  def patient(patient_id, date)
+    patient_summary(Patient.find(patient_id), date).full_summary
+  end
+
+  def patient_summary(patient, date)
+    PatientSummary.new patient, date
+  end
 end
