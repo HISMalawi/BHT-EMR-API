39666b33e850d54c5cb1d716c76ca0cb505e4a3f
James Mwantisi <jamesmwantisi@gmail.com>
2019-06-21 09:57:47 +0500

    Remove side TB effects mapping

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -48,9 +48,4 @@ class Drug < ActiveRecord::Base
     Drug.where(concept: tb_concept)
   end
 
-  def self.tb_side_effects_drug
-    tb_concept = Concept.joins(:concept_names)\
-                        .where(concept_name: { name: %w[Streptomycin Pyridoxine Rifabutin] })
-    Drug.where(concept: tb_concept)
-  end
 end
