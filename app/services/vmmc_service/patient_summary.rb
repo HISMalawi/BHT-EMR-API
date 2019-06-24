ba3190e993c47b923a64b0ccad3e961baf1877fa
kajanesoko <starjsoko@gmail.com>
2019-06-24 11:01:18 +0000

    created methods for pulling vitals data on patient dashboard

--- app/services/vmmc_service/patient_summary.rb
+++ app/services/vmmc_service/patient_summary.rb
@@ -16,15 +16,66 @@ module VMMCService
     def initialize(patient, date)
       @patient = patient
       @date = date
+      @vmmc_service = VMMCService::PatientSummary
     end
   
     def full_summary
+    
+        vitals_bp = "#{systolic_blood_pressure} / #{diastolic_blood_pressure}" 
         
       {
         patient_id: patient.patient_id,
-        current_outcome: "",
-        residence: ""
+        vitals_temperature: vitals_temperature,
+        vitals_bp: vitals_bp,
+        vitals_weight: vitals_weight,
+        vitals_pulse: vitals_pulse
       }
+    end
+
+    def vitals_temperature
+
+      temperature = Observation.joins([:encounter])
+        .where(['person_id = ? AND encounter_type = ? AND obs.concept_id = ?',
+          patient.id,
+          EncounterType.find_by_name('Vitals').id,
+          ConceptName.find_by_name('Temperature (c)').concept_id]
+        ).order(:obs_datetime).last&.value_numeric
+    end
+
+    def systolic_blood_pressure
+      sbp = Observation.joins([:encounter])
+        .where(['person_id = ? AND encounter_type = ? AND obs.concept_id = ?',
+          patient.id,
+          EncounterType.find_by_name('Vitals').id,
+          ConceptName.find_by_name('Systolic blood pressure').concept_id]
+        ).order(:obs_datetime).last&.value_numeric.to_i
+    end
+
+    def diastolic_blood_pressure
+      dbp = Observation.joins([:encounter])
+        .where(['person_id = ? AND encounter_type = ? AND obs.concept_id = ?',
+          patient.id,
+          EncounterType.find_by_name('Vitals').id,
+          ConceptName.find_by_name('Diastolic blood pressure').concept_id]
+        ).order(:obs_datetime).last&.value_numeric.to_i
+    end
+
+    def vitals_weight
+      weight = Observation.joins([:encounter])
+        .where(['person_id = ? AND encounter_type = ? AND obs.concept_id = ?',
+          patient.id,
+          EncounterType.find_by_name('Vitals').id,
+          ConceptName.find_by_name('Weight (kg)').concept_id]
+        ).order(:obs_datetime).last&.value_numeric
+    end
+
+    def vitals_pulse
+      pulse = Observation.joins([:encounter])
+        .where(['person_id = ? AND encounter_type = ? AND obs.concept_id = ?',
+          patient.id,
+          EncounterType.find_by_name('Vitals').id,
+          ConceptName.find_by_name('Pulse').concept_id]
+        ).order(:obs_datetime).last&.value_numeric
     end
   
   end
