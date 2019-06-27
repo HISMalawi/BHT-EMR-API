b6b61a464bd4469edd615586b72cfb389c9e5d70
kwalter <walterkaunda@live.co.uk>
2019-06-27 09:59:12 +0200

    Fixed last seen observation timestamps persistence in ART stock update script

--- bin/daily_art_stock_updates.rb
+++ bin/daily_art_stock_updates.rb
@@ -10,29 +10,33 @@ LOGGER = Logger.new(STDOUT)
 ActiveRecord::Base.logger = LOGGER
 
 def main
-  debits_offset, credits_offset = read_timestamps
-  process_debits(debits_offset)
-  process_credits(credits_offset)
-  save_timestamps(debits_offset, credits_offset)
+  debits_timestamp, credits_timestamp = read_timestamps
+  debits_timestamp = process_debits(debits_timestamp)
+  credits_timestamp = process_credits(credits_timestamp)
+  save_timestamps(debits_timestamp, credits_timestamp)
 end
 
 def process_debits(date)
   LOGGER.debug('Processing ART stock debits (dispensations)')
-  dispensations(date).reduce do |_accumulator, dispensation|
+  dispensations = dispensations(date)
+
+  dispensations.each do |dispensation|
     login_as(dispensation.creator, at: dispensation.location_id)
     stock_service.update_batch_items(StockManagementService::STOCK_DEBIT,
                                      dispensation.value_drug,
                                      dispensation.value_numeric,
                                      dispensation.obs_datetime.to_date)
-
-    dispensation.date_created # Capture the very last date
   end
+
+  dispensations.last&.date_created || date
 end
 
 # Credits are simply just voids
 def process_credits(date)
   LOGGER.debug('Processing ART stock credits (voided dispensations)')
-  voided_dispensations(date).reduce do |_accumulator, dispensation|
+  dispensations = voided_dispensations(date)
+
+  dispensations.each do |dispensation|
     next if dispensation.date_voided.to_date == dispensation.date_created.to_date
 
     login_as(dispensation.creator, at: dispensation.location_id)
@@ -40,13 +44,13 @@ def process_credits(date)
                                      dispensation.value_drug,
                                      dispensation.value_numeric,
                                      dispensation.obs_datetime.to_date)
-
-    dispensation.date_voided # Capture the very last date
   end
+
+  dispensations.last&.date_voided || date
 end
 
 def dispensations(date)
-  Observation.where('concept_id = ? AND date_created >= ?',
+  Observation.where('concept_id = ? AND date_created > ?',
                     concept_name_to_id('Amount dispensed'), date)\
              .order(:date_created)
 end
@@ -80,18 +84,18 @@ OFFSETS_FILE = Rails.root.join('log', 'ART-stock-timestamps.yml')
 
 def read_timestamps
   File.open(OFFSETS_FILE) do |fin|
-    timestamps = YAML.load(fin.read)
-    [timestamps[:debits_timestamp], timestamps[:credits_timestamp]]
+    timestamps = YAML.safe_load(fin.read)
+    return [timestamps['debits_timestamp'].to_time, timestamps['credits_timestamp'].to_time]
   end
-rescue StandardError => e
+rescue Errno::ENOENT => e
   LOGGER.debug("#{OFFSETS_FILE} not found: #{e} - #{e.message}")
   [Date.today.to_time, Date.today.to_time]
 end
 
 def save_timestamps(debits_timestamp, credits_timestamp)
   File.open(Rails.root.join(OFFSETS_FILE), 'w') do |fout|
-    fout.write({ debits_timestamp: debits_timestamp,
-                 credits_timestamp: credits_timestamp }.to_yaml)
+    fout.write({ 'debits_timestamp' => debits_timestamp.to_s,
+                 'credits_timestamp' => credits_timestamp.to_s }.to_yaml)
   end
 end
 
