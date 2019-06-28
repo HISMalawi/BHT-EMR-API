57f829de0c90702dcccb7ff674ada5553daad535
kwalter <walterkaunda@live.co.uk>
2019-06-28 15:16:08 +0200

    Updated field creator_user of VoidableRecord to be optional.

--- app/models/voidable_record.rb
+++ app/models/voidable_record.rb
@@ -6,5 +6,5 @@ class VoidableRecord < ApplicationRecord
 
   default_scope { where(voided: 0) }
 
-  belongs_to :creator_user, foreign_key: 'creator', class_name: 'User'
+  belongs_to :creator_user, foreign_key: 'creator', class_name: 'User', optional: true
 end
