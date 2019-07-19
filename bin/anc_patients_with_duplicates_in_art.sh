38d7114ac8e4cadc98bf1cb1edbadef690ff971e
Deliwe <dbnkhoma@gmail.com>
2019-07-19 16:15:57 +0200

    ANC fix for Machinjiri

--- bin/anc_patients_with_duplicates_in_art.sh
+++ bin/anc_patients_with_duplicates_in_art.sh
@@ -1,3 +1,37 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 ENVIRONMENT"
+  echo
+  echo "ENVIRONMENT should be: development|test|production"
+}
+
+ENV=$1
+
+if [ -z "$ENV" ]; then
+  usage
+  exit 255
+fi
+
+set -x # turns on stacktrace mode which gives useful debug information
+
+export RAILS_ENV=$ENV
+rails db:environment:set RAILS_ENV=$ENV
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
+ANCDATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['anc_database']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
+
+echo "=================merging patients in ANC only into ART database"
+
+start_now=$(date +”%T”)
+echo "the script is starting at: " start_now 
+
+mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE <<EOF
+
+
 SET foreign_key_checks = 0;
 /* the defaults */
 SET @max_encounter_id := (SELECT max(encounter_id) FROM $DATABASE.encounter);
@@ -8,16 +42,17 @@ SET @max_obs_id := (SELECT max(obs_id) FROM $DATABASE.obs);
 SET @max_user_id := (select max(user_id) from $DATABASE.users);
 
 /* dropping and creating person_back_up  */
-DROP table IF EXISTS $ANCDATABASE.ANC_last_patients_not_migrated;
+DROP table IF EXISTS $ANCDATABASE.patients_remaining_to_be_migrated;
 
-create table $ANCDATABASE.ANC_last_patients_not_migrated as
-SELECT patient_id AS ANC_patient_id, (SELECT @max_patient_id + patient_id) AS ART_patient_id FROM $ANCDATABASE.patient_identifier WHERE identifier_type = 3 AND voided = 0 AND patient_id NOT IN (SELECT ANC_patient_id FROM $ANCDATABASE.ANC_patient_details) AND identifier NOT IN (SELECT identifier FROM $DATABASE.patient_identifier WHERE identifier in (SELECT identifier FROM $ANCDATABASE.patient_identifier WHERE patient_id NOT IN (SELECT ANC_patient_id FROM $ANCDATABASE.ANC_patient_details))) GROUP BY patient_id;
+create table $ANCDATABASE.patients_remaining_to_be_migrated as
+SELECT ANC_patient_id, (SELECT @max_patient_id + ANC_patient_id) AS ART_patient_id, ART_gender, new_identifier_type, ART_creator
+FROM $ANCDATABASE.anc_art_patients_with_voided_art_identifier WHERE per_voided = 0;
 
 /* The first query is inserting BDE person table into main person table minus the users  */
 INSERT INTO $DATABASE.person (person_id, gender, birthdate, birthdate_estimated, dead, death_date, cause_of_death, creator, date_created, changed_by, date_changed, voided, voided_by, date_voided, void_reason, uuid)
 select pp.ART_patient_id, p.gender, p.birthdate, p.birthdate_estimated, p.dead, p.death_date, p.cause_of_death, c.ART_user_id, p.date_created, u.ART_user_id, p.date_changed, p.voided, p.voided_by, p.date_voided, p.void_reason, (select uuid()) 
 from $ANCDATABASE.person p 
- inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
+ inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
  left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator
  left join $ANCDATABASE.user_bak u on u.ANC_user_id = p.changed_by;
 
@@ -25,7 +60,7 @@ from $ANCDATABASE.person p
 INSERT INTO $DATABASE.person_name (preferred, person_id, prefix, given_name, middle_name, family_name_prefix, family_name, family_name2, family_name_suffix, degree, creator, date_created, voided, voided_by, date_voided, void_reason, changed_by, date_changed, uuid)
 select p.preferred, pp.ART_patient_id, p.prefix, p.given_name, p.middle_name, p.family_name_prefix, p.family_name, p.family_name2, p.family_name_suffix, p.degree, c.ART_user_id, p.date_created, p.voided,  p.voided_by, p.date_voided, p.void_reason,  u.ART_user_id, p.date_changed, (select uuid())
 from $ANCDATABASE.person_name p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided  = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided  = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator
 	left join $ANCDATABASE.user_bak u on u.ANC_user_id = p.changed_by;
 
@@ -33,14 +68,14 @@ from $ANCDATABASE.person_name p
 INSERT INTO $DATABASE.person_address (person_id,  preferred,  address1,  address2,  city_village,  state_province,  postal_code,  country,  latitude,  longitude,  creator,  date_created,  voided,  voided_by,  date_voided, void_reason, county_district,  neighborhood_cell,  region,  subregion,  township_division,  uuid)
 select pp.ART_patient_id, p.preferred,  p.address1,  p.address2,  p.city_village,  p.state_province, p.postal_code,  p.country,  p.latitude,  p.longitude,  c.ART_user_id,  p.date_created,  p.voided,  p.voided_by, p.date_voided, p.void_reason, p.county_district,  p.neighborhood_cell,  p.region,  p.subregion,  p.township_division, (select uuid())
 from $ANCDATABASE.person_address p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator;
 
 /* This query insert BDE person_attributes into main person_attributes  */
 INSERT INTO $DATABASE.person_attribute (person_id, value, person_attribute_type_id, creator, date_created, changed_by, date_changed, voided, voided_by, date_voided, void_reason, uuid)
 select pp.ART_patient_id, p.value, p.person_attribute_type_id, c.ART_user_id, p.date_created,  u.ART_user_id, p.date_changed, p.voided,  p.voided_by, p.date_voided, p.void_reason, (select uuid()) 
 from $ANCDATABASE.person_attribute p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator
 	left join $ANCDATABASE.user_bak u on u.ANC_user_id = p.changed_by;
 
@@ -48,15 +83,15 @@ from $ANCDATABASE.person_attribute p
 INSERT INTO $DATABASE.patient (patient_id, tribe, creator, date_created, changed_by, date_changed, voided, voided_by, date_voided, void_reason)
 select pp.ART_patient_id, p.tribe, c.ART_user_id, p.date_created,  u.ART_user_id, p.date_changed, p.voided,  p.voided_by, p.date_voided, p.void_reason 
 from $ANCDATABASE.patient p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator
 	left join $ANCDATABASE.user_bak u on u.ANC_user_id = p.changed_by;
 
 /* This query insert BDE patient_identifier into main patient_identifier  */
 INSERT INTO $DATABASE.patient_identifier (patient_id,  identifier,  identifier_type,  preferred,  location_id,  creator,  date_created,  voided,  voided_by,  date_voided,  void_reason,  uuid)
-select pp.ART_patient_id, p.identifier, p.identifier_type,  p.preferred,  p.location_id,  c.ART_user_id,  p.date_created,  p.voided,  p.voided_by, p.date_voided, p.void_reason, (select uuid()) 
+select pp.ART_patient_id, p.identifier, pp.new_identifier_type,  p.preferred,  p.location_id,  c.ART_user_id,  p.date_created,  p.voided,  p.voided_by, p.date_voided, p.void_reason, (select uuid()) 
 from $ANCDATABASE.patient_identifier p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator;
 
 /* This query back-up main encounter  */
@@ -64,7 +99,7 @@ drop table if exists $ANCDATABASE.encounter_bak_up;
 create table  $ANCDATABASE.encounter_bak_up as
 select (select @max_encounter_id + e.encounter_id) as encounter_id, e.encounter_type, pp.ART_patient_id as patient_id, u.ART_user_id AS provider_id, e.location_id, e.form_id, e.encounter_datetime, c.ART_user_id AS creator, e.date_created, e.voided, e.voided_by, e.date_voided, e.void_reason, e.uuid, e.changed_by, e.date_changed
 from $ANCDATABASE.encounter e 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = e.patient_id and e.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = e.patient_id and e.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = e.creator
 	left join $ANCDATABASE.user_bak u on u.ANC_user_id = e.provider_id;
 
@@ -77,7 +112,7 @@ drop table if exists $ANCDATABASE.patient_program_bakup;
 create table  $ANCDATABASE.patient_program_bakup as
 SELECT patient_program_id as anc_patient_program_id, (select @max_patient_program_id + patient_program_id) as patient_program_id, pp.ART_patient_id as patient_id, program_id,  date_enrolled,  date_completed,  c.ART_user_id as creator, p.date_created, u.ART_user_id as changed_by, p.date_changed,  p.voided,  p.voided_by,  p.date_voided,  p.void_reason,  p.uuid,  location_id 
 FROM $ANCDATABASE.patient_program p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator
 	left join $ANCDATABASE.user_bak u on u.ANC_user_id = p.changed_by;
 
@@ -99,7 +134,7 @@ drop table if exists $ANCDATABASE.orders_bak;
 create table $ANCDATABASE.orders_bak as
 SELECT order_id AS ANC_order_id,(SELECT @max_order_id + order_id) as ART_order_id, order_type_id, concept_id, orderer, (SELECT @max_encounter_id + encounter_id) as encounter_id, instructions,  start_date,  auto_expire_date,  discontinued,  discontinued_date, discontinued_by,  discontinued_reason, c.ART_user_id as creator, p.date_created,  p.voided,  p.voided_by,  p.date_voided,  p.void_reason, pp.ART_patient_id as patient_id,  accession_number, (SELECT @max_obs_id + obs_id) as obs_id,  uuid, discontinued_reason_non_coded 
 FROM $ANCDATABASE.orders p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.patient_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator;
 
 INSERT INTO $DATABASE.orders (order_id, order_type_id, concept_id, orderer,  encounter_id,  instructions,  start_date,  auto_expire_date,  discontinued,  discontinued_date, discontinued_by,  discontinued_reason, creator, date_created,  voided,  voided_by,  date_voided,  void_reason, patient_id,  accession_number, obs_id,  uuid, discontinued_reason_non_coded)
@@ -115,7 +150,7 @@ FROM $ANCDATABASE.drug_order dr
 INSERT INTO $DATABASE.obs (obs_id, person_id,  concept_id,  encounter_id,  order_id,  obs_datetime,  location_id,  obs_group_id,  accession_number,  value_group_id,  value_boolean,  value_coded,  value_coded_name_id,  value_drug,  value_datetime,  value_numeric,  value_modifier,  value_text,  date_started,  date_stopped,  comments,  creator,  date_created,  voided,  voided_by,  date_voided,  void_reason,  value_complex,  uuid)
 SELECT (SELECT @max_obs_id + obs_id) as obs_id, pp.ART_patient_id,  concept_id,  (SELECT @max_encounter_id + encounter_id) as encounter_id,  (SELECT @max_order_id + order_id) as order_id, p.obs_datetime, p.location_id, p.obs_group_id, p.accession_number, p.value_group_id, p.value_boolean, p.value_coded, p.value_coded_name_id, p.value_drug, p.value_datetime, p.value_numeric, p.value_modifier, p.value_text, p.date_started, p.date_stopped,  p.comments, c.ART_user_id, p.date_created, p.voided, p.voided_by, p.date_voided, p.void_reason, p.value_complex,  (SELECT UUID()) 
 FROM $ANCDATABASE.obs  p 
-	inner join $ANCDATABASE.ANC_last_patients_not_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
+	inner join $ANCDATABASE.patients_remaining_to_be_migrated pp ON pp.ANC_patient_id = p.person_id and p.voided = 0
 	left join $ANCDATABASE.user_bak c on c.ANC_user_id = p.creator;
 
 /* Update Observation (61) encounter_type to ANC Examination (98) encounter_type */
@@ -157,3 +192,13 @@ UPDATE $DATABASE.encounter SET provider_id = creator WHERE provider_id = 0;
 UPDATE $DATABASE.encounter SET provider_id = creator WHERE provider_id IS NULL;
 
 SET foreign_key_checks = 1;
+
+EOF
+
+echo "====================================================Finished merging the data"
+
+end_now=$(date +”%T”)
+
+echo "the script started running at: " start_now
+echo "and ended at: " end_now 
+
