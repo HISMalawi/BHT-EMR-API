60b4137392d15e9779fd655ca82902d751180fbf
danielmwakanema <danielmwakanema95@gmail.com>
2019-08-20 20:25:41 +0200

    feat(Reports): Add encounters query object

---
+++ app/queries/tb_queries/encounters_query.rb
@@ -0,0 +1,26 @@
+include TimeUtils
+
+class TBQueries::EncountersQuery
+  include ModelUtils
+
+  def initialize (relation = Encounter.all)
+    @relation = relation
+    @program = program('TB Program')
+  end
+
+  def by_date (date, type)
+    type = encounter_type(type)
+    start_time, end_time = TimeUtils.day_bounds(date)
+
+    @relation.where(program: @program,
+                    type: type,
+                    encounter_datetime: start_time..end_time)
+  end
+
+  def by_year (year, type)
+    type = encounter_type(type)
+    @relation.where(program: @program,
+                    type: type)\
+             .where('YEAR(encounter_datetime) = ?', year)
+  end
+end
