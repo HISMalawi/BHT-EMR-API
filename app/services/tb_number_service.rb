d885ec0597edb7cec86eee32cb5fb1265099ecdc
danielmwakanema <danielmwakanema95@gmail.com>
2019-08-23 09:05:57 +0200

    feat(Services): Add TB Number service

---
+++ app/services/tb_number_service.rb
@@ -0,0 +1,64 @@
+include ModelUtils
+
+class TBNumberService
+  class TbNumberAlreadyExistsException < StandardError; end
+
+  NORMAL_TYPE = 'District TB Number'
+  IPT_TYPE = 'District IPT Number'
+
+  def self.assign_tb_number (patient_id, date, number = nil)
+    suggested = generate_tb_number(patient_id, date, number)
+    raise TbNumberAlreadyExistsException if number && number_exists?(number: suggested)
+
+    PatientIdentifier.create(
+      identifier: suggested,
+      type: number_type(patient_id: patient_id),
+      patient_id: patient_id,
+      location_id: Location.current.location_id,
+      date_created: date
+    )
+  end
+
+  private
+
+  def self.generate_tb_number (patient_id, date, number = nil)
+    is_ipt_patient = ipt_eligible?(patient_id: patient_id)
+    category = is_ipt_patient ? 'IPT' : 'TB'
+
+    next_number = number
+    next_number = next_available_number(patient_id: patient_id) if number == nil
+
+    "#{facility_code}/#{category}/#{next_number}/#{date.year}"
+  end
+
+  def self.number_exists?(number:)
+    PatientIdentifier.where(identifier: number)\
+                     .exists?
+  end
+
+  def self.ipt_eligible? (patient_id:)
+    regimen_engine.is_eligible_for_ipt?(person: Person.find(patient_id))
+  end
+
+  def self.next_available_number (patient_id:)
+    number = PatientIdentifier.where(type: number_type(patient_id: patient_id))\
+                              .order(date_created: :desc)\
+                              .first
+
+    return 1 if number.blank?
+    number.patient_identifier_id.next
+  end
+
+  def self.number_type (patient_id:)
+    type = ipt_eligible?(patient_id: patient_id) ? IPT_TYPE : NORMAL_TYPE
+    patient_identifier_type(type)
+  end
+
+  def self.regimen_engine
+    TBService::RegimenEngine.new(program: program('TB Program'))
+  end
+
+  def self.facility_code
+    global_property('site_prefix')&.property_value
+  end
+end
