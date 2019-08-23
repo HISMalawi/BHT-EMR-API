50af2aa96c1b281324cc42962f8d9a31d34c981d
danielmwakanema <danielmwakanema95@gmail.com>
2019-08-23 09:06:34 +0200

    refactor(TBPatientEngine): Remove TB Number code

--- app/services/tb_service/patients_engine.rb
+++ app/services/tb_service/patients_engine.rb
@@ -7,10 +7,6 @@ module TBService
   class PatientsEngine
     include ModelUtils
 
-    TB_NUMBER_IDENTIFIER_NAME = 'District TB Number'
-    IPT_NUMBER_IDENTIFIER_NAME = 'District IPT Number'
-    FACILITY_CODE_GLOBAL_PROPERTY_NAME = 'site_prefix'
-
     def initialize(program:)
       @program = program
     end
@@ -88,85 +84,10 @@ module TBService
       TBService::PatientVisitLabel.new patient, date
     end
 
-    # TB registration
-
-    def assign_tb_number(patient_id, date)
-      person = Person.find_by(person_id: patient_id)
-      if regimen_engine.is_eligible_for_ipt?(person: person)
-        PatientIdentifier.create(
-          identifier: generate_ipt_number(date),
-          type: patient_identifier_type(IPT_NUMBER_IDENTIFIER_NAME),
-          patient_id: patient_id,
-          location_id: Location.current.location_id,
-          date_created: date
-        )
-      else
-        PatientIdentifier.create(
-          identifier: generate_tb_number(date),
-          type: patient_identifier_type(TB_NUMBER_IDENTIFIER_NAME),
-          patient_id: patient_id,
-          location_id: Location.current.location_id,
-          date_created: date
-        )
-      end
-    end
-
-    def get_tb_number(patient_id)
-      tb_number = PatientIdentifier.where(
-        type: patient_identifier_type(TB_NUMBER_IDENTIFIER_NAME),
-        patient_id: patient_id
-      )
-                                   .order(date_created: :desc)
-                                   .first
-
-      ipt_number = PatientIdentifier.where(
-        type: patient_identifier_type(IPT_NUMBER_IDENTIFIER_NAME),
-        patient_id: patient_id
-      )
-                                    .order(date_created: :desc)
-                                    .first
-
-      (tb_number || ipt_number)
-    end
-
     private
 
     def patient_summary(patient, date)
       TBService::PatientSummary.new patient, date
-    end
-
-    def regimen_engine
-      TBService::RegimenEngine.new program: program('TB PROGRAM')
-    end
-
-    # TB registration
-
-    def generate_tb_number(date)
-      id = retrieve_recent_id_number(TB_NUMBER_IDENTIFIER_NAME).next
-      "#{get_facility_code}/TB/#{id}/#{date.year}"
-    end
-
-    def generate_ipt_number(date)
-      id = retrieve_recent_id_number(IPT_NUMBER_IDENTIFIER_NAME).next
-      "#{get_facility_code}/IPT/#{id}/#{date.year}"
-    end
-
-    def retrieve_recent_id_number(id_type)
-      patient_identifer = PatientIdentifier.where(
-        type: patient_identifier_type(id_type)
-      )
-                                           .order(date_created: :desc)
-                                           .first
-
-      begin
-        patient_identifer.patient_identifier_id
-      rescue StandardError
-        0
-      end
-    end
-
-    def get_facility_code
-      global_property(FACILITY_CODE_GLOBAL_PROPERTY_NAME)&.property_value
     end
   end
   end
