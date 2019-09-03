769911ec383f25fa0b8b50c22203c27ab8de7f50
danielmwakanema <danielmwakanema95@gmail.com>
2019-09-03 12:05:53 +0200

    refactor(RelapsePatientsQuery): Add clinically confirmed

--- app/queries/tb_queries/relapse_patients_query.rb
+++ app/queries/tb_queries/relapse_patients_query.rb
@@ -38,6 +38,35 @@ class TBQueries::RelapsePatientsQuery
     )
   end
 
+  def clinically_confirmed (start_date, end_date)
+    type = encounter_type('Diagnosis').encounter_type_id
+    value = concept('Positive').concept_id
+    ActiveRecord::Base.connection.select_all(
+      <<~SQL
+        SELECT States.patient_id
+        FROM
+        (
+          SELECT DISTINCT(patient_id), patient_state.date_created
+          FROM
+            patient JOIN patient_program USING(patient_id)
+          JOIN patient_state USING(patient_program_id)
+          WHERE patient_state.state = '#{STATES[:RP]}' AND patient_state.end_date IS NULL AND patient_state.voided = 0
+        ) AS States
+        JOIN
+        (
+          SELECT DISTINCT(person_id), obs_datetime
+          FROM encounter JOIN obs USING(encounter_id)
+          WHERE
+            value_coded = '#{value}' AND encounter_datetime BETWEEN "#{start_date}"
+            AND "#{end_date}" AND encounter_type = '#{type}' AND encounter.voided = 0
+            AND encounter.program_id = '#{@program.program_id}'
+        ) AS BactConfirmed
+        ON States.patient_id = BactConfirmed.person_id
+        WHERE BactConfirmed.obs_datetime <= States.date_created;
+      SQL
+    )
+  end
+
   def clinical_pulmonary (start_date, end_date)
     type = encounter_type('Diagnosis').encounter_type_id
     value = concept('Positive').concept_id
