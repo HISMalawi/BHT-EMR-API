1aca8cd2c50a4d66c8956cc0d25349350c1a79f1
danielmwakanema <danielmwakanema95@gmail.com>
2019-09-03 12:08:04 +0200

    refactor(TbHivReport): Refactor hiv status queries

--- app/services/tb_service/reports/tbhiv.rb
+++ app/services/tb_service/reports/tbhiv.rb
@@ -3,35 +3,54 @@
 module TBService::Reports::Tbhiv
   class << self
     def new_and_relapse_tb_cases_notified (start_date, end_date)
-      patients = patients_query.with_encounters(['TB_Initial'], start_date, end_date)
+      patients = patients_query.new_patients(start_date, end_date)
 
-      relapse_patients = states_query.any_relapse(start_date, end_date)
+      bacterial = relapse_patients_query.bacteriologically_confirmed(start_date, end_date)
+      clinic = relapse_patients_query.clinically_confirmed(start_date, end_date)
 
-      return [] if patients.empty? && relapse_patients.empty?
+      return [] if patients.empty? && bacterial.empty? && clinic.empty?
 
       new_ids = patients.map(&:patient_id)
+      bacterial_ids = bacterial.map { |patient| patient['patient_id'] }
+      clinical_ids = clinic.map { |patient| patient['patient_id'] }
 
-      ids = (new_ids + relapse_patients).uniq
+      ids = (new_ids + bacterial_ids + clinical_ids).uniq
 
       persons_query.group_by_gender(ids)
     end
 
     def total_with_hiv_result_documented (start_date, end_date)
-      patients = patients_query.with_obs_ignore_value('TB_Initial', 'HIV Status', start_date, end_date)
+      hiv_status = concept('HIV Status')
+      type = encounter_type('TB_Initial')
+
+      patients = Observation.select(:person_id).distinct\
+                            .joins(:encounter)\
+                            .where(:encounter => { encounter_type: type,
+                                                   encounter_datetime: start_date..end_date },
+                                   :obs => { concept_id: hiv_status })
 
       return [] if patients.empty?
 
-      ids = patients.map(&:patient_id)
+      ids = patients.map(&:person_id)
 
       persons_query.group_by_gender(ids)
     end
 
     def total_tested_hiv_positive (start_date, end_date)
-      patients = patients_query.with_obs('TB_Initial', 'HIV Status', 'Positive', start_date, end_date)
+      hiv_status = concept('HIV Status')
+      type = encounter_type('TB_Initial')
+      positive = concept('Positive')
+
+      patients = Observation.select(:person_id).distinct\
+                            .joins(:encounter)\
+                            .where(:encounter => { encounter_type: type,
+                                                   encounter_datetime: start_date..end_date },
+                                   :obs => { concept_id: hiv_status,
+                                             value_coded: positive})
 
       return [] if patients.empty?
 
-      ids = patients.map(&:patient_id)
+      ids = patients.map(&:person_id)
 
       persons_query.group_by_gender(ids)
     end
@@ -113,6 +132,10 @@ module TBService::Reports::Tbhiv
 
     def persons_query
       TBQueries::PersonsQuery.new
+    end
+
+    def relapse_patients_query
+      TBQueries::RelapsePatientsQuery.new
     end
   end
 end
