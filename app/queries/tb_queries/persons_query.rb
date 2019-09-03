0f4e112f97411e730d44bd8a3cf827fcdfaf4cfa
danielmwakanema <danielmwakanema95@gmail.com>
2019-09-03 12:42:09 +0200

    refactor(Queries): Drop unnecessary include

--- app/queries/tb_queries/persons_query.rb
+++ app/queries/tb_queries/persons_query.rb
@@ -1,13 +1,11 @@
-module TBQueries
-  class PersonsQuery
-    def initialize (relation = Person.all)
-      @relation = relation
-    end
+class TBQueries::PersonsQuery
+  def initialize (relation = Person.all)
+    @relation = relation
+  end
 
-    def group_by_gender (ids)
-      @relation.select('gender', 'COUNT(*) AS count')
-               .where(person_id: ids)
-               .group(:gender)
-    end
+  def group_by_gender (ids)
+    @relation.select('gender', 'COUNT(*) AS count')
+             .where(person_id: ids)
+             .group(:gender)
   end
 end
