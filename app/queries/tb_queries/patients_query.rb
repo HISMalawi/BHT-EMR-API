7c6daf5e92d751a43d7cb959c566704c0efd8445
danielmwakanema <danielmwakanema95@gmail.com>
2019-09-03 12:34:00 +0200

    refactor(PatientsQuery): Drop duplicate methods

--- app/queries/tb_queries/patients_query.rb
+++ app/queries/tb_queries/patients_query.rb
@@ -38,30 +38,12 @@ module TBQueries
                           .having('GROUP_CONCAT(encounter.encounter_type) LIKE ?', filter)
       end
 
-      def or_with_encounters (first, second, start_date, end_date)
-        filter_one = encounters_filter(first)
-        filter_two = encounters_filter(second)
-        program = program('TB Program')
-
-        joins(:encounters).where(encounter: { encounter_datetime: start_date..end_date, program_id: program.program_id })\
-                          .group(:patient_id)\
-                          .having('GROUP_CONCAT(encounter.encounter_type) LIKE ? OR GROUP_CONCAT(encounter.encounter_type) LIKE ?', filter_one, filter_two)
-      end
-
-      def without_encounters (encounters, start_date, end_date)
+      def without_encounters (encounters, start_date = nil, end_date = nil)
         filter = encounters_filter(encounters)
-        program = program('TB Program')
+        where_filter = { :encounter => { program_id: tb_program.program_id } }
+        where_filter[:encounter][:encounter_datetime] = (start_date..end_date) if (start_date && end_date)
 
-        joins(:encounters).where(encounter: { encounter_datetime: start_date..end_date, program_id: program.program_id })\
-                          .group(:patient_id)\
-                          .having('GROUP_CONCAT(encounter.encounter_type) NOT LIKE ?', filter)
-      end
-
-      def without_encounters_ever (encounters)
-        filter = encounters_filter(encounters)
-        program = program('TB Program')
-
-        joins(:encounters).where(encounter: { program_id: program.program_id })\
+        joins(:encounters).where(where_filter)\
                           .group(:patient_id)\
                           .having('GROUP_CONCAT(encounter.encounter_type) NOT LIKE ?', filter)
       end
@@ -76,29 +58,6 @@ module TBQueries
                                                                encounter_datetime: start_date..end_date,
                                                                program_id: program.program_id },
                                                   obs: { concept_id: concept.concept_id, value_coded: value.concept_id })
-      end
-
-      def with_obs_before (encounter, name, answer, start_date)
-        type = encounter_type(encounter)
-        concept = concept(name)
-        value = concept(answer)
-        program = program('TB Program')
-
-        joins(:encounters => :observations).where(encounter: { encounter_type: type.encounter_type_id,
-                                                               program_id: program.program_id },
-                                                  obs: { concept_id: concept.concept_id, value_coded: value.concept_id })\
-                                           .where('encounter.encounter_datetime < ?', start_date)
-      end
-
-      def with_obs_ignore_value (encounter, name, start_date, end_date)
-        type = encounter_type(encounter)
-        concept = concept(name)
-        program = program('TB Program')
-
-        joins(:encounters => :observations).where(encounter: { encounter_type: type.encounter_type_id,
-                                                               encounter_datetime: start_date..end_date,
-                                                               program_id: program.program_id },
-                                                  obs: { concept_id: concept.concept_id })
       end
 
       def some_with_obs (patients, encounter, name, answer)
