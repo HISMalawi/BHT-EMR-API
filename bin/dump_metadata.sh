98eb00de61bd02a41449d52fc4e4ddd904aa3cd8
Walter Kaunda <walterkaunda@live.co.uk>
2020-05-11 16:26:11 +0200

    Added script for dumping metadata

---
+++ bin/dump_metadata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_PATH="`dirname $BIN_PATH`"
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['host']"`
+
+METADATA_FILE=${ROOT_PATH}/db/sql/openmrs_metadata_1_7.sql
+
+mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $DATABASE $(cat $METADATA_FILE | awk '/CREATE TABLE .*/ { gsub(/`/, "", $3); print $3 }')
