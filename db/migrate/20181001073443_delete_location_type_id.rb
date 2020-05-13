394f01e39c560269d27ecee4956f443006099aeb
mwatha <mwathabwanali@gmail.com>
2020-05-13 11:45:25 +0200

    adding new locations; * Amecca Chilaweni Health Centre * Kameza Macro * Umunthu Foundation Clinic

--- db/migrate/20181001073443_delete_location_type_id.rb
+++ db/migrate/20181001073443_delete_location_type_id.rb
@@ -1,6 +1,6 @@
 class DeleteLocationTypeId < ActiveRecord::Migration[5.2]
   def change
     execute 'ALTER TABLE location MODIFY COLUMN date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()'
-    remove_column :location, :location_type_id
+    execute 'ALTER TABLE location DROP COLUMN location_type_id' rescue 'Does not exisits'
   end
 end
