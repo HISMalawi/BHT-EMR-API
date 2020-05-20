d6695acf0fd219b5297a88d735837b9eb2047169
Walter Kaunda <walterkaunda@live.co.uk>
2020-05-20 10:36:33 +0200

    Added regimens 14 and 15 plus adjusted DTG weight bands to match 2020 MoH HIV clinical guidelines.

---
+++ bin/dump_moh_regimens.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_PATH="`dirname $BIN_PATH`"
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['host']"`
+
+
+mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $DATABASE moh_regimens moh_regimen_doses moh_regimen_ingredient \
+          moh_regimen_ingredient_starter_packs moh_regimen_lookup moh_regimen_ingredient_tb_treatment alternative_drug_names
