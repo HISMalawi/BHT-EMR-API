c5a4cba8837d8a1cdb22cefecec7aa81b90ec11a
mwatha <mwathabwanali@gmail.com>
2020-06-19 09:43:18 +0200

    adding missed appointment report to eM

--- app/services/program_appointment_service.rb
+++ app/services/program_appointment_service.rb
@@ -3,17 +3,19 @@ class ProgramAppointmentService
 
   def self.booked_appointments(program_id, date)
     clients = ActiveRecord::Base.connection.select_all("SELECT
-    i.identifier, p.birthdate, p.gender, n.given_name,
+    i2.identifier arv_number, i.identifier, p.birthdate, p.gender, n.given_name,
     n.family_name, obs.person_id, p.birthdate_estimated
     FROM obs
     INNER JOIN encounter e ON e.encounter_id = obs.encounter_id
     AND e.voided = 0 AND obs.voided = 0 AND e.program_id = #{program_id}
     AND e.encounter_type = #{encounter_type('APPOINTMENT').id}
-    RIGHT JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
-    RIGHT JOIN person_address a ON a.person_id = e.patient_id AND a.voided = 0
-    RIGHT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
-    RIGHT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
+    LEFT JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
+    LEFT JOIN person_address a ON a.person_id = e.patient_id AND a.voided = 0
+    LEFT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
+    LEFT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
     AND i.identifier_type IN(2,3)
+    LEFT JOIN patient_identifier i2 ON i2.patient_id = e.patient_id AND i2.voided = 0
+    AND i2.identifier_type IN(4)
     WHERE obs.concept_id = #{concept('Appointment date').concept_id}
     AND value_datetime BETWEEN '#{date.strftime('%Y-%m-%d 00:00:00')}'
     AND '#{date.strftime('%Y-%m-%d 23:59:59')}'
@@ -31,7 +33,8 @@ class ProgramAppointmentService
       clients_formatted << {
         given_name: c['given_name'], family_name: c['family_name'],
         birthdate: c['birthdate'], gender: c['gender'], person_id: c['person_id'],
-        npid: c['identifier'], birthdate_estimated: c['birthdate_estimated']
+        npid: c['identifier'], birthdate_estimated: c['birthdate_estimated'],
+        arv_number: c['arv_number']
       }
     end
 
@@ -52,10 +55,10 @@ class ProgramAppointmentService
     INNER JOIN encounter e ON e.encounter_id = obs.encounter_id
     AND e.voided = 0 AND obs.voided = 0 AND e.program_id = #{program_id}
     AND e.encounter_type = #{encounter_type('APPOINTMENT').id}
-    RIGHT JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
-    RIGHT JOIN person_address a ON a.person_id = e.patient_id AND a.voided = 0
-    RIGHT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
-    RIGHT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
+    LEFT JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
+    LEFT JOIN person_address a ON a.person_id = e.patient_id AND a.voided = 0
+    LEFT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
+    LEFT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
     AND i.identifier_type = #{patient_identifier_type('ARV Number').patient_identifier_type_id}
     WHERE obs.concept_id = #{concept('Appointment date').concept_id}
     AND value_datetime BETWEEN '#{date.strftime('%Y-%m-%d 00:00:00')}'
