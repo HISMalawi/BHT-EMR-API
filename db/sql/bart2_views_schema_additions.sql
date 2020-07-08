00db3206bc2e81a16a990beeebb40dca595d6bb3
mwatha <mwathabwanali@gmail.com>
2020-07-08 16:17:05 +0200

    Fixing PEPFAR default cal function

--- db/sql/bart2_views_schema_additions.sql
+++ db/sql/bart2_views_schema_additions.sql
@@ -2211,63 +2211,67 @@ DROP FUNCTION IF EXISTS `current_pepfar_defaulter`;
 
 CREATE  FUNCTION `current_pepfar_defaulter`(my_patient_id INT, my_end_date DATETIME) RETURNS int(1)
 BEGIN
-	DECLARE done INT DEFAULT FALSE;
-	DECLARE my_start_date, my_expiry_date, my_obs_datetime DATETIME;
-	DECLARE my_daily_dose, my_quantity, my_pill_count, my_total_text, my_total_numeric DECIMAL;
-	DECLARE my_drug_id, flag INT;
+DECLARE done INT DEFAULT FALSE;
+  DECLARE my_start_date, my_expiry_date, my_obs_datetime DATETIME;
+  DECLARE my_daily_dose, my_quantity, my_pill_count, my_total_text, my_total_numeric DECIMAL;
+  DECLARE my_drug_id, flag INT;
 
-	DECLARE cur1 CURSOR FOR SELECT d.drug_inventory_id, o.start_date, d.equivalent_daily_dose daily_dose, d.quantity, o.start_date FROM drug_order d
-		INNER JOIN arv_drug ad ON d.drug_inventory_id = ad.drug_id
-		INNER JOIN orders o ON d.order_id = o.order_id
-			AND d.quantity > 0
-			AND o.voided = 0
-			AND o.start_date <= my_end_date
-			AND o.patient_id = my_patient_id;
+  DECLARE cur1 CURSOR FOR SELECT d.drug_inventory_id, o.start_date, d.equivalent_daily_dose daily_dose, d.quantity, o.start_date FROM drug_order d
+    INNER JOIN arv_drug ad ON d.drug_inventory_id = ad.drug_id
+    INNER JOIN orders o ON d.order_id = o.order_id
+      AND d.quantity > 0
+      AND o.voided = 0
+      AND o.start_date <= my_end_date
+      AND o.patient_id = my_patient_id;
 
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-	SELECT MAX(o.start_date) INTO @obs_datetime FROM drug_order d
-		INNER JOIN arv_drug ad ON d.drug_inventory_id = ad.drug_id
-		INNER JOIN orders o ON d.order_id = o.order_id
-			AND d.quantity > 0
-			AND o.voided = 0
-			AND o.start_date <= my_end_date
-			AND o.patient_id = my_patient_id
-		GROUP BY o.patient_id;
+  SELECT MAX(o.start_date) INTO @obs_datetime FROM drug_order d
+    INNER JOIN arv_drug ad ON d.drug_inventory_id = ad.drug_id
+    INNER JOIN orders o ON d.order_id = o.order_id
+      AND d.quantity > 0
+      AND o.voided = 0
+      AND o.start_date <= my_end_date
+      AND o.patient_id = my_patient_id
+    GROUP BY o.patient_id;
 
-	OPEN cur1;
+  OPEN cur1;
 
-	SET flag = 0;
+  SET flag = 0;
 
-	read_loop: LOOP
-		FETCH cur1 INTO my_drug_id, my_start_date, my_daily_dose, my_quantity, my_obs_datetime;
+  read_loop: LOOP
+    FETCH cur1 INTO my_drug_id, my_start_date, my_daily_dose, my_quantity, my_obs_datetime;
 
-		IF done THEN
-			CLOSE cur1;
-			LEAVE read_loop;
-		END IF;
+    IF done THEN
+      CLOSE cur1;
+      LEAVE read_loop;
+    END IF;
 
-		IF DATE(my_obs_datetime) = DATE(@obs_datetime) THEN
+    IF DATE(my_obs_datetime) = DATE(@obs_datetime) THEN
+
+      IF my_daily_dose = 0 OR LENGTH(my_daily_dose) < 1 OR my_daily_dose IS NULL THEN
+        SET my_daily_dose = 1;
+      END IF;
 
             SET my_pill_count = drug_pill_count(my_patient_id, my_drug_id, my_obs_datetime);
 
             SET @expiry_date = ADDDATE(DATE_SUB(my_start_date, INTERVAL 2 DAY), ((my_quantity + my_pill_count)/my_daily_dose));
 
-			IF my_expiry_date IS NULL THEN
-				SET my_expiry_date = @expiry_date;
-			END IF;
+      IF my_expiry_date IS NULL THEN
+        SET my_expiry_date = @expiry_date;
+      END IF;
 
-			IF @expiry_date < my_expiry_date THEN
-				SET my_expiry_date = @expiry_date;
+      IF @expiry_date < my_expiry_date THEN
+        SET my_expiry_date = @expiry_date;
             END IF;
         END IF;
     END LOOP;
 
-    IF DATEDIFF(my_end_date, my_expiry_date) > 30 THEN
+    IF TIMESTAMPDIFF(day, my_expiry_date, my_end_date) > 30 THEN
         SET flag = 1;
     END IF;
 
-	RETURN flag;
+  RETURN flag;
 END;
 
 DROP FUNCTION IF EXISTS `current_pepfar_defaulter_date`;
