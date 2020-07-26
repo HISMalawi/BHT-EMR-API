938845afe867c8cf96ad503ea36d48b8226c84fd
mwatha <mwathabwanali@gmail.com>
2020-07-26 20:36:52 +0200

    merging

--- app/services/program_appointment_service.rb
+++ app/services/program_appointment_service.rb
@@ -49,8 +49,10 @@ class ProgramAppointmentService
     end
 
     clients = ActiveRecord::Base.connection.select_all("SELECT
-    i.identifier, p.birthdate, p.gender, n.given_name,
-    n.family_name, obs.person_id, p.birthdate_estimated
+      i.identifier, p.birthdate, p.gender, n.given_name,
+      n.family_name, obs.person_id, p.birthdate_estimated,att.value cell_phone,
+      a.state_province district,
+      a.township_division village, a.city_village land_mark
     FROM obs
     INNER JOIN encounter e ON e.encounter_id = obs.encounter_id
     AND e.voided = 0 AND obs.voided = 0 AND e.program_id = #{program_id}
@@ -60,6 +62,9 @@ class ProgramAppointmentService
     LEFT JOIN person_name n ON n.person_id = e.patient_id AND n.voided = 0
     LEFT JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.voided = 0
     AND i.identifier_type = #{patient_identifier_type('ARV Number').patient_identifier_type_id}
+    LEFT JOIN person_attribute att ON p.person_id = att.person_id AND att.voided = 0
+    AND att.person_attribute_type_id = 12
+
     WHERE obs.concept_id = #{concept('Appointment date').concept_id}
     AND value_datetime BETWEEN '#{date.strftime('%Y-%m-%d 00:00:00')}'
     AND '#{date.strftime('%Y-%m-%d 23:59:59')}'
@@ -78,7 +83,9 @@ class ProgramAppointmentService
       clients_formatted << {
         given_name: c['given_name'], family_name: c['family_name'],
         birthdate: c['birthdate'], gender: c['gender'], person_id: c['person_id'],
-        arv_number: c['identifier'], birthdate_estimated: c['birthdate_estimated']
+        arv_number: c['identifier'], birthdate_estimated: c['birthdate_estimated'],
+        cell_phone: c["cell_phone"], land_mark: c["land_mark"],
+        village: c["village"], district: c["district"]
       }
     end
 
