99d5a3053332d65fdda9f3eee715a22efe52a7ad
Walter Kaunda <walterkaunda@live.co.uk>
2020-07-29 16:12:40 +0200

    ART: Added new MoH regimen tables to moh_regimens dump script.

--- bin/dump_moh_regimens.sh
+++ bin/dump_moh_regimens.sh
@@ -10,4 +10,5 @@ HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['de
 
 
 mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $DATABASE moh_regimens moh_regimen_doses moh_regimen_ingredient \
-          moh_regimen_ingredient_starter_packs moh_regimen_lookup moh_regimen_ingredient_tb_treatment alternative_drug_names
+          moh_regimen_ingredient_starter_packs moh_regimen_lookup moh_regimen_ingredient_tb_treatment alternative_drug_names \
+          moh_regimen_combination moh_regimen_combination_drug moh_regimen_name
