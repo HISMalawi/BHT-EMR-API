92d9396c65122542f04d95a2112b7b86a7ab94cd
mwatha <mwathabwanali@gmail.com>
2020-09-10 12:02:42 +0200

    making sure all side effects display  on  dashboard if pres

--- app/services/art_service/patient_side_effect.rb
+++ app/services/art_service/patient_side_effect.rb
@@ -13,17 +13,18 @@ module ARTService
       etype = encounter_type 'HIV CLINIC CONSULTATION'
       yes_concept = concept 'Yes'
       mw_side_effect_concept = concept 'Malawi ART side effects'
+      other_side_effect_concept = concept 'Other side effect'
       results = {}
 
       data = Observation.joins('INNER JOIN obs t2 ON obs.obs_id = t2.obs_group_id
-        INNER JOIN concept_name n ON n.concept_id = t2.concept_id 
+        INNER JOIN concept_name n ON n.concept_id = t2.concept_id
         INNER JOIN encounter e ON e.encounter_id = obs.encounter_id').\
-        where('obs.concept_id = ? AND obs.person_id = ? 
-        AND obs.obs_datetime <= ? AND t2.value_coded = ? 
-        AND e.encounter_type = ?', mw_side_effect_concept.concept_id,
+        where('obs.concept_id IN(?) AND obs.person_id = ?
+        AND obs.obs_datetime <= ? AND (t2.value_coded = ? OR t2.value_text IS NOT  NULL)
+        AND e.encounter_type = ?', [mw_side_effect_concept.concept_id, other_side_effect_concept],
         patient.id, @date.strftime('%Y-%m-%d 23:59:59'), yes_concept.concept_id, etype.id).\
         group('n.concept_id, date(obs.obs_datetime)').\
-        select('n.name, t2.concept_id, t2.obs_datetime')
+        select('n.name, t2.concept_id,  t2.value_text, t2.obs_datetime')
 
       data.select do |r|
         obs_date    = r['obs_datetime'].to_date
@@ -33,8 +34,12 @@ module ARTService
           name: nil , drug_induced: false, drug: 'N/A'
         } if results[obs_date][concept_id].blank?
 
-        results[obs_date][concept_id][:name] = r['name']
-        drug_induced?(results[obs_date][concept_id], concept_id, obs_date)
+        if r['name'].match(/Other/i)
+          results[obs_date][concept_id][:name] = r['value_text']
+        else
+          results[obs_date][concept_id][:name] = r['name']
+          drug_induced?(results[obs_date][concept_id], concept_id, obs_date)
+        end
       end
 
       return results
@@ -47,10 +52,10 @@ module ARTService
       contraindication     = concept 'Contraindications'
 
       data = Observation.where('concept_id = ? AND person_id = ?
-        AND value_coded = ? AND DATE(obs_datetime) = ?', 
-        drug_induced_concept.concept_id, @patient.id, 
+        AND value_coded = ? AND DATE(obs_datetime) = ?',
+        drug_induced_concept.concept_id, @patient.id,
         concept_id, obs_date).last
-      
+
       unless data.blank?
         drug_name = Drug.find(data.value_drug).name rescue data.value_text
         e[:drug_induced] = true
