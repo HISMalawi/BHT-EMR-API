8730337d30f86e4397d47a8a53ab8450ed009281
Walter Kaunda <walterkaunda@live.co.uk>
2021-01-13 10:58:18 +0200

    [Lab] Added Lab extension module

---
+++ config/initializers/rswag-ui.rb
@@ -0,0 +1,17 @@
+require 'rswag/ui'
+
+Rswag::Ui.configure do |c|
+
+  # List the Swagger endpoints that you want to be documented through the swagger-ui
+  # The first parameter is the path (absolute or relative to the UI host) to the corresponding
+  # endpoint and the second is a title that will be displayed in the document selector
+  # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
+  # then the list below should correspond to the relative paths for those endpoints
+
+  c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API Core v1 docs'
+  c.swagger_endpoint '/api-docs/lab/v1/swagger.yaml', 'Lab API v1 docs'
+
+  # Add Basic Auth in case your API is private
+  # c.basic_auth_enabled = true
+  # c.basic_auth_credentials 'username', 'password'
+end
