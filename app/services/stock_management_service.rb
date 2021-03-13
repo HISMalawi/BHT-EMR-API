e628c8980a9c78e3aaa014794b0116feda6d88d8
Walter Kaunda <walterkaunda@live.co.uk>
2021-03-13 22:09:01 +0200

    [ART Stock] Fixed crash on edit batch item

--- app/services/stock_management_service.rb
+++ app/services/stock_management_service.rb
@@ -118,15 +118,16 @@ class StockManagementService
   def edit_batch_item(batch_item_id, params)
     ActiveRecord::Base.transaction do
       item = PharmacyBatchItem.find(batch_item_id)
+      reason = params.delete(:reason)
 
       if params[:current_quantity]
         diff = params[:current_quantity].to_f - item.current_quantity
-        commit_transaction(item, STOCK_EDIT, diff, Date.today, update_item: false, transaction_reason: params[:reason])
+        commit_transaction(item, STOCK_EDIT, diff, Date.today, update_item: false, transaction_reason: reason)
       end
 
       if params[:delivered_quantity]
         diff = params[:delivered_quantity].to_f - item.delivered_quantity
-        commit_transaction(item, STOCK_EDIT, diff, Date.today, update_item: true, transaction_reason: params[:reason])
+        commit_transaction(item, STOCK_EDIT, diff, Date.today, update_item: true, transaction_reason: reason)
       end
 
       unless item.update(params)
