3350c6bbf568b2ebcc24b9eb0f90bce8685cb816
Walter Kaunda <walterkaunda@live.co.uk>
2021-03-15 10:40:12 +0200

    [ART Stock] Added full creator information to pharmacy_batch_item

--- app/models/pharmacy_batch_item.rb
+++ app/models/pharmacy_batch_item.rb
@@ -26,21 +26,6 @@ class PharmacyBatchItem < VoidableRecord
     }
   end
 
-  def creator_id
-    method(:creator).super_method.call
-  end
-
-  def creator
-    user = User.unscoped.find_by(user_id: creator_id)
-    person_name = PersonName.where(person_id: creator_id).first
-
-    {
-      username: user&.username,
-      given_name: person_name&.given_name,
-      family_name: person_name&.family_name
-    }
-  end
-
   def drug_name
     drug.name
   end
