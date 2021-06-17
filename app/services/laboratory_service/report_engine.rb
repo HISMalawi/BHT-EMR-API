4028acef67470fab45ee0c160107fa185742548d
mwatha <mwathabwanali@gmail.com>
2021-06-17 10:00:59 +0200

    adding end-point to pull orderd/drawn tests

--- app/services/laboratory_service/report_engine.rb
+++ app/services/laboratory_service/report_engine.rb
@@ -27,5 +27,9 @@ module LaboratoryService
     def test_results(start_date, end_date)
       REPORTS['SAMPLES_DRAWN'].new(start_date: start_date, end_date: end_date).test_results
     end
+
+    def orders_made(start_date, end_date, status)
+      REPORTS['SAMPLES_DRAWN'].new(start_date: start_date, end_date: end_date).orders_made(status)
+    end
   end
 end
