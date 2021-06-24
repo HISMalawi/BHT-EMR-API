bd94e37d35c2d4952407e03fd76ecb9acf41fb2f
Walter Kaunda <walterkaunda@live.co.uk>
2021-06-24 13:55:13 +0200

    ART: Updated viral load results report to follow MoH ART Clinical guidelines.

--- app/services/art_service/reports/viral_load_results.rb
+++ app/services/art_service/reports/viral_load_results.rb
@@ -3,16 +3,21 @@
 module ARTService
   module Reports
     ##
-    # Returns all ART patients whose most recent viral load is high
-    # in a reporting period.
+    # Pulls patient's whose most recent viral load result in selected reporting period
+    # is in specified viral load classification.
     #
-    # High viral load is defined as a viral load with a numeric value
-    # equal to or exceeding 1000.
-    class HighViralLoadPatients
-      def initialize(start_date:, end_date:, range: 'high', **_kwargs)
+    # Classification are passed as parameter range and are limited to the following
+    # values:
+    #   - suppressed
+    #   - low-level-viraemia
+    #   - viraemia-1000
+    #
+    # The classifications above follow the ART guidelines 2018 Addendum.
+    class ViralLoadResults
+      def initialize(start_date:, end_date: nil, range: nil, **_kwargs)
         @start_date = start_date
         @end_date = end_date
-        @range = range || 'high'
+        @range = range || 'viraemia-1000+'
       end
 
       def find_report
@@ -26,11 +31,16 @@ module ARTService
                  cohort_disaggregated_age_group(person.birthdate, #{end_date}) AS age_group,
                  person.gender AS gender,
                  orders.start_date AS order_date,
+                 specimen_type.name AS specimen,
                  COALESCE(orders.discontinued_date, orders.start_date) AS specimen_drawn_date,
                  test_results_obs.obs_datetime AS result_date,
                  COALESCE(test_result_measure_obs.value_modifier, '=') AS result_modifier,
                  COALESCE(test_result_measure_obs.value_numeric, test_result_measure_obs.value_text) AS result
           FROM orders
+          INNER JOIN concept_name AS specimen_type
+            ON specimen_type.concept_id = orders.concept_id
+            AND specimen_type.name IN ('Blood', 'DBS (Free drop to DBS card)', 'DBS (Using capillary tube)')
+            AND specimen_type.voided = 0
           LEFT JOIN patient_identifier
             ON patient_identifier.patient_id = orders.patient_id
             AND patient_identifier.voided = 0
@@ -93,33 +103,71 @@ module ARTService
               SELECT concept_id FROM concept_name INNER JOIN concept USING (concept_id)
               WHERE concept_name.name = 'Viral load' AND concept.retired = 0 AND concept_name.voided = 0
             )
-            AND (#{query_range})
+            AND (test_result_measure_obs.value_numeric IS NOT NULL
+                 OR test_result_measure_obs.value_text IS NOT NULL)
             AND test_result_measure_obs.voided = 0
-          WHERE orders.concept_id IN (
-              SELECT concept_id FROM concept_name INNER JOIN concept USING (concept_id)
-              WHERE concept_name.name = 'Blood' AND concept.retired = 0 AND concept_name.voided = 0
-            )
-            AND orders.order_type_id IN (SELECT order_type_id FROM order_type WHERE name = 'Lab' AND retired = 0)
+            AND (#{query_range})
+          WHERE orders.order_type_id IN (SELECT order_type_id FROM order_type WHERE name = 'Lab' AND retired = 0)
             AND orders.voided = 0
           GROUP BY orders.patient_id
         SQL
       end
 
-      def query_range
+      def specimen_types
+        Concept.joins(:concept_names)
+               .merge(ConceptName.where(name: ['Blood', ]))
+               .select(:concept_id)
+               .to_sql
+      end
+
+      def dbs_query_range
         case @range.downcase
-        when 'high' then <<~SQL
-          ((test_result_measure_obs.value_modifier IN ('=', '>') AND test_result_measure_obs.value_numeric >= 1000)
-           OR (test_result_measure_obs.value_modifier = '>' AND test_result_measure_obs.value_text = 'LDL'))
+        when 'suppressed' then <<~SQL
+
         SQL
-        when 'low' then <<~SQL
-          (test_result_measure_obs.value_modifier IN ('=', '<')
-           AND (test_result_measure_obs.value_numeric <= 50 OR test_result_measure_obs.value_text = 'LDL'))
+        when 'low-level-viremia' then <<~SQL
+
         SQL
-        when 'intermediate' then <<~SQL
-          (test_result_measure_obs.value_numeric > 50 AND test_result_measure_obs.value_numeric < 1000)
+        when 'viraemia-1000+' then <<~SQL
+          (test_result_measure_obs.value_numeric >= 1000)
         SQL
         else
-          raise InvalidParameterError, "Invalid viral load range specified: #{@range}"
+          raise InvalidParameterError, "Invalid viral load range: #{@range}"
+        end
+      end
+
+      def query_range
+        case @range.downcase
+        when 'suppressed' then <<~SQL
+          (/* Plasma/Blood */
+           (specimen_type.name IN ('Blood', 'Plasma')
+            AND ((test_result_measure_obs.value_modifier = '<'
+                  AND (test_result_measure_obs.value_text LIKE 'LDL'
+                       OR test_result_measure_obs.value_numeric IN (20, 40, 150)))
+                 OR (test_result_measure_obs.value_numeric >= 20
+                     AND test_result_measure_obs.value_numeric < 200)))
+          /* DBS */
+          OR (specimen_type.name IN ('DBS (Free drop to DBS card)', 'DBS (Using capillary tube)')
+              AND (test_result_measure_obs.value_modifier = '<'
+                   AND test_result_measure_obs.value_text LIKE 'LDL')))
+        SQL
+        when 'low-level-viraemia' then <<~SQL
+          (/* Plasma/Blood */
+           (specimen_type.name IN ('Blood', 'Plasma')
+            AND (test_result_measure_obs.value_numeric >= 200
+                 AND test_result_measure_obs.value_numeric < 1000))
+           /* DBS */
+           OR (specimen_type.name IN ('DBS (Free drop to DBS card)', 'DBS (Using capillary tube)')
+               AND (test_result_measure_obs.value_modifier = '<'
+                    AND test_result_measure_obs.value_numeric IN (400, 550, 839))
+               OR (test_result_measure_obs.value_numeric >= 400
+                   AND test_result_measure_obs.value_numeric < 1000)))
+        SQL
+        when 'viraemia-1000' then <<~SQL
+          (test_result_measure_obs.value_numeric >= 1000)
+        SQL
+        else
+          raise InvalidParameterError, "Invalid viral load range: #{@range}"
         end
       end
     end
