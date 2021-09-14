2220a024c54a8ee041fc7670ff79e022207c79c2
Walter Kaunda <walterkaunda@live.co.uk>
2021-09-14 13:33:27 +0200

    Fixed tx_curr report query
    Query was initially pulling everyone who has ever been on ART not necessarily
    those who are currently on ART.

--- app/services/art_service/reports/patients_alive_and_on_treatment.rb
+++ app/services/art_service/reports/patients_alive_and_on_treatment.rb
@@ -17,11 +17,31 @@ module ARTService
       end
 
       def query
-        PatientState.joins(:patient_program)
-                    .where(patient_program: { program_id: ARTService::Constants::PROGRAM_ID },
-                           state: ARTService::Constants::States::ON_ANTIRETROVIRALS)
-                    .where('start_date <= ? AND (end_date >= ? OR end_date IS NULL)', start_date, end_date)
-                    .select(:patient_id)
+        Patient.find_by_sql <<~SQL
+          SELECT patient_program.patient_id
+          FROM patient_program
+          INNER JOIN patient_state
+            ON patient_state.patient_program_id = patient_program.patient_program_id
+            AND patient_state.state = 7
+            AND patient_state.voided = 0
+          INNER JOIN (
+            SELECT patient_program.patient_program_id,
+                   MAX(patient_state.start_date) AS start_date
+            FROM patient_program
+            INNER JOIN program
+              ON program.program_id = patient_program.program_id
+              AND program.name = 'HIV Program'
+              AND program.retired = 0
+            INNER JOIN patient_state
+              ON patient_state.patient_program_id = patient_program.patient_program_id
+              AND patient_state.voided = 0
+              AND patient_state.start_date < DATE(#{ActiveRecord::Base.connection.quote(end_date)}) + INTERVAL 1 DAY
+            WHERE patient_program.voided = 0
+            GROUP BY patient_state.patient_program_id
+          ) AS latest_patient_state
+            ON latest_patient_state.patient_program_id = patient_program.patient_program_id
+            AND latest_patient_state.start_date = patient_state.start_date
+        SQL
       end
     end
   end
