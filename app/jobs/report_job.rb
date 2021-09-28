f8cee9167fc46a4fdf67ed97499cc036e038e8d5
Walter Kaunda <walterkaunda@live.co.uk>
2021-09-28 13:30:16 +0200

    [ART] Refactored cohort reporting logic.
    - Removed locking from the reporting service and moved it to the report
      itself. This makes report locking something that must explicitly done on
      the report itself (look at SynchronisationUtils for helpers)
    - Fixed a bug that would prevent the report from running if a lock file left
      behind by a previously crashed process exists

--- app/jobs/report_job.rb
+++ app/jobs/report_job.rb
@@ -6,14 +6,10 @@ class ReportJob < ApplicationJob
   def perform(clazzname, kwargs)
     logger.debug("Running report job #{clazzname}(#{kwargs})")
 
-    lock = kwargs.delete(:lock)
-
     User.current = User.find(kwargs.delete(:user))
 
     clazz = clazzname.constantize
     report_engine = clazz.new
     report_engine.generate_report(**kwargs)
-  ensure
-    ReportService.release_report_lock(lock)
   end
 end
--- app/utils/concurrency_utils.rb
+++ app/utils/concurrency_utils.rb
@@ -3,6 +3,32 @@
 module ConcurrencyUtils
   LOCK_FILES_DIR_PATH = Rails.root.join('tmp', 'locks')
 
+  ##
+  # Acquire a lock and run the given block of code.
+  #
+  # The locking mechanism uses files to allow for blocking across processes.
+  # This is useful for example in situations where you only want to run one
+  # instance of a report.
+  #
+  # Parameters:
+  #   lock_file_path: A relative path to the lock file (allows for namespacing your locks, eg art_service/regimens.lock)
+  #   blocking: If lock can not be acquired wait else throw an error (defaults to true)
+  #
+  # Raises:
+  #   FailedToAcquireLock: When lock couldn't be acquired and blocking is set to false
+  #
+  # Usage:
+  #   class Someclass
+  #     include ModelUtils
+  #
+  #     def do_something
+  #       with_lock('mylockfile.lock') do
+  #         # Run some task requiring exclusive access to some resource
+  #       end
+  #     end
+  #   end
+  #
+  #   SomeClass.new.do_something
   def with_lock(lock_file_path, blocking: true)
     path = LOCK_FILES_DIR_PATH.join(lock_file_path)
 
@@ -14,7 +40,7 @@ module ConcurrencyUtils
     File.open(path, 'w') do |lock_file|
       Rails.logger.debug("Attempting to acquire lock: #{lock_file_path}")
       locking_mode = blocking ? File::LOCK_EX : File::LOCK_NB | File::LOCK_EX
-      if blocking && !lock_file.flock(locking_mode)
+      unless blocking || lock_file.flock(locking_mode)
         raise FailedToAcquireLock, "Lock #{lock_file_path} is locked by another process"
       end
 
