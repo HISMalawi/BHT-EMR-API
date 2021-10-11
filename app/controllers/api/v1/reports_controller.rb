71c6e93e76f1130c5507bdea8c3683c244e50886
dominickasanga <dominickasanga@gmail.com>
2021-10-11 18:13:55 +0200

    removed reports def

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -33,20 +33,6 @@ class Api::V1::ReportsController < ApplicationController
     render json: stats
   end
 
-  def diagnosis_ls
-    start_date, end_date = params.require %i[start_date end_date]
-    stats = service.diagnosis_ls(start_date, end_date)
-
-    render json: stats
-  end
-
-  def diagnosis
-    start_date, end_date,diagnosis_name = params.require %i[start_date end_date diagnosis_name]
-    stats = service.diagnosis(start_date, end_date,diagnosis_name)
-
-    render json: stats
-  end
-
   def registration
     start_date, end_date = params.require %i[start_date end_date]
     stats = service.registration(start_date, end_date)
