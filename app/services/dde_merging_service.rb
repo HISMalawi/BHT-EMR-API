0d7bb6f0240a46fb9f4677bc3b30700963e35d3f
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-10-27 12:02:32 +0200

    Added voiding of all National IDs not longer keeping legacy IDS

--- app/services/dde_merging_service.rb
+++ app/services/dde_merging_service.rb
@@ -72,10 +72,11 @@ class DDEMergingService
     doc_id_type = patient_identifier_type('DDE person document id')
 
     local_patient.patient_identifiers.where(type: [national_id_type, doc_id_type]).each do |identifier|
-      if identifier.identifier_type == national_id_type.id && identifier.identifier.match?(/^\s*P\d{12}\s*$/i)
-        # We have a v3 NPID that should get demoted to legacy national id
-        create_local_patient_identifier(local_patient, identifier.identifier, 'Old Identification Number')
-      end
+      # We are now voiding all ids
+      # if identifier.identifier_type == national_id_type.id && identifier.identifier.match?(/^\s*P\d{12}\s*$/i)
+      #   # We have a v3 NPID that should get demoted to legacy national id
+      #   create_local_patient_identifier(local_patient, identifier.identifier, 'Old Identification Number')
+      # end
 
       identifier.void("Assigned new id: #{remote_patient['doc_id']}")
     end
