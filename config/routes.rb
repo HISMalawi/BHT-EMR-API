134207267b4e7883d1bf6b4d8aebb8edce81251f
mwatha <mwathabwanali@gmail.com>
2021-11-03 17:47:16 +0200

    adding dispatch: 50%

--- config/routes.rb
+++ config/routes.rb
@@ -338,5 +338,5 @@ Rails.application.routes.draw do
 
   get '/api/v1/screened_for_cxca', to: 'api/v1/reports#cxca_reports'
   get '/api/v1/dispatch_order/:order_id', to: 'api/v1/dispatch_orders#show'
-  post '/api/v1/dispatch_order/:order_id', to: 'api/v1/dispatch_orders#create'
+  post '/api/v1/dispatch_order', to: 'api/v1/dispatch_orders#create'
 end
