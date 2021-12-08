516d8aa81799e019088a7d7229c3e85f3751fa47
mwatha <mwathabwanali@gmail.com>
2021-12-08 10:21:02 +0200

    adding new age groups

--- app/services/art_service/reports/tpt_newly_initiated.rb
+++ app/services/art_service/reports/tpt_newly_initiated.rb
@@ -31,21 +31,18 @@ module ARTService
 
       AGE_GROUPS = [
         'Unknown',
-        '0-5 months',
-        '6-11 months',
-        '12-23 months',
-        '2-4 years',
-        '5-9 years',
-        '10-14 years',
-        '15-17 years',
-        '18-19 years',
+        '<1 year',
+        '1-4 years', '5-9 years',
+        '10-14 years', '15-19 years',
         '20-24 years',
-        '25-29 years',
-        '30-34 years',
-        '35-39 years',
-        '40-44 years',
-        '45-49 years',
-        '50 plus years'
+        '25-29 years', '30-34 years',
+        '35-39 years', '40-44 years',
+        '45-49 years', '50-54 years',
+        '55-59 years', '60-64 years',
+        '65-69 years', '70-74 years',
+        '75-79 years', '80-84 years',
+        '85-89 years',
+        '90 plus years'
       ].freeze
 
       def init_report
