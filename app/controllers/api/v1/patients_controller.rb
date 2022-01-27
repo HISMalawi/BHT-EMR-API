d4aaa59ad7699d5b2c714df8322885df0de2ae9c
Roy Chanunkha <roychanunkha@gmail.com>
2022-01-27 09:45:03 +0200

    added a condition that check whether to include defaulter dates when fetching visit dates

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -87,7 +87,7 @@ class Api::V1::PatientsController < ApplicationController
 
   def visits
     program = params[:program_id] ? Program.find(params[:program_id]) : nil
-    render json: service.find_patient_visit_dates(patient, program)
+    render json: service.find_patient_visit_dates(patient, program, params[:include_defaulter_dates])
   end
 
   def find_median_weight_and_height
