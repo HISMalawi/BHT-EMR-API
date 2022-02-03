9a52b26b99ad14e8150953d9f13cde452e4a65c3
Roy Chanunkha <roychanunkha@gmail.com>
2022-02-03 16:24:09 +0200

    changes to date interval when calculating when drugs will run out moh

--- db/sql/bart2_views_schema_additions.sql
+++ db/sql/bart2_views_schema_additions.sql
@@ -1766,7 +1766,7 @@ DECLARE done INT DEFAULT FALSE;
 
             SET my_pill_count = drug_pill_count(my_patient_id, my_drug_id, my_obs_datetime);
 
-            SET @expiry_date = ADDDATE(DATE_SUB(my_start_date, INTERVAL 2 DAY), ((my_quantity + my_pill_count)/my_daily_dose));
+            SET @expiry_date = ADDDATE(DATE_SUB(my_start_date, INTERVAL 1 DAY), ((my_quantity + my_pill_count)/my_daily_dose));
 
       IF my_expiry_date IS NULL THEN
         SET my_expiry_date = @expiry_date;
