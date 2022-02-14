70507f7203bccc7fab701270147a19a01de26b64
Roy Chanunkha <roychanunkha@gmail.com>
2022-02-14 16:56:51 +0200

    included logic for creating patient initial state after enrollment into program

--- app/services/patient_program_service.rb
+++ app/services/patient_program_service.rb
@@ -6,14 +6,30 @@ class PatientProgramService
     date_enrolled ||= Time.now
     location ||= Location.current
     user ||= User.current
+    return if program.blank? || patient.blank?
 
-    patient_program = find_program(patient: patient, program: program)
+    patient_program = find_patient_program(patient: patient, program: program)
     return patient_program unless patient_program.blank?
 
-    PatientProgram.create(patient: patient, program: program, date_enrolled: date_enrolled, location: location, creator: user.id)
+    ActiveRecord::Base.transaction do
+      patient_program = PatientProgram.create(patient: patient, program: program, date_enrolled: date_enrolled,
+                                              location: location, creator: user.id)
+      initial_state = initial_program_state(program)
+      unless initial_state.blank?
+        PatientState.create(patient_program_id: patient_program.id, start_date: date_enrolled,
+                            state: initial_state.id, creator: user.id)
+      end
+    end
+
+    patient_program
   end
 
-  def find_program(patient:, program:)
+  def find_patient_program(patient:, program:)
     PatientProgram.where(patient: patient, program: program).first
+  end
+
+  def initial_program_state(program)
+    ProgramWorkflowState.joins(:program_workflow).where(initial: 1, terminal: 0,
+                                                        program_workflow: { program_id: program.id }).first
   end
 end
