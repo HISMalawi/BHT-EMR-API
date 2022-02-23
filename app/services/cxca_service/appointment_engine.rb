bff73bf150255be357d2e2b6b33762a2b4faed1d
mwatha <mwathabwanali@gmail.com>
2022-02-23 13:46:39 +0200

    Editing workflow: making sure we book +3 years for negative clients

--- app/services/cxca_service/appointment_engine.rb
+++ app/services/cxca_service/appointment_engine.rb
@@ -15,14 +15,40 @@ module CXCAService
     end
 
     def next_appointment_date
-      concept = ConceptName.find_by_name 'Directly observed treatment option'
-      ob = Observation.where("concept_id = ? AND person_id = ? AND DATE(obs_datetime)=?",
-       concept.concept_id, @patient.patient_id, @ref_date.to_date)
-      ref_period = ob.blank? ? 1.year : 1.week
+      concept = ConceptName.find_by_name 'HIV status'
+      negative_concept = ConceptName.find_by_name 'Negative'
+
+      ob = Observation.where("concept_id = ? AND person_id = ?
+        AND DATE(obs_datetime) <= ?",
+       concept.concept_id, @patient.patient_id, @ref_date.to_date).\
+       order("obs_datetime DESC, date_created DESC").first
+
+      ref_period = 1.week
+
+      unless ob.blank?
+        if ob.value_coded == negative_concept.concept_id
+          ref_period = 3.year
+        end
+      else
+        ref_period = 1.year
+      end
+
+      clinic_days =  ['Monday','Tuesday','Wednesday','Thursday','Friday']
+      appointment_date = @ref_date + ref_period
+      valid_day = false
+
+      while !valid_day do
+        if clinic_days.include?(appointment_date.strftime('%A'))
+          valid_day = true
+        else
+          appointment_date -= 1.day
+        end
+      end
+
 
       {
         drugs_run_out_date: "",
-        appointment_date: @ref_date + ref_period
+        appointment_date: appointment_date
       }
     end
   end
