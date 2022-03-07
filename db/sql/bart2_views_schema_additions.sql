fe44633f31be51936a3ce230ff11f45cbb1fdcdc
petros kayange <kayangepetros@gmail.com>
2022-03-07 15:44:59 +0200

    updating mysql opd diagnosis fuction

--- db/sql/bart2_views_schema_additions.sql
+++ db/sql/bart2_views_schema_additions.sql
@@ -2165,8 +2165,8 @@ SET age_in_years  = (SELECT timestampdiff(year, birthdate, end_date));
 SET age_group = ('Unknown');
 
 IF age_in_months >= 0 AND age_in_months <= 5 THEN SET age_group = "0-5 months";
-ELSEIF age_in_months >= 6 AND age_in_years < 5 THEN SET age_group = "6 months to < 5 years";
-ELSEIF age_in_years >= 5 AND age_in_years < 14 THEN SET age_group = "5 years to < 14 years";
+ELSEIF age_in_months >= 6 AND age_in_years < 5 THEN SET age_group = "6 mth < 5 yrs";
+ELSEIF age_in_years >= 5 AND age_in_years < 14 THEN SET age_group = "5-14 yrs";
 ELSEIF age_in_years >= 14 THEN SET age_group = ">= 14 years";
 END IF;
 
