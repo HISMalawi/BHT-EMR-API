f32382eca073a212b4329795f696033447b6db19
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-20 14:22:42 +0200

    init on rolling back identifiers

--- app/services/dde_rollback_service.rb
+++ app/services/dde_rollback_service.rb
@@ -12,7 +12,7 @@ class DDERollbackService
   # this is the method to rollback patient name
   def rollback_name
     result = ActiveRecord::Base.connect.select_one <<~SQL
-      SELECT * from person_names
+      SELECT * FROM person_names
       WHERE voided = 1
       AND person_id = #{secondary_patient.id}
       AND void_reason = 'Merged into patient ##{primary_patient.patient_id}:#{primary_patient.person.names.first.id}'
@@ -24,5 +24,26 @@ class DDERollbackService
     result.delete('creator')
 
     PersonName.create(result)
+  end
+
+  def rollback_identifiers
+    result = ActiveRecord::Base.connection.select_all <<~SQL
+      SELECT * FROM patient_identifier
+      WHERE patient_id = #{secondary_patient.id} AND voided = 1
+      AND void_reason LIKE 'Merged into patient ##{primary_patient.patient_id}:%'
+    SQL
+  end
+
+  def process_identifiers(voided_identifiers: nil)
+    return if voided_identifiers.blank?
+
+    voided_identifiers.each do |identifier|
+      process_identifiers(identifier['void_reason'])
+    end
+  end
+
+  def process_patient_id_and_row_id(reason)
+    patient_id, row_id = reason.split('#')[1].split(':')
+    { patient: patient_id.to_i, row: row_id.to_i }
   end
 end
