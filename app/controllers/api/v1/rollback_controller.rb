50d8239ca6b2de75b6e397a6f8da9276ec22bf54
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-29 10:55:42 +0200

    added action methods

--- app/controllers/api/v1/rollback_controller.rb
+++ app/controllers/api/v1/rollback_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
+# controller for managing merge rollback
 class Api::V1::RollbackController < ApplicationController
+  def merge_history
+    identifier = params.require(:identifier)
+    render json: merge_service.get_patient_audit(identifier), status: :ok
+  end
+
+  def rollback_patient
+    patient_id = params.require(:patient_id)
+    render json: rollback_service.rollback_merged_patient(patient_id), status: :ok
+  end
+
+  private
+
+  def merge_service
+    MergeAuditService.new
+  end
+
+  def rollback_service
+    DDERollbackService.new
+  end
 end
