ce11f3d5963ef1d71dd4bf4e43b36a2eb27d8525
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-29 09:53:04 +0200

    rollback controller added

--- app/services/dde_rollback_service.rb
+++ app/services/dde_rollback_service.rb
@@ -12,7 +12,7 @@ class DDERollbackService
         @primary_patient = record['primary_id']
         @secondary_patient = record['secondary_id']
         process_rollback
-        MergeAudit.find(record['id']).void("Rolling Back #{patient_id}")
+        MergeAudit.find(record['id']).void("Rolling back to #{patient_id}")
       end
     end
     Patient.find(patient_id)
