0eb706c803237be673dcfe95b83f86092244496c
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-09 10:53:05 +0200

    metada script updated to handle new YAML load

--- bin/update_art_metadata.sh
+++ bin/update_art_metadata.sh
@@ -18,11 +18,11 @@ set -x # turns on stacktrace mode which gives useful debug information
 export RAILS_ENV=$ENV
 rails db:environment:set RAILS_ENV=$ENV
 
-USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
-PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
-DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
-HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
-PORT=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['port']"`
+USERNAME=`ruby -ryaml -e "puts YAML::unsafe_load_file('config/database.yml')['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::unsafe_load_file('config/database.yml')['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::unsafe_load_file('config/database.yml')['${ENV}']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::unsafe_load_file('config/database.yml')['${ENV}']['host']"`
+PORT=`ruby -ryaml -e "puts YAML::unsafe_load_file('config/database.yml')['${ENV}']['port']"`
 
 # Only update metadata if migration is successful
 rails db:migrate && {
