40dfca5f6745b9d43c5fc1a136ac08227d3bba06
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-12 17:40:46 +0200

    added program_id parameter for update action method

--- app/controllers/api/v1/encounters_controller.rb
+++ app/controllers/api/v1/encounters_controller.rb
@@ -98,11 +98,12 @@ class Api::V1::EncountersController < ApplicationController
     encounter = Encounter.find(params[:id])
     type = params[:type_id] && EncounterType.find(params[:type_id])
     patient = params[:patient_id] && Patient.find(params[:patient_id])
+    program = params[:program_id] && Program.find(params[:program_id])
     provider = params[:provider_id] ? Person.find(params[:provider_id]) : User.current.person
     encounter_datetime = TimeUtils.retro_timestamp(params[:encounter_datetime]&.to_time || Time.now)
 
     encounter_service.update(encounter, type: type, patient: patient,
-                                        provider: provider,
+                                        provider: provider, program: program,
                                         encounter_datetime: encounter_datetime)
   end
 
