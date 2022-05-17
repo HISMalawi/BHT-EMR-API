880020212348a4bf8a00846cd7b8cc7bfab263be
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-17 11:44:02 +0200

    side effects fixed

--- app/services/art_service/patient_visit.rb
+++ app/services/art_service/patient_visit.rb
@@ -167,13 +167,18 @@ module ARTService
       parent_obs = Observation.where(concept: concept('Malawi ART side effects'), person: patient.person)
                               .where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(date))
                               .order(obs_datetime: :desc)
-                              .first
+
       return [] unless parent_obs
 
-      @side_effects = parent_obs.children
-                                .where(value_coded: ConceptName.find_by_name!('Yes').concept_id)
-                                .collect { |side_effect| side_effect.concept.fullname }
-                                .compact
+      @side_effects = []
+      parent_obs.each do |obs|
+        @side_effects << obs.children
+                            .where(value_coded: ConceptName.find_by_name!('Yes').concept_id)
+                            .collect { |side_effect| side_effect.concept.fullname }
+                            .join(',')
+      end
+
+      @side_effects
     end
 
     def viral_load_result
