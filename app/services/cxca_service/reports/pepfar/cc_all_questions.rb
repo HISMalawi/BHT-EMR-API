8c1fba5314a1d6de4678eae6688bc4e2db9ac999
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-24 14:52:44 +0200

    report standardize to match the requirements

--- app/services/cxca_service/reports/pepfar/cc_all_questions.rb
+++ app/services/cxca_service/reports/pepfar/cc_all_questions.rb
@@ -5,29 +5,95 @@ module CXCAService
     module Pepfar
       # class providing all major Cervical Cancer quetions
       class CcAllQuestions
+        attr_reader :response
+
         AGE_GROUPS = ['15-19', '20-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50+', 'Unknown Age'].freeze
 
         def initialize(start_date:, end_date:)
           @start_date = start_date.strftime('%Y-%m-%d 00:00:00')
           @end_date = end_date.strftime('%Y-%m-%d 23:59:59')
+          @response = init_response_structure
         end
 
-        def data; end
+        def general_report
+          visit_report(is_returning: false)
+          screening_result_report(is_returning: false)
+          treatment_resport(is_returning: false)
+          response
+        end
+
+        def visit_report(is_returning: true)
+          type_of_screening.each do |record|
+            group = response.find { |patient| patient[:age_group] == record['age_group'] }
+            assign_to_type_of_screening(group, record)
+          end
+          return response if is_returning
+        end
+
+        def screening_result_report(is_returning: true)
+          screening_result.each do |record|
+            group = response.find { |patient| patient[:age_group] == record['age_group'] }
+            assign_to_screening_result(group, record)
+          end
+          return response if is_returning
+        end
+
+        def treatment_resport(is_returning: true)
+          type_of_treatment.each do |record|
+            group = response.find { |patient| patient[:age_group] == record['age_group'] }
+            assign_to_type_of_treatment(group, record)
+          end
+          return response if is_returning
+        end
+
+        private
 
         def init_response_structure
           struct = []
           AGE_GROUPS.each do |group|
-            struct << { group => { first_Screen: [], rescreen: [], follow_Up_Screen: [],
-                                   result_negative: [], result_positive: [], result_suspected_cancer: [] } }
+            struct << { age_group: group, first_screen: [], rescreen: [], follow_up_screen: [],
+                        result_negative: [], result_positive: [],
+                        result_suspected_cancer: [], leep: [],
+                        thermocoagulation: [], cryotherapy: [] }
           end
           struct
         end
 
+        def assign_to_type_of_screening(group, record)
+          visit_type = Concept.find(record['value_coded']).fullname
+          return  group[:first_screen] << record['patient_id'] if visit_type == 'Initial Screening'
+          return  group[:rescreen] << record['patient_id'] if ['Postponed treatment',
+                                                               'Problem visit after treatment'].include?(visit_type)
+          return  group[:follow_up_screen] << record['patient_id'] if ['One year subsequent check-up after treatment',
+                                                                       'Subsequent screening'].include?(visit_type)
+        end
+
+        # rubocop:disable Metrics/AbcSize
+        # rubocop:disable Metrics/CyclomaticComplexity
+        def assign_to_screening_result(group, record)
+          result_type = Concept.find(record['value_coded']).fullname
+          if result_type.match(/negative/i) || result_type.match(/normal/i) || result_type == 'No visible Lesion'
+            return group[:result_negative] << record['patient_id']
+          end
+          if result_type.match(/positive/i) || result_type.match(/abnormal/i) || result_type == 'Visible Lesion'
+            return group[:result_positive] << record['patient_id']
+          end
+          return group[:result_suspected_cancer] << record['patient_id'] if result_type.match(/suspect/i)
+        end
+        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/CyclomaticComplexity
+
+        def assign_to_type_of_treatment(group, record)
+          treatment_type = Concept.find(record['value_coded']).fullname
+          return  group[:cryotherapy] << record['patient_id'] if treatment_type == 'Cryotherapy'
+          return  group[:thermocoagulation] << record['patient_id'] if treatment_type == 'Thermocoagulation'
+          return  group[:leep] << record['patient_id'] if treatment_type == 'LEEP'
+        end
+
         def type_of_screening
           ActiveRecord::Base.connection.select_all <<~SQL
-            SELECT e.patient_id, o.value_coded, p.birthdate FROM encounter e
+            SELECT e.patient_id, o.value_coded, cxca_age_group(p.birthdate, '#{@end_date}') as age_group FROM encounter e
             INNER JOIN obs o ON o.encounter_id = e.encounter_id AND o.voided = 0 AND o.concept_id = #{ConceptName.find_by_name('Reason for visit').concept_id}
-            AND o.value_coded IN (#{ConceptName.where(name: ['Initial Screening']).to_sql})
             INNER JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
             WHERE e.voided = 0 AND e.encounter_type = #{EncounterType.find_by_name('CxCa test').id}
             AND e.encounter_datetime BETWEEN '#{@start_date}' AND '#{@end_date}'
@@ -37,8 +103,8 @@ module CXCAService
 
         def screening_result
           ActiveRecord::Base.connection.select_all <<~SQL
-            SELECT e.patient_id, o.value_coded, p.birthdate FROM encounter e
-            INNER JOIN obs o ON o.encounter_id = e.encounter_id AND o.voided = 0 AND o.concept_id = #{ConceptName.find_by_name('Screening results').concept_id}
+            SELECT e.patient_id, o.value_coded, cxca_age_group(p.birthdate, '#{@end_date}') age_group FROM encounter e
+            INNER JOIN obs o ON o.encounter_id = e.encounter_id AND o.voided = 0 AND o.concept_id = #{ConceptName.find_by_name('Screening results').concept_id} AND o.value_coded IS NOT NULL
             INNER JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
             WHERE e.voided = 0 AND e.encounter_type = #{EncounterType.find_by_name('CxCa screening result').id}
             AND e.encounter_datetime BETWEEN '#{@start_date}' AND '#{@end_date}'
@@ -48,7 +114,7 @@ module CXCAService
 
         def type_of_treatment
           ActiveRecord::Base.connection.select_all <<~SQL
-            SELECT e.patient_id, o.value_coded, p.birthdate FROM encounter e
+            SELECT e.patient_id, o.value_coded, cxca_age_group(p.birthdate, '#{@end_date}') age_group FROM encounter e
             INNER JOIN obs o ON o.encounter_id = e.encounter_id AND o.voided = 0 AND o.concept_id = #{ConceptName.find_by_name('Treatment').concept_id}
             AND o.value_coded IN (#{ConceptName.where(name: %w[Cryotherapy Thermocoagulation LEEP]).select(:concept_id).to_sql})
             INNER JOIN person p ON p.person_id = e.patient_id AND p.voided = 0
