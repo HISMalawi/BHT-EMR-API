0060a60185c6bf68a5c8ca9758b0b1b017fe18b3
Roy Chanunkha <roychanunkha@gmail.com>
2022-06-13 17:25:54 +0200

    updated notification service and unmounted the web socket

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -25,12 +25,11 @@ class NotificationService
     return if alert_type != 'LIMS'
 
     lab = User.find_by(username: 'lab_daemon')
-
     ActiveRecord::Base.transaction do
-      alert = NotificationAlert.create!(text: alert_message, date_to_expire: Time.now + 3.months,
+      alert = NotificationAlert.create!(text: alert_message.to_json, date_to_expire: Time.now + 3.months,
                                         creator: lab, changed_by: lab, date_created: Time.now)
-      notify(alert, User.joins(:roles).where(user_role: { role: %w[Clinician Nurse] }))
-      ActionCable.server.broadcast('nlims_channel', alert)
+      notify(alert, User.joins(:roles).where(user_role: { role: %w[Clinician Nurse] }).uniq)
+      # ActionCable.server.broadcast('nlims_channel', alert)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   mount EmrOhspInterface::Engine => '/'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  mount ActionCable.server => '/cable'
 
   namespace :api do
     namespace :v1 do
