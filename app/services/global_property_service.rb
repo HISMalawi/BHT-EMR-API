cd2e7f46eae9a4d33df361c2888ae4d13c465677
Roy Chanunkha <roychanunkha@gmail.com>
2022-07-25 10:47:44 +0200

    added radiology AccessionNumberService

--- app/services/global_property_service.rb
+++ app/services/global_property_service.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 ##
-# A collection of various helpers for dealing wi
+# A collection of various helpers for dealing with global properties
 module GlobalPropertyService
-  def self.use_filing_numbers?
-    GlobalProperty.where(property: 'use.filing.number', property_value: 'true')
-                  .exists?
+  class << self
+    def use_filing_numbers?
+      GlobalProperty.where(property: 'use.filing.number', property_value: 'true')
+                    .exists?
+    end
+
+    def site_code
+      property = GlobalProperty.find_by(property: 'site_prefix')
+      value = property&.property_value&.strip
+
+      raise "Global property 'site_prefix' not set" unless value
+
+      value
+    end
   end
 end
