7eca260ded9c221d8c3aba91949cab47d0e06fb6
Roy Chanunkha <roychanunkha@gmail.com>
2022-07-27 15:51:14 +0200

    order label done

--- app/services/radiology_service/order_label.rb
+++ app/services/radiology_service/order_label.rb
@@ -9,7 +9,6 @@ module RadiologyService
     end
 
     def print
-      type = @order.concept.shortname || @order.concept.fullname
       label = ZebraPrinter::StandardLabel.new
       label.font_size = 4
       label.x = 200
@@ -20,12 +19,28 @@ module RadiologyService
       label.draw_multi_text(@order.patient.person.name)
       label.draw_multi_text("#{@order.patient.national_id_with_dashes} #{@order.patient.person.gender} #{@order.patient.person.birth_date}")
       if detailed_examination.blank?
-        label.draw_multi_text("#{type}-#{examination}")
+        label.draw_multi_text("#{order_type}-#{examination}")
       else
-        label.draw_multi_text("#{type}-#{examination}-#{detailed_examination}")
+        label.draw_multi_text("#{order_type}-#{examination}-#{detailed_examination}")
       end
       label.draw_multi_text("#{session_date}, #{order.accession_number} (#{referred_from})")
       label.print(1)
+    end
+
+    def session_date
+      @session_date ||= @order.start_date.strftime('%d-%b-%Y')
+    end
+
+    def detailed_examination
+      return @detailed_examination if @detailed_examination
+
+      detailed_examination_concept = ConceptName.find_by_name('DETAILED EXAMINATION').concept_id
+      detailed_examination_obs = Observation.find(concept_id: detailed_examination_concept, encounter_id: order.encounter_id)
+      @detailed_examination ||= detailed_examination_obs&.answer_concept&.shortname || detailed_examination_obs&.answer_concept&.fullname
+    end
+
+    def order_type
+      @order_type ||= @order.concept.shortname || @order.concept.fullname
     end
 
     def referred_from
