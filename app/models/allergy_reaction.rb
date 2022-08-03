52372d1c2b42c656ed7262cdd675db31f714f56f
Roy Chanunkha <roychanunkha@gmail.com>
2022-08-03 14:10:49 +0200

    changes to the allergy reaction model

--- app/models/allergy_reaction.rb
+++ app/models/allergy_reaction.rb
@@ -1,2 +1,4 @@
 class AllergyReaction < ApplicationRecord
+  self.table_name = :allergy_reaction
+  self.primary_key = :allergy_reaction_id
 end
--- db/migrate/20220803115010_create_allergy_reactions.rb
+++ db/migrate/20220803115010_create_allergy_reactions.rb
@@ -1,13 +1,13 @@
 class CreateAllergyReactions < ActiveRecord::Migration[5.2]
   def change
-    create_table :allergy_reactions, id: false do |t|
+    create_table :allergy_reaction, id: false do |t|
       t.bigint :allergy_reaction_id, null: false, primary_key: true, auto_increment: true
-      t.string :allergy_id, null: false
+      t.bigint :allergy_id, null: false
       t.integer :reaction_concept_id, null: false
       t.string :reaction_non_coded, null: true, limit: 255
       t.string :uuid, null: false, limit: 38, unique: true
     end
-    add_foreign_key :allergy_reactions, :allergy, column: :allergy_id, primary_key: :allergy_id
-    add_foreign_key :allergy_reactions, :concept, column: :reaction_concept_id, primary_key: :concept_id
+    add_foreign_key :allergy_reaction, :allergy, column: :allergy_id, primary_key: :allergy_id
+    add_foreign_key :allergy_reaction, :concept, column: :reaction_concept_id, primary_key: :concept_id
   end
 end
