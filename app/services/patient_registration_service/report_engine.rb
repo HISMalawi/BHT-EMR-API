c44097a79e931e7672c485bb7e06ce4a1482119b
Roy Chanunkha <roychanunkha@gmail.com>
2022-08-11 10:31:35 +0200

    fixes on the metadata

--- app/services/patient_registration_service/report_engine.rb
+++ app/services/patient_registration_service/report_engine.rb
@@ -6,14 +6,14 @@ module PatientRegistrationService
     REPORT_NAMES = {
       'DASHBOARD STATISTICS' => PatientRegistrationService::Reports::Clinic::OverviewReport
     }.freeze
-  end
 
-  def reports(start_date, end_date, name)
-    name = name.upcase
-    REPORT_NAMES[name].new(start_date: start_date, end_date: end_date).data
-  end
+    def reports(start_date, end_date, name)
+      name = name.upcase
+      REPORT_NAMES[name].new(start_date: start_date, end_date: end_date).data
+    end
 
-  def dashboard_stats(date = Date.today)
-    REPORT_NAMES['DASHBOARD STATISTICS'].new(date).data
+    def dashboard_stats(date = Date.today)
+      REPORT_NAMES['DASHBOARD STATISTICS'].new(date).data
+    end
   end
 end
