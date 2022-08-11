fceda5f7522934099c06cc6247253fe258c2881a
Roy Chanunkha <roychanunkha@gmail.com>
2022-08-11 09:17:36 +0200

    fixed bug on vl disaggregated: date ranges cannot be generated from strings

--- app/services/art_service/reports/viral_load_disaggregated.rb
+++ app/services/art_service/reports/viral_load_disaggregated.rb
@@ -6,8 +6,8 @@ module ARTService
       attr_reader :start_date, :end_date, :from, :to
 
       def initialize(start_date:, end_date:, from: nil, to: nil, **_kwargs)
-        @start_date = start_date
-        @end_date = end_date
+        @start_date = start_date.to_date
+        @end_date = end_date.to_date
         @from = from&.to_f
         @to = to&.to_f
       end
