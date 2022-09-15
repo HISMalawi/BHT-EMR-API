e35269ff0fa7a197f182186f55bd898df318cb0f
dominickasanga <dominickasanga@gmail.com>
2022-09-15 09:54:35 +0200

    add attendance endpoint for OPD

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -47,6 +47,18 @@ class Api::V1::ReportsController < ApplicationController
     render json: stats
   end
 
+  def attendance
+    start_date, end_date = params.require %i[start_date end_date]
+    puts("#################################################################")
+    puts("#################################################################")
+    puts("#################################################################")
+    puts( params.require %i[start_date end_date])
+    puts("#################################################################")
+    stats = service.attendance(start_date, end_date)
+
+    render json: stats
+  end
+
   def diagnosis_by_address
     start_date, end_date = params.require %i[start_date end_date]
     stats = service.diagnosis_by_address(start_date, end_date)
