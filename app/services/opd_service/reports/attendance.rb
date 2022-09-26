f66bc4d147b2006cdc0fc6d1414200b021b28628
dominickasanga <dominickasanga@gmail.com>
2022-09-26 13:48:49 +0200

    modify attendance

--- app/services/opd_service/reports/attendance.rb
+++ app/services/opd_service/reports/attendance.rb
@@ -9,11 +9,10 @@ class OPDService::Reports::Attendance
     data = Encounter.find_by_sql(
       "SELECT patient_id, DATE_FORMAT(encounter_datetime,'%Y-%m-%d') enc_date
       FROM encounter e
-      LEFT OUTER JOIN person p ON p.person_id = e.patient_id
       WHERE e.voided = 0 AND encounter_datetime BETWEEN '" + start_date.to_date.strftime('%Y-%m-%d 00:00:00') +"'
         AND '" + end_date.to_date.strftime('%Y-%m-%d 23:59:59') + "'
         AND program_id ='" + programID.program_id.to_s + "'
-      GROUP BY enc_date"
+      GROUP BY patient_id, DATE(encounter_datetime)"
     ).map{|e| e. patient_id}
 
     return data
