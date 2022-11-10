9595b17489c6055a884d6b7e3c5bee388eba8f30
Roy Chanunkha <8115806+Skipper-116@users.noreply.github.com>
2022-11-10 09:58:47 +0200

    HTS REPORTS (#252)
    * init on hts_index report
    
    * hts index report first run done
    
    * init on hts self
    
    * hts tst community report done
    
    * hts recent community report
    
    * adding patient ids to make it easy querying maternal status
    
    * hts index report logic updated
    
    * hts service report engine added
    
    * placing the hts engine in the right folder
    
    * added the hts engine to the report service
    
    * add reports to controller
    
    * removing debuggers
    
    * swagger documentation added
    
    * schema updates
    
    * descriptions updated
    
    * added a special hts report route
    
    * hts report controller file added
    
    * Swagger documentation added
    
    * added validation functions
    
    * updated hts_concepts
    
    * Metadata
    
    * Added workflow encounters
    
    * Added deleted code
    
    * replacing screening encounter with testing
    
    Co-authored-by: pamzey <mvumapatrick@gmail.com>

---
+++ app/controllers/api/v1/hts_reports_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Api::V1::HtsReportsController < ApplicationController
+  before_action :validate_params
+  def index
+    report = service.generate_report(name: @name,
+                                     type: @name,
+                                     start_date: parse_date(@start_date).to_s,
+                                     end_date: parse_date(@end_date).to_s)
+
+    if report
+      render json: report
+    else
+      render status: :no_content
+    end
+  end
+
+  private
+
+  def validate_params
+    @start_date, @end_date, @name = params.require(%i[start_date end_date name])
+    handle_errors 'start date cannot be greater than end date', 'start_date' if @start_date > @end_date
+    handle_errors 'end date cannot be greater than today', 'end_date' if @end_date.to_date > Date.today
+    handle_errors 'name cannot be blank', 'name' if @name.blank?
+  end
+
+  def handle_errors(message, entity)
+    error = UnprocessableEntityError.new(message)
+    error.add_entity(entity)
+    raise error
+  end
+
+  def service
+    ReportService.new(program_id: 18, overwrite_mode: false)
+  end
+end
