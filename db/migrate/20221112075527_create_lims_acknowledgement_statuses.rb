863476f5fa1fdc51719c4c65a776995f77985bd1
Roy Chanunkha <roychanunkha@gmail.com>
2022-11-12 13:59:51 +0200

    changes to the migrations

--- db/migrate/20221112075527_create_lims_acknowledgement_statuses.rb
+++ db/migrate/20221112075527_create_lims_acknowledgement_statuses.rb
@@ -1,11 +1,15 @@
 class CreateLimsAcknowledgementStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :lims_acknowledgement_statuses, id: false do |t|
-      t.string :tracking_number, primary_key: true
-      t.string :test_name, null: false
-      t.string :date_acknowledged, null: false
+      t.integer :order_id, primary_key: true
+      t.integer :test, null: false
+      t.datetime :date_received, null: false
+      t.datetime :date_pushed, null: true
       t.string :acknowledgment_type, null: false
-      t.boolean :status, null: false, default: false
+      t.boolean :pushed, null: false, default: false
     end
+
+    add_foreign_key :lims_acknowledgement_statuses, :orders, column: :order_id, primary_key: :order_id
+    add_foreign_key :lims_acknowledgement_statuses, :concept, column: :test, primary_key: :concept_id
   end
 end
