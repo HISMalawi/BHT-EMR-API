683c027ed5ba8c23e70f992008aa2c7e1770016a
Emmanuel Z.K. Nyondo <emmanuelnyondo@gmail.com>
2022-11-27 16:15:34 +0200

    feat: add drug_cms controller and controller methods for CRUD

---
+++ app/controllers/api/v1/drug_cms_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::DrugCmsController < ApplicationController
+  before_action :authenticate, except: %i[index create update show search]
+
+  def index
+    render json: paginate(drug_cms_service.get_all_drug_cms)
+  end
+
+  def create
+    render json: drug_cms_service.create_drug_cms(params)
+  end
+
+  def update
+    render json: drug_cms_service.update_drug_cms(params)
+  end
+
+  def show
+    id = params.require(:id)
+    render json: drug_cms_service.get_drug_cms(params)
+  end
+
+  def search
+    render json: paginate(drug_cms_service.search_drug_cms(params))
+  end
+
+  private
+  def drug_cms_service
+    DrugCmsService.new
+  end
+end
