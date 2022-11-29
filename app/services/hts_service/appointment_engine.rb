05f7a8d5943464c4208ed8aca5b2b00166333ff4
Andrew Mfune <andiemfune@gmail.com>
2022-11-29 15:08:50 +0200

    Refactor to account for hts accesspoint and hiv positive status

--- app/services/hts_service/appointment_engine.rb
+++ app/services/hts_service/appointment_engine.rb
@@ -8,38 +8,47 @@ module HTSService
       @patient = patient
     end
 
+    def concept_id(name)
+      concept(name).concept_id
+    end
+
     def next_appointment_date
-      return @ref_date + 2.weeks if has_inconclusive_hiv_result?
-      return @ref_date + 4.weeks if is_high_risk_within_last_three_months?
-      return @ref_date + 12.months if is_ongoing_risk?
-      nil
+      inconclusive_concept = 10609
+      hiv_status = recent_hiv_status
+      return @ref_date + 2.weeks if recent_access_point == concept_id('Community') && hiv_status == concept_id('Positive')
+      return @ref_date + 2.weeks if hiv_status == inconclusive_concept
+      if hiv_status == concept_id('Negative')
+        risk_category = recent_risk_category
+        return @ref_date + 4.weeks if risk_category == concept_id('High risk event in last 3 months')
+        return @ref_date + 12.months if risk_category == concept_id('On-going risk')
+      end
     end
 
-    def is_high_risk_within_last_three_months?
-      Observation.joins(:encounter).where(
+    def recent_hiv_status
+      obs = Observation.joins(:encounter).where(
+        encounter: { type: encounter_type("Testing") },
+        concept: concept('HIV status'),
+        person: @patient.person,
+      ).where('encounter_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
+      return obs.first.value_coded if obs.exists?
+    end
+
+    def recent_access_point
+      obs = Observation.joins(:encounter).where(
+        encounter: { type: encounter_type("Testing") },
+        concept: concept('HTS Access Type'),
+        person: @patient.person,
+      ).where('encounter_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
+      return obs.first.value_coded if obs.exists?
+    end
+
+    def recent_risk_category
+      obs = Observation.joins(:encounter).where(
         encounter: { type: encounter_type("Testing") },
         concept: concept('client risk category'),
-        value_coded: concept('High risk event in last 3 months')
-      ).where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
-      .exists?
-    end
-
-    def has_inconclusive_hiv_result?
-      Observation.joins(:encounter).where(
-        encounter: { type: encounter_type("Testing") },
-        concept: concept('HIV Status'),
-        value_coded: concept('Inconclusive')
-      ).where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
-      .exists?
-    end
-
-    def is_ongoing_risk?
-      Observation.joins(:encounter).where(
-        encounter: { type: encounter_type("Testing") },
-        concept: concept('client risk category'),
-        value_coded: concept('On-going risk')
-      ).where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
-      .exists?
+        person: @patient.person,
+      ).where('encounter_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@ref_date))
+      return obs.first.value_coded if obs.exists?
     end
   end
 end
