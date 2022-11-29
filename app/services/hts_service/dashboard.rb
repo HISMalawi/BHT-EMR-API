ea65f719bff724fd1c10a2ed2cfee62b75d896a0
Andrew Mfune <andiemfune@gmail.com>
2022-11-29 11:00:35 +0200

    Initial HTS dashboard stats

--- app/services/hts_service/dashboard.rb
+++ app/services/hts_service/dashboard.rb
@@ -3,36 +3,45 @@
 module HTSService
   # Dashbiard Class
   class Dashboard
-
-  def self.daily_statistics(start_date,end_date)
-
-art = Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
+    def self.total_registered(date)
+      # TODO: Check encounter_type_id for Testing
+      Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
                  .joins("INNER JOIN encounter ON encounter.encounter_id = obs.encounter_id")
-                 .where(obs: {value_text:"ART"},encounter:{program_id: 18},concept_name:{name:"Referrals ordered"})
-                 .where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(start_date))
+                 .where(encounter: {program_id: 18 }, concept_name: { name: "HIV Status" })
+                 .where('DATE(obs_datetime) = ?', date)
                  .count
+    end
 
-booked = Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
-                    .joins("INNER JOIN encounter ON encounter.encounter_id = obs.encounter_id")
-                    .where(encounter:{program_id: 18},concept_name:{name:"ART visit"})
-                    .where('obs.value_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(start_date))
-                    .count
+    def self.total_enrolled_into_art(date)
+      on_art_concept = 7010
+      Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
+                 .joins("INNER JOIN encounter ON encounter.encounter_id = obs.encounter_id")
+                 .joins("INNER JOIN encounter_type ON encounter.encounter_type = encounter_type.encounter_type_id")
+                 .where(encounter: { program_id: 18 }, encounter_type: { name: 'ART_FOLLOWUP' },
+                    obs: { value_coded: on_art_concept }, concept_name: { name: "Antiretroviral therapy referral"}
+                  )
+                 .where('DATE(obs_datetime) = ?', date)
+                 .count
+    end
 
-tested = Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
-                    .joins("INNER JOIN encounter ON encounter.encounter_id = obs.encounter_id")
-                    .where(encounter:{program_id: 18},concept_name:{name:"ART visit"}, obs:{value_datetime:start_date})
-                    .where('obs_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(start_date))
-                    .count
+    def self.total_tested_returning(date)
+      Observation.joins("INNER JOIN concept_name ON concept_name.concept_id = obs.concept_id")
+                 .joins("INNER JOIN encounter ON encounter.encounter_id = obs.encounter_id")
+                 .joins("INNER JOIN encounter_type ON encounter.encounter_type = encounter_type.encounter_type_id")
+                 .where(encounter: { program_id: 18 }, encounter_type: { name: 'APPOINTMENT' },
+                    concept_name: { name: "Appointment date"}
+                  )
+                 .where('DATE(obs.value_datetime) = ? ', date)
+                 .count
+    end
 
-            stats = [{
-                      hts_registered: PatientProgram.where(program_id:18).count,
-                      enrolled_on_art: art,
-                      booked_appointments: booked,
-                      tested_appointments: tested,
-                     }]
-   end
-
-
-
+    def self.daily_statistics(start_date)
+      date = start_date.to_date
+      {
+        total_enrolled_into_art: self.total_enrolled_into_art(date),
+        total_registered: self.total_registered(date),
+        total_tested_returning: self.total_tested_returning(date)
+      }
+    end
   end
 end
