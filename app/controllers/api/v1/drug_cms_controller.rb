06e8ca6e28f35347daedd66ac696f4e05db13920
Roy Chanunkha <roychanunkha@gmail.com>
2022-11-30 13:35:47 +0200

    minor fixes on the naming convetions

--- app/controllers/api/v1/drug_cms_controller.rb
+++ app/controllers/api/v1/drug_cms_controller.rb
@@ -1,59 +1,62 @@
+# frozen_string_literal: true
+
+# DrugCmsController is a controller class for DrugCMS
 class Api::V1::DrugCmsController < ApplicationController
-  before_action :get_record, except: %i[index create search]
+  before_action :set_record, except: %i[index create search]
 
-  #Return drug_cms array of objects
+  # Return drug_cms array of objects
   def index
-    render json: paginate(DrugCms.all)
+    render json: paginate(DrugCms.all), status: :ok
   end
 
-  #show specific record on drug_cms/#
+  # show specific record on drug_cms/#
   def show
-    id = params.require(:id)
-    render json: @found_record
+    render json: @drug_cms, status: :ok
   end
 
-  #create new drug_cms
+  # create new drug_cms
   def create
     new_drug = DrugCms.create(create_parameters)
     handle_errors(new_drug) unless new_drug.errors.blank?
-    render json: new_drug
+    render json: new_drug, status: :created
   end
 
-  #update specific drug_cms/#
+  # update specific drug_cms/#
   def update
-    update_drug_cms = @found_record.update(update_parameters)
-    handle_errors(@found_record) unless update_drug_cms
-    render json: @found_record
+    update_drug_cms = @drug_cms.update(update_parameters)
+    handle_errors(@drug_cms) unless update_drug_cms
+    render json: @drug_cms, status: :ok
   end
 
-  #search drug_cms using param[:keyword]
+  # search drug_cms using param[:keyword]
   def search
     kwd = params[:keyword]
     if kwd.present?
-      render json: paginate(drug_cms_service.search_drug_cms(kwd))
+      render json: paginate(service.search_drug_cms(kwd))
     else
       render json: []
     end
   end
 
-  #destroy
+  # destroy
   def destroy
     reason = params.require(:reason)
-    @found_record.void(reason)
-    render json: {message: 'Removed successfully'}, status: :ok
+    @drug_cms.void(reason)
+    render json: { message: 'Removed successfully' }, status: :ok
   end
 
   private
-  def drug_cms_service
+
+  def service
     DrugCmsService.new
   end
 
-  def get_record
-    @found_record = DrugCms.find(params[:id])
+  def set_record
+    @drug_cms = DrugCms.find(params[:id])
   end
 
   def handle_errors(model)
-    error = InvalidParameterError.new("Failed to Updated Record")
+    error = InvalidParameterError.new('Failed to Updated Record')
     error.model_errors = model.errors
     raise error
   end
