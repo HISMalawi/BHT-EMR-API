ec3cb6f48720bfaf18d3cfbd7767449090eed0ae
Roy Chanunkha <roychanunkha@gmail.com>
2022-11-30 13:35:47 +0200

    minor fixes on the naming convetions

--- app/services/drug_cms_service.rb
+++ app/services/drug_cms_service.rb
@@ -1,6 +1,7 @@
-class DrugCmsService
+# frozen_string_literal: true
 
-  #search drug_cms
+# DrugCmsService is a service class for DrugCMS
+class DrugCmsService
   def search_drug_cms(kwd)
     DrugCms.where("code LIKE('%#{kwd}%') OR name LIKE('%#{kwd}%') OR short_name LIKE('%#{kwd}%')")
   end
