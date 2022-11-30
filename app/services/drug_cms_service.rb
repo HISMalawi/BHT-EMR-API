7ce9651455b56abc309fd6dbac795b594b2f429e
Roy Chanunkha <roychanunkha@gmail.com>
2022-11-30 16:18:29 +0200

    adding arv drug join

--- app/services/drug_cms_service.rb
+++ app/services/drug_cms_service.rb
@@ -3,6 +3,11 @@
 # DrugCmsService is a service class for DrugCMS
 class DrugCmsService
   def search_drug_cms(kwd)
-    DrugCms.where("code LIKE('%#{kwd}%') OR name LIKE('%#{kwd}%') OR short_name LIKE('%#{kwd}%')")
+    ActiveRecord::Base.connection.select_all <<~SQL
+      SELECT dc.*
+      FROM drug_cms dc
+      INNER JOIN arv_drug ad ON ad.drug_id = dc.drug_inventory_id
+      WHERE dc.name LIKE '%#{kwd}%' OR dc.code LIKE '%#{kwd}%' OR dc.short_name LIKE '%#{kwd}%'
+    SQL
   end
 end
