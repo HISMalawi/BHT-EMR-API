8eaeca5433dbb478fa9aef1ef4e9ee6353ad6056
Roy Chanunkha <roychanunkha@gmail.com>
2022-12-08 14:50:44 +0200

    init on radiology migration

---
+++ bin/radiology/add_order_id_to_oprhaned_obs.rb
@@ -0,0 +1,39 @@
+RADIOLOGY_EXAMINATION = EncounterType.find_by_name!('RADIOLOGY EXAMINATION')
+RADIOLOGY_PROGRAM = Program.find_by_name!('RADIOLOGY PROGRAM')
+
+def all_encounters
+  Encounter.where(type: RADIOLOGY_EXAMINATION, program: RADIOLOGY_PROGRAM)
+end
+
+def all_orders
+  Order.where(encounter: all_encounters)
+end
+
+def process_orphaned_obs(order)
+  return unless Observation.where(order: order).empty?
+
+  obs = order.encounter.observations.where('obs_datetime <= ? AND order_id IS NULL', order.date_created)
+  obs.each do |ob|
+    ob.order_id = order.id
+    ob.save!
+  end
+end
+
+def process_all_orphaned_obs
+  all_orders.each do |order|
+    process_orphaned_obs(order)
+  end
+end
+
+Rails.logger = Logger.new($stdout)
+ActiveRecord::Base.logger = Rails.logger
+ActiveRecord::Base.logger.level = :debug
+
+start_time = Time.now
+ActiveRecord::Base.transaction do
+  process_all_orphaned_obs
+  Rails.logger.info("Successfully processed")
+end
+end_time = Time.now
+
+Rails.logger.info("Finished in #{end_time - start_time} seconds")
