81d1fa12ada879908223f5a1b838283255c64939
petroskayange <kayangepetros@gmail.com>
2023-01-15 20:23:42 +0200

    merging

--- config/routes.rb
+++ config/routes.rb
@@ -197,7 +197,6 @@ Rails.application.routes.draw do
         get 'earliest_expiring_item', to: 'items#earliest_expiring'
         get 'drug_consumption', to: 'drugs#drug_consumption'
         get 'stock_report', to: 'audit_trails#stock_report'
-        get '/audit_trail/grouped', to: 'audit_trails#show_grouped_audit_trail'
       end
 
       namespace :types do
@@ -273,10 +272,6 @@ Rails.application.routes.draw do
       get '/sequences/next_accession_number', to: 'sequences#next_accession_number'
 
       post '/reports/encounters' => 'encounters#count'
-
-      #drugs_cms routes
-      get '/drug_cms/search', to: "drug_cms#search"
-      resources :drug_cms, only: %i[index]
     end
   end
 
@@ -374,5 +369,4 @@ Rails.application.routes.draw do
 
   get '/api/v1/data_cleaning_confirmation', to: 'api/v1/data_cleaning#view'
   post '/api/v1/data_cleaning_confirmation', to: 'api/v1/data_cleaning#create'
-
 end
