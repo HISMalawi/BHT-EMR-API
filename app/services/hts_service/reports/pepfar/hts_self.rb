7b4c3b1e6929e906750e9a56eaa35b244c35eb33
Andrew Mfune <andiemfune@gmail.com>
2023-01-26 07:49:48 +0200

    Query implementation for hts self

--- app/services/hts_service/reports/pepfar/hts_self.rb
+++ app/services/hts_service/reports/pepfar/hts_self.rb
@@ -1,5 +1,5 @@
+include ModelUtils
 # frozen_string_literal: true
-
 module HtsService
   module Reports
     module Pepfar
@@ -29,6 +29,18 @@ module HtsService
 
         GENDER_TYPES = %w[F M].freeze
 
+        def load_patients_into_report(report, patients)
+          patients.each do | patient |
+            age_group = patient['age_group']
+            gender = patient['gender']
+            report[age_group][gender][:directly_assisted] << patient['directly_assisted'] if !patient['directly_assisted'].nil?
+            report[age_group][gender][:unassisted] << patient['unassisted'] if !patient['unassisted'].nil?
+            report[age_group][gender][:self_recipient] << patient['self_recipient'] if !patient['self_recipient'].nil?
+            report[age_group][gender][:sex_partner] << patient['sex_partner'] if !patient['sex_partner'].nil?
+            report[age_group][gender][:other] << patient['other'] if !patient['other'].nil?
+          end
+        end
+
         def init_report
           pepfar_age_groups.each_with_object({}) do |age_group, report|
             next if age_group == 'Unknown'
@@ -37,7 +49,7 @@ module HtsService
               gender_sub_report[gender] = {
                 directly_assisted: [],
                 unassisted: [],
-                self: [],
+                self_recipient: [],
                 sex_partner: [],
                 other: []
               }
@@ -46,7 +58,47 @@ module HtsService
         end
 
         def fetch_clients
-          raise NotImplementedError
+          ActiveRecord::Base.connection.select_all <<~SQL
+            SELECT
+              p.person_id,
+              p.gender,
+              disaggregated_age_group(p.birthdate, '#{@end_date}') as age_group,
+              directly_assisted.person_id as directly_assisted,
+              unassisted.person_id as unassisted,
+              self_recipient.person_id as self_recipient,
+              sex_partner.person_id as sex_partner,
+              other.person_id as other
+            FROM
+              person p
+            INNER JOIN
+              encounter e on e.patient_id = p.person_id
+            LEFT JOIN
+              obs directly_assisted on directly_assisted.encounter_id = e.encounter_id and
+              directly_assisted.concept_id = #{concept('Self-test approach').concept_id} and
+              directly_assisted.value_text = "Directly-assisted"
+            LEFT JOIN
+              obs unassisted on unassisted.encounter_id = e.encounter_id and
+              unassisted.concept_id = #{concept('Self-test approach').concept_id} and
+              unassisted.value_text = "Un-assisted"
+            LEFT JOIN
+              obs self_recipient on self_recipient.encounter_id = e.encounter_id and
+              self_recipient.concept_id = #{concept('Self-test end user').concept_id} and
+              self_recipient.value_coded = #{concept('Self').concept_id}
+            LEFT JOIN
+              obs sex_partner on sex_partner.encounter_id = e.encounter_id and
+              sex_partner.concept_id = #{concept('Self-test end user').concept_id} and
+              sex_partner.value_coded = #{concept('Sexual Partner').concept_id}
+            LEFT JOIN
+              obs other on other.encounter_id = e.encounter_id and
+              other.concept_id = #{concept('Self-test end user').concept_id} and
+              other.value_coded = #{concept('Other').concept_id}
+            WHERE
+              e.voided = 0 and
+              e.program_id = 18 and
+              e.encounter_type = #{encounter_type('ITEMS GIVEN').encounter_type_id} and
+              DATE(e.encounter_datetime) BETWEEN "#{@start_date}" AND "#{@end_date}"
+            GROUP BY p.person_id
+          SQL
         end
       end
     end
