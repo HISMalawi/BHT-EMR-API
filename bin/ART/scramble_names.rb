9e7cba227c3a5890131afbae5ac5b187ad485c23
Roy Chanunkha <roychanunkha@gmail.com>
2023-02-10 14:13:08 +0200

    added a randomly generate phone numbers

--- bin/ART/scramble_names.rb
+++ bin/ART/scramble_names.rb
@@ -19,11 +19,30 @@ def process_addresses(patient)
   end
 end
 
+def generate_phone_number
+  country_code = '+265'
+  first_two_digits = [88, 89, 98, 99, 1, 212].sample
+  seven_random_digits = rand(10**7).to_s.rjust(7, '0')
+  phone_number = "#{country_code}#{first_two_digits}#{seven_random_digits}"
+
+  phone_number = "#{country_code}#{first_two_digits}#{seven_random_digits.first(6)}" if [1, 212].include?(first_two_digits)
+
+  phone_number
+end
+
+def process_phone_number(patient)
+  patient.person.person_attributes.where(person_attribute_type_id: 12).each do |person_attribute|
+    person_attribute.value = generate_phone_number
+    person_attribute.save!
+  end
+end
+
 def process_patient
-  Patient.all.each do |patient|
+  Parallel.each(Patient.all, in_threads: 10) do |patient|
     puts "Processing patient #{patient.id}"
     process_names patient
     process_addresses patient
+    process_phone_number patient
   end
 end
 
