7fe0cf4647c5efa0eef17769f409a3b06354d328
bryan <brianmsyamboza@gmail.com>
2023-02-14 16:01:50 +0200

    fix alignment of hts linkage code barcode

--- app/services/hts_service/hts_linkage_code.rb
+++ app/services/hts_service/hts_linkage_code.rb
@@ -20,10 +20,9 @@ module HTSService
       person_name = PersonName.find_by(person_id: @patient_id)
       name = "#{person_name.given_name} #{person_name.family_name}"
       label = ZebraPrinter::StandardLabel.new
-      label.draw_text(name, 40, 10, 0, 2, 2, 2, false)
-      label.draw_text('HTS LINK CODE', 40, 60, 0, 2, 2, 2, false)
-      label.draw_text(hts_linkage_code, 40, 120, 0, 2, 2, 2, false)
-      label.draw_barcode(50, 180, 0, 1, 5, 15, 120, false, hts_linkage_code)
+      label.draw_text(name, 20, 10, 0, 1, 2, 2, false)
+      label.draw_text(hts_linkage_code, 30, 60, 0, 1, 2, 2, false)
+      label.draw_barcode(40, 100, 0, 1, 5, 15, 120, false, hts_linkage_code)
       label.print(1)
     end
   end
