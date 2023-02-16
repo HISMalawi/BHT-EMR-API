438d28c1d147964282d98a1ac31451edf0d4dcf7
Roy Chanunkha <roychanunkha@gmail.com>
2023-02-16 18:16:26 +0200

    updates to the regimen data report

--- app/services/art_service/reports/regimen_data.rb
+++ app/services/art_service/reports/regimen_data.rb
@@ -6,19 +6,31 @@ module ARTService
     class RegimenData
       attr_reader :start_date, :end_date, :type
 
-      def initialize(start_date:, end_date:, type:)
+      def initialize(start_date:, end_date:, **kwargs)
         @start_date = ActiveRecord::Base.connection.quote(start_date)
         @end_date = ActiveRecord::Base.connection.quote(end_date)
-        @type = type
+        @type = kwargs[:type]
       end
 
-      def data
+      def find_report
         drop_regimen_data
-        clients_alive_on_treatment
+        create_filtered_data
         process_clients
       end
 
       private
+
+      def drop_regimen_data
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_dispensation'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_drug_dispensed'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_regimen_names'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_patient_regimen'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_reg_outcome'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_vl_results'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_vl_results'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_regimen_patient_weight'
+        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_regimen_data'
+      end
 
       def create_filtered_data
         create_temp_current_dispensation
@@ -26,6 +38,9 @@ module ARTService
         create_temp_current_regimen
         create_temp_current_patient_regimen
         create_temp_patient_outcome
+        create_temp_vl_result
+        create_temp_current_vl_results
+        create_temp_regimen_patient_weight
       end
 
       def create_temp_current_dispensation
@@ -45,7 +60,7 @@ module ARTService
       def create_temp_drug_dispensed
         ActiveRecord::Base.connection.execute <<~SQL
           CREATE table temp_drug_dispensed
-          SELECT o.patient_id, od.drug_inventory_id, d.name, o.start_date
+          SELECT o.patient_id, od.drug_inventory_id, d.name, o.start_date, od.quantity
           FROM orders o
           INNER JOIN temp_current_dispensation tcd ON tcd.patient_id = o.patient_id AND tcd.start_date = o.start_date
           INNER JOIN drug_order od ON od.order_id = o.order_id AND od.quantity > 0 AND od.drug_inventory_id IN (SELECT drug_id FROM arv_drug)
@@ -65,7 +80,7 @@ module ARTService
           INNER JOIN moh_regimen_name AS regimen_name USING (regimen_name_id)
           GROUP BY combo.regimen_combination_id
         SQL
-        ActiveRecord::Base.connection.execute 'create index regimen_names on temp_current_regimen_names (drugs)'
+        ActiveRecord::Base.connection.execute 'create index regimen_names on temp_current_regimen_names (drugs(50))'
       end
 
       def create_temp_current_patient_regimen
@@ -129,129 +144,83 @@ module ARTService
         ActiveRecord::Base.connection.execute 'create index current_vl on temp_current_vl_results (patient_id)'
       end
 
-      def clients_alive_on_treatment
+      def create_temp_regimen_patient_weight
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE table tmp_latest_arv_dispensation
+          CREATE TABLE temp_regimen_patient_weight
+          SELECT tro.patient_id, o.value_numeric AS weight
+          FROM temp_reg_outcome tro
+          INNER JOIN (
+            SELECT person_id, MAX(obs_datetime) AS max_datetime
+            FROM obs
+            WHERE concept_id = 5089 AND voided = 0 AND obs_datetime < #{end_date} + INTERVAL 1 DAY
+            GROUP BY person_id
+          ) latest_obs ON latest_obs.person_id = tro.patient_id
+          INNER JOIN obs o ON o.person_id = latest_obs.person_id AND o.concept_id = 5089 AND o.obs_datetime = latest_obs.max_datetime AND o.voided = 0 AND o.obs_datetime < #{end_date} + INTERVAL 1 DAY
+          WHERE tro.outcome = 'On antiretrovirals'
+        SQL
+        ActiveRecord::Base.connection.execute 'create index patient_weight on temp_regimen_patient_weight (patient_id)'
+      end
+
+      def clients_alive_on_treatment
+        ActiveRecord::Base.connection.select_all <<~SQL
           SELECT
-            o.patient_id,
-            o.start_date,
-            pe.gender,
-            pe.birthdate,
+            trc.patient_id,
+            p.gender,
+            p.birthdate,
             pn.given_name,
             pn.family_name,
             i.identifier arv_number,
-            o.order_id,
-            GROUP_CONCAT(CONCAT('{"medication":"', d.name, '", "start_date": "', DATE(o.start_date),'", "quantity":"',od.quantity,'"}')) medication,
-            cast(patient_date_enrolled(o.patient_id) as date) AS date_enrolled,
-            date_antiretrovirals_started(o.patient_id, min(ps.start_date)) AS earliest_start_date, #{current_outcome} outcome, #{current_regimen} regimen
-          FROM orders o
-          INNER JOIN drug_order od ON od.order_id = o.order_id AND od.quantity > 0 AND od.drug_inventory_id IN (SELECT drug_id FROM arv_drug)
-          INNER JOIN drug d ON d.drug_id = od.drug_inventory_id AND d.retired = 0
-          INNER JOIN (
-            SELECT o.patient_id, MAX(o.start_date) AS start_date
-            FROM orders o
-            INNER JOIN drug_order od ON od.order_id = o.order_id AND od.quantity > 0 AND od.drug_inventory_id IN (SELECT drug_id FROM arv_drug)
-            WHERE o.start_date > #{end_date} - INTERVAL 18 MONTH AND o.start_date < #{end_date} + INTERVAL 1 DAY
-            AND o.voided = 0
-            AND o.order_type_id = 1
-            GROUP BY o.patient_id
-          ) AS last_order ON last_order.patient_id = o.patient_id AND last_order.start_date = o.start_date
-          INNER JOIN patient p ON p.patient_id = o.patient_id AND p.voided = 0
-          INNER JOIN person pe ON pe.person_id = o.patient_id AND pe.voided = 0
-          INNER JOIN patient_program pg ON pg.patient_id = o.patient_id AND pg.program_id = 1 AND pg.voided = 0
-          LEFT JOIN patient_state ps ON ps.patient_program_id = pg.patient_program_id AND ps.voided = 0 AND ps.state = 7 AND ps.start_date <= #{end_date} + INTERVAL 1 DAY
-          LEFT JOIN person_name pn ON pn.person_id = pe.person_id AND pn.voided = 0
-          LEFT JOIN patient_identifier i ON i.patient_id = pe.person_id AND i.identifier_type = 4 AND i.voided = 0
-          WHERE o.voided = 0
-          GROUP BY o.patient_id
+            tpw.weight,
+            tcvr.result_date,
+            tcvr.result,
+            GROUP_CONCAT(CONCAT('{medication:"', tdd.name, '", start_date: "', DATE(tdd.start_date),'", quantity:"',tdd.quantity,'"}')) medication
+          FROM temp_reg_outcome trc
+          INNER JOIN temp_current_patient_regimen tcp ON tcp.patient_id = trc.patient_id
+          INNER JOIN temp_drug_dispensed tdd ON tdd.patient_id = trc.patient_id
+          INNER JOIN person p ON p.person_id = trc.patient_id AND p.voided = 0
+          INNER JOIN person_name pn ON pn.person_id = p.person_id AND pn.voided = 0
+          LEFT JOIN patient_identifier i ON i.patient_id = p.person_id AND i.identifier_type = 4 AND i.voided = 0
+          LEFT JOIN temp_regimen_patient_weight tpw ON tpw.patient_id = trc.patient_id
+          LEFT JOIN temp_current_vl_results tcvr ON tcvr.patient_id = trc.patient_id
+          WHERE trc.outcome = 'On antiretrovirals'
+          GROUP BY trc.patient_id
         SQL
       end
 
-      def current_outcome
-        outcome = ", patient_outcome(o.patient_id, #{end_date}) outcome"
-        outcome = ", pepfar_patient_outcome(o.patient_id, #{end_date}) outcome" if type == 'pepfar'
-        outcome
-      end
-
-      def current_regimen
-        ", patient_current_regimen(o.patient_id, #{end_date}) regimen"
-      end
-
-      def drop_regimen_data
-        ActiveRecord::Base.connection.execute('drop table if exists tmp_latest_arv_dispensation ;')
-      end
-
-      def alive_clients(fields)
-        ActiveRecord::Base.connection.select_all <<~SQL
-          SELECT #{fields || '*'} FROM tmp_latest_arv_dispensation where outcome = 'On antiretrovirals'
+      def alive_clients
+        clients = ActiveRecord::Base.connection.select_all <<~SQL
+          SELECT trc.patient_id
+          FROM temp_reg_outcome trc
+          INNER JOIN person p ON p.person_id = trc.patient_id AND p.voided = 0
+          where trc.outcome = 'On antiretrovirals' AND LEFT(p.gender, 1) = 'F'
         SQL
+        clients.map { |client| client['patient_id'] }
       end
 
       def process_clients
         clients = {}
-        @vl_result = vl_result
         @maternal_status = maternal_status
-        alive_clients(nil).each do |client|
+        clients_alive_on_treatment.each do |client|
           clients[client['patient_id']] = {
             arv_number: client['arv_number'],
             given_name: client['given_name'],
             family_name: client['family_name'],
             birthdate: client['birthdate'],
-            gender: client['gender'] == 'M' ? 'M' : maternal_status(patient_id, demo['gender']),
+            gender: client['gender'] == 'M' ? 'M' : fetch_maternal_status(client['patient_id']),
             current_regimen: client['regimen'],
-            current_weight: current_weight(patient_id),
+            current_weight: client['weight'],
             art_start_date: client['earliest_start_date'],
-            medication: [],
-            vl_result: fetch_viral_load(client['patient_id']) ? @result['result'] : nil,
-            vl_result_date: @result ? @result['result_date'] : nil
+            medication: client['medication'].split(',').map(&:as_json) || [],
+            vl_result: client['result'],
+            vl_result_date: client['result_date']
           }
         end
-      end
-
-      def vl_result
-        ActiveRecord::Base.connection.select_all <<~SQL
-          SELECT lab_result_obs.obs_datetime AS result_date,
-          CONCAT (COALESCE(measure.value_modifier, '='),' ',COALESCE(measure.value_numeric, measure.value_text, '')) as result,
-          obs.person_id AS patient_id
-          FROM obs AS lab_result_obs
-          INNER JOIN orders
-            ON orders.order_id = lab_result_obs.order_id
-            AND orders.voided = 0
-          INNER JOIN obs AS measure
-            ON measure.obs_group_id = lab_result_obs.obs_id
-            AND measure.voided = 0
-          INNER JOIN (
-            SELECT concept_id, name
-            FROM concept_name
-            INNER JOIN concept USING (concept_id)
-            WHERE concept.retired = 0
-            AND name NOT LIKE 'Lab test result'
-            GROUP BY concept_id
-          ) AS measure_concept
-            ON measure_concept.concept_id = measure.concept_id
-          WHERE lab_result_obs.voided = 0
-          AND measure.person_id IN (SELECT patient_id FROM tmp_latest_arv_dispensation WHERE outcome = 'On antiretrovirals')
-          AND (measure.value_numeric IS NOT NULL || measure.value_text IS NOT NULL)
-          AND lab_result_obs.obs_datetime <= '#{@end_date.to_date.strftime('%Y-%m-%d 23:59:59')}'
-          ORDER BY lab_result_obs.obs_datetime DESC
-        SQL
+        clients
       end
 
       def maternal_status
         ARTService::Reports::Pepfar::ViralLoadCoverage2.new(start_date: @start_date,
-                                                            end_date: @end_date).vl_maternal_status(alive_clients('patient_id').map do |client|
-                                                                                                      client['patient_id']
-                                                                                                    end.uniq.join(',') || 0)
-      end
-
-      def fetch_viral_load(patient_id)
-        return nil if patient_id.blank?
-
-        # return the row where the patient_id is equal to the patient_id in the @viral_load array
-        @result = @viral_load.select { |row| row['patient_id'] == patient_id }.first
-        return nil if @result.blank?
-
-        @result
+                                                            end_date: @end_date).vl_maternal_status(alive_clients)
       end
 
       def fetch_maternal_status(patient_id)
@@ -261,18 +230,6 @@ module ARTService
         gender = 'FP' if @maternal_status[:FP].include?(patient_id)
         gender = 'FBf' if @maternal_status[:FBf].include?(patient_id)
         gender
-      end
-
-      def current_weight(patient_id)
-        weight_concept = ConceptName.find_by_name('Weight (kg)').concept_id
-        obs = Observation.where("person_id = ? AND concept_id = ?
-          AND obs_datetime <= ? AND (value_numeric IS NOT NULL OR value_text IS NOT NULL)",
-                                patient_id, weight_concept, @end_date.to_date.strftime('%Y-%m-%d 23:59:59'))\
-                         .order('obs_datetime DESC, date_created DESC')
-
-        return nil if obs.blank?
-
-        (obs.first.value_numeric.blank? ? obs.first.value_text : obs.first.value_numeric)
       end
     end
   end
