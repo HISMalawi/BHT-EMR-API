5b79b56df3f546be43f2e31587457d1128494f26
bryan <brianmsyamboza@gmail.com>
2023-02-28 14:02:37 +0200

    route to initialize sync

--- config/routes.rb
+++ config/routes.rb
@@ -377,4 +377,6 @@ Rails.application.routes.draw do
 
   get '/api/v1/data_cleaning_confirmation', to: 'api/v1/data_cleaning#view'
   post '/api/v1/data_cleaning_confirmation', to: 'api/v1/data_cleaning#create'
+
+  post 'api/v1/sync_to_ait', to: 'api/v1/patients#sync_to_ait'
 end
