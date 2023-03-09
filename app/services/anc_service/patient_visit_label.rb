355f4af514e89675d73b3b7c4c3904c09099e602
bryan <brianmsyamboza@gmail.com>
2023-03-09 15:02:37 +0200

    add printout for detailed obstetric history

--- app/services/anc_service/patient_visit_label.rb
+++ app/services/anc_service/patient_visit_label.rb
@@ -12,7 +12,7 @@ module ANCService
       end
 
       def print
-        self.print1 + self.print2
+        self.print1 + self.print2 + self.detailed_obstetric_history_label
       end
 
       def print1
@@ -405,13 +405,471 @@ module ANCService
               #   label.draw_text(use[m],710,(200 + (18 * m)),0,2,1,1,false)
               # }
 
-              label.draw_text(use.to_s,710,200,0,2,1,1,false)
+              label.draw_text(use.to_s,730,200,0,2,1,1,false)
 
             end
           end
 
           label.print(1)
       end
+
+      def active_range(date = Date.today)
+
+        current_range = {}
+
+        active_date = date
+
+        pregnancies = {};
+
+      # active_years = {}
+
+        abortion_check_encounter = self.patient.encounters.where(["encounter_type = ? AND encounter_datetime > ? AND DATE(encounter_datetime) <= ?",
+            EncounterType.find_by_name("PREGNANCY STATUS").encounter_type_id, date.to_date - 7.months, date.to_date]).order(["encounter_datetime DESC"]).first rescue nil
+
+        aborted = abortion_check_encounter.observations.collect{|ob| ob.answer_string.downcase.strip if ob.concept_id == ConceptName.find_by_name("PREGNANCY ABORTED").concept_id}.compact.include?("yes")  rescue false
+
+        date_aborted = abortion_check_encounter.observations.find_by_concept_id(ConceptName.find_by_name("DATE OF SURGERY").concept_id).answer_string rescue nil
+        recent_lmp = self.find_by_sql(["SELECT * from obs WHERE person_id = #{self.patient.id} AND concept_id =
+                            (SELECT concept_id FROM concept_name WHERE name = 'DATE OF LAST MENSTRUAL PERIOD' LIMIT 1)"]).last.answer_string.squish.to_date rescue nil
+
+        self.patient.encounters.order(["encounter_datetime DESC"]).each{|e|
+          if e.name == "CURRENT PREGNANCY" && !pregnancies[e.encounter_datetime.strftime("%Y-%m-%d")]
+            pregnancies[e.encounter_datetime.strftime("%Y-%m-%d")] = {}
+
+            e.observations.each{|o|
+              concept = o.concept.name rescue nil
+              if concept
+                # if !active_years[e.encounter_datetime.beginning_of_quarter.strftime("%Y-%m-%d")]
+                if o.concept_id == (ConceptName.find_by_name("DATE OF LAST MENSTRUAL PERIOD").concept_id rescue nil)
+                  pregnancies[e.encounter_datetime.strftime("%Y-%m-%d")]["DATE OF LAST MENSTRUAL PERIOD"] = o.answer_string.squish
+                  # active_years[e.encounter_datetime.beginning_of_quarter.strftime("%Y-%m-%d")] = true
+                end
+                # end
+              end
+            }
+          end
+        }
+
+        # pregnancies = pregnancies.delete_if{|x, v| v == {}}
+
+        pregnancies.each{|preg|
+          if preg[1]["DATE OF LAST MENSTRUAL PERIOD"]
+            preg[1]["START"] = preg[1]["DATE OF LAST MENSTRUAL PERIOD"].to_date
+            preg[1]["END"] = preg[1]["DATE OF LAST MENSTRUAL PERIOD"].to_date + 7.day + 45.week # 9.month
+          else
+            preg[1]["START"] = preg[0].to_date
+            preg[1]["END"] = preg[0].to_date + 7.day + 45.week # 9.month
+          end
+
+          if active_date >= preg[1]["START"] && active_date <= preg[1]["END"]
+            current_range["START"] = preg[1]["START"]
+            current_range["END"] = preg[1]["END"]
+          end
+        }
+
+        if recent_lmp.present?
+          current_range["START"] = recent_lmp
+          current_range["END"] = current_range["START"] + 9.months
+        end
+
+        if (abortion_check_encounter.present? && aborted && date_aborted.present? && current_range["START"].to_date < date_aborted.to_date rescue false)
+
+          current_range["START"] = date_aborted.to_date + 10.days
+          current_range["END"] = current_range["START"] + 9.months
+        end
+
+        current_range["END"] = current_range["START"] + 7.day + 45.week unless ((current_range["START"]).to_date.blank? rescue true)
+
+        return [current_range, pregnancies]
+    end
+
+    def truncate(string, length = 6)
+      string[0,length] + "."
+    end
+
+    def detailed_obstetric_history_label(date = Date.today)
+      @patient = self.patient rescue nil
+
+      @obstetrics = {}
+      search_set = ["YEAR OF BIRTH", "PLACE OF BIRTH", "BIRTHPLACE", "PREGNANCY", "GESTATION", "LABOUR DURATION",
+        "METHOD OF DELIVERY", "CONDITION AT BIRTH", "BIRTH WEIGHT", "ALIVE",
+        "AGE AT DEATH", "UNITS OF AGE OF CHILD", "PROCEDURE DONE"]
+      current_level = 0
+
+      @new_encounter = @patient.encounters.joins([:observations]).where(["encounter_type = ? AND comments regexp 'p'",
+          EncounterType.find_by_name("OBSTETRIC HISTORY")]).last
+
+      if @new_encounter.blank?
+        Encounter.where(["encounter_type = ? AND patient_id = ?",
+            EncounterType.find_by_name("OBSTETRIC HISTORY").id, @patient.id]).order(["encounter_datetime ASC"]).each{|e|
+          e.observations.each{|obs|
+            concept = obs.concept.concept_names.map(& :name).last rescue nil
+            if(!concept.nil?)
+              if search_set.include?(concept.upcase)
+                if obs.concept_id == (ConceptName.find_by_name("YEAR OF BIRTH").concept_id rescue nil)
+                  current_level += 1
+
+                  @obstetrics[current_level] = {}
+                end
+
+                if @obstetrics[current_level]
+                  @obstetrics[current_level][concept.upcase] = obs.answer_string rescue nil
+
+                  if obs.concept_id == (ConceptName.find_by_name("YEAR OF BIRTH").concept_id rescue nil) && obs.answer_string.to_i == 0
+                    @obstetrics[current_level]["YEAR OF BIRTH"] = "Unknown"
+                  end
+                end
+
+              end
+            end
+          }
+        }
+      else
+
+        @data = {}
+        @new_encounter.observations.each do |obs|
+
+
+          next if !(obs.comments || "").match(/p/i)
+          p = obs.comments.match(/p\d+/i)[0].match(/\d+/)[0]
+          n = obs.comments.match(/b\d+/i)[0].match(/\d+/)[0]
+          @data[p] = {} if @data[p].blank?
+          @data[p][n] = {} if @data[p][n].blank?
+          concept = obs.concept.concept_names.map(& :name).last rescue nil
+          @data[p][n][concept.upcase.strip] = obs.answer_string
+        end
+
+        current_level = 1
+        @data.keys.sort.each do |prg|
+
+          @data[prg].keys.sort.each do |key|
+
+            @obstetrics[current_level] = @data[prg][key]
+            current_level += 1
+          end
+        end
+      end
+
+      #grab units of age of child
+      unit = Observation.where(["person_id = ? AND concept_id = ?", @patient.id,
+          ConceptName.find_by_name('UNITS OF AGE OF CHILD').concept_id]).last.answer_string.squish rescue nil
+
+
+      # raise @anc_patient.to_yaml
+
+      @pregnancies = self.active_range
+
+      @range = []
+
+      @pregnancies = @pregnancies[1]
+
+      @pregnancies.each{|preg|
+        @range << preg[0].to_date
+      }
+
+      @range = @range.sort
+
+      @range.each{|y|
+        current_level += 1
+        @obstetrics[current_level] = {}
+        @obstetrics[current_level]["YEAR OF BIRTH"] = y.year
+        @obstetrics[current_level]["PLACE OF BIRTH"] = "<b>(Here)</b>"
+      }
+
+      label = ZebraPrinter::StandardLabel.new
+      label2 = ZebraPrinter::StandardLabel.new
+      label2set = false
+      label3 = ZebraPrinter::StandardLabel.new
+      label3set = false
+
+      label.draw_text("Detailed Obstetric History",28,29,0,1,1,2,false)
+      label.draw_text("Pr.",35,65,0,2,1,1,false)
+      label.draw_text("No.",35,85,0,2,1,1,false)
+      label.draw_text("Year",59,65,0,2,1,1,false)
+      label.draw_text("Place",110,65,0,2,1,1,false)
+      label.draw_text("Gest.",225,65,0,2,1,1,false)
+      label.draw_text("months",223,85,0,2,1,1,false)
+      #label.draw_text("Labour",305,65,0,2,1,1,false)
+      #label.draw_text("durat.",305,85,0,2,1,1,false)
+      #label.draw_text("(hrs)",310,105,0,2,1,1,false)
+      label.draw_text("Delivery",310,65,0,2,1,1,false)
+      label.draw_text("Method",310,85,0,2,1,1,false)
+      label.draw_text("Condition",430,65,0,2,1,1,false)
+      label.draw_text("at birth",430,85,0,2,1,1,false)
+      label.draw_text("Birth",552,65,0,2,1,1,false)
+      label.draw_text("weight",547,85,0,2,1,1,false)
+      label.draw_text("(kg)",550,105,0,2,1,1,false)
+      label.draw_text("Alive.",643,65,0,2,1,1,false)
+      label.draw_text("now?",645,85,0,2,1,1,false)
+      label.draw_text("Age at",715,65,0,2,1,1,false)
+      label.draw_text("death*",715,85,0,2,1,1,false)
+      label.draw_text("(" + unit[0..2] + ".)", 745,105,0,2,1,1,false) if unit
+
+
+
+      label.draw_line(20,60,800,2,0)
+      label.draw_line(20,60,2,245,0)
+      label.draw_line(20,305,800,2,0)
+      label.draw_line(805,60,2,245,0)
+      label.draw_line(20,125,800,2,0)
+
+      label.draw_line(56,60,2,245,0)
+      label.draw_line(105,60,2,245,0)
+      label.draw_line(220,60,2,245,0)
+      label.draw_line(295,60,2,245,0)
+      #label.draw_line(380,60,2,245,0)
+      label.draw_line(415,60,2,245,0)
+      label.draw_line(535,60,2,245,0)
+      label.draw_line(643,60,2,245,0)
+      label.draw_line(700,60,2,245,0)
+
+      (1..(@obstetrics.length + 1)).each do |pos|
+
+        @place = (@obstetrics[pos] ? (@obstetrics[pos]["BIRTHPLACE"] ?
+              @obstetrics[pos]["BIRTHPLACE"] : "") : "").gsub(/Centre/i,
+          "C.").gsub(/Health/i, "H.").gsub(/Center/i, "C.")
+
+        @gest = (@obstetrics[pos] ? (@obstetrics[pos]["GESTATION"] ?
+              @obstetrics[pos]["GESTATION"] : "") : "")
+
+        @gest = (@gest.length > 5 ? truncate(@gest, 5) : @gest)
+
+        @delmode = (@obstetrics[pos] ? (@obstetrics[pos]["METHOD OF DELIVERY"] ?
+              @obstetrics[pos]["METHOD OF DELIVERY"].titleize : (@obstetrics[pos]["PROCEDURE DONE"] ?
+                @obstetrics[pos]["PROCEDURE DONE"] : "")) : "").gsub(/Spontaneous\sVaginal\sDelivery/i,
+          "S.V.D.").gsub(/Caesarean\sSection/i, "C-Section").gsub(/Vacuum\sExtraction\sDelivery/i,
+          "Vac. Extr.").gsub("(MVA)", "").gsub(/Manual\sVacuum\sAspiration/i,
+          "M.V.A.").gsub(/Evacuation/i, "Evac")
+
+        @labor = (@obstetrics[pos] ? (@obstetrics[pos]["LABOUR DURATION"] ?
+              @obstetrics[pos]["LABOUR DURATION"] : "") : "")
+
+        @labor = (@labor.length > 5 ? truncate(@labor,5) : @labor)
+
+        @cond = (@obstetrics[pos] ? (@obstetrics[pos]["CONDITION AT BIRTH"] ?
+              @obstetrics[pos]["CONDITION AT BIRTH"] : "") : "").titleize
+
+        @birt_weig = (@obstetrics[pos] ? (@obstetrics[pos]["BIRTH WEIGHT"] ?
+              @obstetrics[pos]["BIRTH WEIGHT"] : "") : "")
+        @birt_weig = @birt_weig.match(/Small/i) ? "<2.5 kg" : (@birt_weig.match(/Big/i) ? ">4.5 kg" : @birt_weig)
+
+        @dea = (@obstetrics[pos] ? (@obstetrics[pos]["AGE AT DEATH"] ?
+              (@obstetrics[pos]["AGE AT DEATH"].to_s.match(/\.[1-9]/) ? @obstetrics[pos]["AGE AT DEATH"].to_s :
+                @obstetrics[pos]["AGE AT DEATH"].to_s) : "") : "").to_s +
+          (@obstetrics[pos] ? (@obstetrics[pos]["UNITS OF AGE OF CHILD"] ?
+              @obstetrics[pos]["UNITS OF AGE OF CHILD"] : "") : "")
+
+        if pos <= 3
+
+          label.draw_text(pos.to_s,28,(85 + (60 * pos)),0,2,1,1,false)
+
+          label.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["YEAR OF BIRTH"] ?
+                  (@obstetrics[pos]["YEAR OF BIRTH"].to_i > 0 ? @obstetrics[pos]["YEAR OF BIRTH"].to_i :
+                    "????") : "") : ""),58,(70 + (60 * pos)),0,2,1,1,false)
+
+          if @place.length < 9
+            label.draw_text(@place,111,(70 + (60 * pos)),0,2,1,1,false)
+          else
+            @place = paragraphate(@place)
+
+            (0..(@place.length)).each{|p|
+              label.draw_text(@place[p].to_s,111,(70 + (60 * pos) + (18 * p)),0,2,1,1,false)
+            }
+          end
+
+          label.draw_text(@gest,225,(70 + (60 * pos)),0,2,1,1,false)
+
+          #label.draw_text(@labor,300,(70 + (60 * pos)),0,2,1,1,false)
+
+          label.draw_text(@delmode,300,(70 + (60 * pos)),0,2,1,1,false)
+
+          label.draw_text(@cond,420,(70 + (60 * pos)),0,2,1,1,false)
+
+          label.draw_text(@birt_weig,539,(70 + (60 * pos)),0,2,1,1,false)
+
+          label.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["ALIVE"] ?
+                  @obstetrics[pos]["ALIVE"] : "") : ""),647,(70 + (60 * pos)),0,2,1,1,false)
+
+          if @dea.length < 10
+            label.draw_text(@dea,708,(70 + (60 * pos)),0,2,1,1,false)
+          else
+            @dea = paragraphate(@dea, 4)
+
+            (0..(@dea.length)).each{|p|
+              label.draw_text(@dea[p],708,(70 + (60 * pos) + (18 * p)),0,2,1,1,false)
+            }
+          end
+
+          label.draw_line(20,((135 + (45 * pos)) <= 305 ? (125 + (60 * pos)) : 305),800,2,0)
+
+        elsif pos >= 4 && pos <= 8
+          if pos == 4
+            label2.draw_line(20,30,800,2,0)
+            label2.draw_line(20,30,2,275,0)
+            label2.draw_line(20,305,800,2,0)
+            label2.draw_line(805,30,2,275,0)
+
+            label2.draw_line(55,30,2,275,0)
+            label2.draw_line(105,30,2,275,0)
+            label2.draw_line(220,30,2,275,0)
+            label2.draw_line(295,30,2,275,0)
+            label2.draw_line(380,30,2,275,0)
+            label2.draw_line(510,30,2,275,0)
+            label2.draw_line(615,30,2,275,0)
+            label2.draw_line(683,30,2,275,0)
+            label2.draw_line(740,30,2,275,0)
+          end
+          label2.draw_text(pos,28,((55 * (pos - 3))),0,2,1,1,false)
+
+          label2.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["YEAR OF BIRTH"] ?
+                  (@obstetrics[pos]["YEAR OF BIRTH"].to_i > 0 ? @obstetrics[pos]["YEAR OF BIRTH"].to_i :
+                    "????") : "") : ""),58,((55 * (pos - 3)) - 13),0,2,1,1,false)
+
+          if @place.length < 8
+            label2.draw_text(@place,111,((55 * (pos - 3)) - 13),0,2,1,1,false)
+          else
+            @place = paragraphate(@place)
+
+            (0..(@place.length)).each{|p|
+              label2.draw_text(@place[p],111,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label2.draw_text(@gest,225,((55 * (pos - 3)) - 13),0,2,1,1,false)
+
+          label2.draw_text(@labor,300,((55 * (pos - 3)) - 13),0,2,1,1,false)
+
+          label2.draw_text(@delmode,385,(55 * (pos - 3)),0,2,1,1,false)
+
+          if @cond.length < 6
+            label2.draw_text(@cond,515,((55 * (pos - 3)) - 13),0,2,1,1,false)
+          else
+            @cond = paragraphate(@cond, 6)
+
+            (0..(@cond.length)).each{|p|
+              label2.draw_text(@cond[p],515,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          if @birt_weig.length < 6
+            label2.draw_text(@birt_weig,620,((55 * (pos - 3)) - 13),0,2,1,1,false)
+          else
+            @birt_weig = paragraphate(@birt_weig, 4)
+
+            (0..(@birt_weig.length)).each{|p|
+              label2.draw_text(@birt_weig[p],620,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label2.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["ALIVE"] ?
+                  @obstetrics[pos]["ALIVE"] : "") : ""),687,((55 * (pos - 3)) - 13),0,2,1,1,false)
+
+          if @dea.length < 10
+            label2.draw_text(@dea,745,((55 * (pos - 3)) - 13),0,2,1,1,false)
+          else
+            @dea = paragraphate(@dea, 4)
+
+            (0..(@dea.length)).each{|p|
+              label2.draw_text(@dea[p],745,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label2.draw_line(20,(((55 * (pos - 3)) + 35) <= 305 ? ((55 * (pos - 3)) + 35) : 305),800,2,0)
+          label2set = true
+        else
+          if pos == 9
+            label3.draw_line(20,30,800,2,0)
+            label3.draw_line(20,30,2,275,0)
+            label3.draw_line(20,305,800,2,0)
+            label3.draw_line(805,30,2,275,0)
+
+            label3.draw_line(55,30,2,275,0)
+            label3.draw_line(105,30,2,275,0)
+            label3.draw_line(220,30,2,275,0)
+            label3.draw_line(295,30,2,275,0)
+            label3.draw_line(380,30,2,275,0)
+            label3.draw_line(510,30,2,275,0)
+            label3.draw_line(615,30,2,275,0)
+            label3.draw_line(683,30,2,275,0)
+            label3.draw_line(740,30,2,275,0)
+          end
+          label3.draw_text(pos,28,((55 * (pos - 8))),0,2,1,1,false)
+
+          label3.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["YEAR OF BIRTH"] ?
+                  (@obstetrics[pos]["YEAR OF BIRTH"].to_i > 0 ? @obstetrics[pos]["YEAR OF BIRTH"].to_i :
+                    "????") : "") : ""),58,((55 * (pos - 8)) - 13),0,2,1,1,false)
+
+          if @place.length < 8
+            label3.draw_text(@place,111,((55 * (pos - 8)) - 13),0,2,1,1,false)
+          else
+            @place = paragraphate(@place)
+
+            (0..(@place.length)).each{|p|
+              label3.draw_text(@place[p],111,(55 * (pos - 8) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label3.draw_text(@gest,225,((55 * (pos - 8)) - 13),0,2,1,1,false)
+
+          label3.draw_text(@labor,300,((55 * (pos - 8)) - 13),0,2,1,1,false)
+
+          if @delmode.length < 11
+            label3.draw_text(@delmode,385,(55 * (pos - 8)),0,2,1,1,false)
+          else
+            @delmode = paragraphate(@delmode)
+
+            (0..(@delmode.length)).each{|p|
+              label3.draw_text(@delmode[p],385,(55 * (pos - 8) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          if @cond.length < 6
+            label3.draw_text(@cond,515,((55 * (pos - 8)) - 13),0,2,1,1,false)
+          else
+            @cond = paragraphate(@cond, 6)
+
+            (0..(@cond.length)).each{|p|
+              label3.draw_text(@cond[p],515,(55 * (pos - 8) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          if @birt_weig.length < 6
+            label3.draw_text(@birt_weig,620,(70 + (60 * pos)),0,2,1,1,false)
+          else
+            @birt_weig = paragraphate(@birt_weig, 4)
+
+            (0..(@birt_weig.length)).each{|p|
+              label3.draw_text(@birt_weig[p],620,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label3.draw_text((@obstetrics[pos] ? (@obstetrics[pos]["ALIVE"] ?
+                  @obstetrics[pos]["ALIVE"] : "") : ""),687,((55 * (pos - 8)) - 13),0,2,1,1,false)
+
+          if @dea.length < 6
+            label3.draw_text(@dea,745,((55 * (pos - 3)) - 13),0,2,1,1,false)
+          else
+            @dea = paragraphate(@dea, 4)
+
+            (0..(@dea.length)).each{|p|
+              label3.draw_text(@dea[p],745,(55 * (pos - 3) + (18 * p))-17,0,2,1,1,false)
+            }
+          end
+
+          label3.draw_line(20,(((55 * (pos - 8)) + 35) <= 305 ? ((55 * (pos - 8)) + 35) : 305),800,2,0)
+          label3set = true
+        end
+
+      end
+
+      if label3set
+        label.print(1) + label2.print(1) + label3.print(1)
+      elsif label2set
+        label.print(1) + label2.print(1)
+      else
+        label.print(1)
+    end
+  end
 
       private
 
