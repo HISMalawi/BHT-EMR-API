7a5bf22e4b10e853c0ae34264d42a9c6c760a8af
bryan <brianmsyamboza@gmail.com>
2023-03-24 01:58:50 +0200

    modify patient query

--- app/services/art_service/reports/master_card/mastercard_struct.rb
+++ app/services/art_service/reports/master_card/mastercard_struct.rb
@@ -41,11 +41,11 @@ module ARTService::Reports::MasterCard
           relationship_type.b_is_to_a as guardian_relation,
           person_attribute.value as patient_phone,
           CONCAT(person_address.township_division, ', ', person_address.city_village, ', ', person_address.state_province) as physical_address,
-          min(guardian_phone.value) as guardian_phone FROM `person` INNER JOIN `person_name` ON `person_name`.`voided` = 0 AND `person_name`.`person_id` = `person`.`person_id` INNER JOIN `person_address` ON `person_address`.`voided` = 0 AND `person_address`.`person_id` = `person`.`person_id` INNER JOIN `relationship` ON `relationship`.`voided` = 0 AND `relationship`.`person_a` = `person`.`person_id` INNER JOIN `person_attribute` ON `person_attribute`.`voided` = 0 AND `person_attribute`.`person_id` = `person`.`person_id` INNER JOIN person_name guardian ON guardian.person_id = relationship.person_b
+          min(guardian_phone.value) as guardian_phone FROM `person` LEFT JOIN `person_name` ON `person_name`.`voided` = 0 AND `person_name`.`person_id` = `person`.`person_id` LEFT JOIN `person_address` ON `person_address`.`voided` = 0 AND `person_address`.`person_id` = `person`.`person_id` LEFT JOIN `relationship` ON `relationship`.`voided` = 0 AND `relationship`.`person_a` = `person`.`person_id` LEFT JOIN `person_attribute` ON `person_attribute`.`voided` = 0 AND `person_attribute`.`person_id` = `person`.`person_id` LEFT JOIN person_name guardian ON guardian.person_id = relationship.person_b
           AND guardian.voided = 0
-          INNER JOIN relationship_type ON relationship_type.relationship_type_id = relationship.relationship
+          LEFT JOIN relationship_type ON relationship_type.relationship_type_id = relationship.relationship
           AND relationship_type.retired = 0
-          INNER JOIN person_attribute guardian_phone ON `guardian_phone`.`voided` = 0
+          LEFT JOIN person_attribute guardian_phone ON `guardian_phone`.`voided` = 0
           AND guardian_phone.person_id = `guardian`.`person_id` WHERE `person`.`voided` = 0 AND `person`.`person_id` = #{patient.id} AND `person_attribute`.`person_attribute_type_id` = 12"
       ).to_hash.first
     end
