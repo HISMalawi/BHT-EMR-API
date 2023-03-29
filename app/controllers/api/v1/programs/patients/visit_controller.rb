b7c4d765bb945c08f946c4704e1e246a7bc3e1e4
bryan <brianmsyamboza@gmail.com>
2023-03-29 09:57:31 +0200

    pass card type in request
    added peds card

--- app/controllers/api/v1/programs/patients/visit_controller.rb
+++ app/controllers/api/v1/programs/patients/visit_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Programs::Patients::VisitController < ApplicationController
-
-  require './app/services/art_service/reports/master_card/mastercard_struct.rb'
+  require "./app/services/art_service/reports/master_card/mastercard_struct.rb"
 
   def index
     permitted = params.permit(:date)
@@ -20,59 +19,58 @@ class Api::V1::Programs::Patients::VisitController < ApplicationController
   end
 
   def patient_visits
-    patients = params[:patient_ids].collect { |id| patient(id) }    
-    htmls = patients.collect do | patient |
-
-      mastercard_service = patient_mastercard_service(patient)
-      mastercard_service = mastercard_service.patient_is_a_pediatric? ? ped_patient_mastercard_service(patient) : mastercard_service
+    patients = params[:patient_ids].collect { |id| patient(id) }
+    card_type = params[:card_type]
+    htmls = patients.collect do |patient|
+      mastercard_service = patient_mastercard_service(patient, card_type)
 
       patient_details = mastercard_service.fetch
 
-      visits_dates = patient_service.find_patient_visit_dates(patient, program, params[:include_defaulter_dates] == 'true')      
+      visits_dates = patient_service.find_patient_visit_dates(patient, program, true)
 
       visits_dates = visits_dates.sort! { |a, b| b.to_date <=> a.to_date }.reverse
 
       filtred_dates = [visits_dates[0]]
       filtred_dates += visits_dates.last(5)
 
-      patient_details[:visits] = filtred_dates.collect do | date |
-        {date: date}.merge(visit_summary(patient.id, date).as_json)
+      patient_details[:visits] = filtred_dates.collect do |date|
+        { date: date }.merge(visit_summary(patient.id, date).as_json)
       end
 
       @data = patient_details
-      template = File.read(Rails.root.join('app', 'views', 'layouts', 'patient_card.html.erb'))
+      template = patient_card card_type
       html = ERB.new(template).result(binding)
-      
-      html
+
+      {html: html}
     end
     render json: htmls
   end
 
   private
 
-  def patient_mastercard_service patient
+  def patient_card(card_type)
+    File.read(Rails.root.join("app", "views", "layouts", "#{card_type == "child" ? "ped_patient_card" : "patient_card"}.html.erb"))
+  end
+
+  def patient_mastercard_service(patient, card_type)
     ARTService::Reports::MasterCard::PatientStruct.new(patient)
   end
-  
-  def ped_patient_mastercard_service patient
-    ARTService::Reports::MasterCard::PediatricCardStruct.new(patient)
-  end
-  
+
   def service
     ProgramServiceLoader
-      .load(Program.find(params[:program_id]), 'PatientsEngine')
+      .load(Program.find(params[:program_id]), "PatientsEngine")
       .new
   end
 
   def patient_service
-   PatientService.new
+    PatientService.new
   end
 
   def patient(patient_id)
     Patient.find(patient_id)
   end
 
-  def program 
+  def program
     Program.find(params[:program_id])
   end
 end
