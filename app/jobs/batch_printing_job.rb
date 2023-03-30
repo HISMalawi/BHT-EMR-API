a84f76561fd6bbd2caebb5bdd0cf1aa4d1e90207
bryan <brianmsyamboza@gmail.com>
2023-03-30 11:31:46 +0200

    modify batch printing job

--- app/jobs/batch_printing_job.rb
+++ app/jobs/batch_printing_job.rb
@@ -2,37 +2,36 @@ class BatchPrintingJob < ActiveJob::Base
   queue_as :default
 
   def perform(patients)
-    batch = 0
+    processed = 0
     program = Program.find(1)
     patient_visit_service = service(program)
     patients.each_slice(2) do |patients_data|
-    batch += 2
-    htmls = patients.collect do | patient |
+      htmls = patients_data.collect do |patient|
+        mastercard_service = patient_mastercard_service(patient)
+        is_peds = mastercard_service.patient_is_a_pediatric?
+        mastercard_service = is_peds ? ped_patient_mastercard_service(patient) : mastercard_service
 
-      mastercard_service = patient_mastercard_service(patient)
-      is_peds = mastercard_service.patient_is_a_pediatric?
-      mastercard_service = is_peds ? ped_patient_mastercard_service(patient) : mastercard_service
+        patient_details = mastercard_service.fetch
 
-      patient_details = mastercard_service.fetch
+        visits_dates = patient_service.find_patient_visit_dates(patient, program, true)
 
-      visits_dates = patient_service.find_patient_visit_dates(patient, program, true)      
+        visits_dates = visits_dates.sort! { |a, b| b.to_date <=> a.to_date }.reverse
 
-      visits_dates = visits_dates.sort! { |a, b| b.to_date <=> a.to_date }.reverse
+        filtred_dates = [visits_dates[0]]
+        filtred_dates += visits_dates.last(5)
 
-      filtred_dates = [visits_dates[0]]
-      filtred_dates += visits_dates.last(5)
+        patient_details[:visits] = filtred_dates.collect do |date|
+          { date: date }.merge(patient_visit_service.patient_visit_summary(patient.id, date).as_json)
+        end
 
-      patient_details[:visits] = filtred_dates.collect do | date |
-        {date: date}.merge(patient_visit_service.patient_visit_summary(patient.id, date).as_json)
+        @data = patient_details
+        template = File.read(Rails.root.join("app", "views", "layouts", is_peds ? "ped_patient_card.html.erb" : "patient_card.html.erb"))
+        html = ERB.new(template).result(binding)
+
+        processed += 1
+          { html: html }
       end
-
-      @data = patient_details
-      template = File.read(Rails.root.join('app', 'views', 'layouts', is_peds ? 'ped_patient_card.html.erb' : 'patient_card.html.erb'))
-      html = ERB.new(template).result(binding)
-      
-      {html: html}
-      end
-      ActionCable.server.broadcast "printing_channel", {batch: batch, total: patients.length, data: htmls}
+      ActionCable.server.broadcast "printing_channel", { processed: processed, total: patients.length, data: htmls }
     end
   end
 
