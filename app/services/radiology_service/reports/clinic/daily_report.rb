05d3ba88699fe9e76ce37b9a15fae3ff2ce8e52e
Roy Chanunkha <8115806+Skipper-116@users.noreply.github.com>
2023-04-05 16:21:41 +0200

    fixes to the daily report (#322)

--- app/services/radiology_service/reports/clinic/daily_report.rb
+++ app/services/radiology_service/reports/clinic/daily_report.rb
@@ -11,34 +11,21 @@ module RadiologyService
         end
 
         def data
-          exam_line = []
-          report.each do |ob|
-            exam_line << {
-              exam_name: Concept.find_by_concept_id(ob['order_concept_id']).shortname,
-              exam_value_name: Concept.find_by_concept_id(ob['exam_value_coded']).shortname,
-              exam_total: ob['exam_total']
-            }
-          end
-          exam_line
+          report
         end
 
         private
 
         def report
           ActiveRecord::Base.connection.select_all <<~SQL
-            SELECT od.concept_id as order_concept_id,o.value_coded as exam_value_coded,COUNT(o.value_coded) as exam_total
-            FROM obs o
-            INNER JOIN orders od ON od.encounter_id = o.encounter_id
-            INNER JOIN encounter en ON en.encounter_id = o.encounter_id
-            AND od.order_type_id = #{radiology_order_type_id}
-            AND o.concept_id = #{examination_concept_id}
-            AND en.encounter_type = #{radiology_encounter_id}
-            AND od.concept_id IN (SELECT concept_id FROM concept_name WHERE name IN ('ULTRASOUND','MAMMOGRAPHY','MRI SCAN','CT SCAN','XRAY', 'BONE DENSITOMETRY') AND voided = 0)
-            AND od.voided = 0
-            AND o.voided = 0
-            AND DATE(o.obs_datetime) BETWEEN '#{@start_date}' AND '#{@end_date}'
-            GROUP BY od.concept_id,o.value_coded
-            ORDER BY od.concept_id DESC
+            SELECT ord.concept_id, o.value_coded, cn.name exam_name, vcn.name exam_value_name, count(DISTINCT(ord.order_id)) exam_total
+            FROM orders ord
+            INNER JOIN concept_name cn ON cn.concept_id = ord.concept_id AND cn.voided = 0
+            INNER JOIN obs o ON o.order_id = ord.order_id AND o.voided = 0 AND o.concept_id = 1336 -- examination concept
+            INNER JOIN concept_name vcn ON vcn.concept_id = o.value_coded AND vcn.voided = 0 AND vcn.name IS NOT NULL AND vcn.name != ''
+            WHERE ord.concept_id IN (SELECT concept_id FROM concept_name WHERE name IN ('ULTRASOUND','MAMMOGRAPHY','MRI SCAN','CT SCAN','XRAY', 'BONE DENSITOMETRY') AND voided = 0)
+            AND ord.start_date BETWEEN '#{@start_date}' AND '#{@end_date}'
+            GROUP BY ord.concept_id, o.value_coded
           SQL
         end
 
