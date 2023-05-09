cab0516fc99e630b8f3be8cf0594b404ecfbc011
bryan <brianmsyamboza@gmail.com>
2023-05-09 17:07:48 +0200

    refactor the returning data structure of the LEGACY report to adhere to frontend template

--- app/services/hts_service/reports/clinic/hts_legacy_returning.rb
+++ app/services/hts_service/reports/clinic/hts_legacy_returning.rb
@@ -50,31 +50,40 @@ module HtsService
           partner_present
           outcome_summary
           result_given_to_client
-          report.keys.each do |key|
-            data = report.delete(key)
-            report["community_#{key}"] = data.select { |q| q['access_type'] == 'Community' }.map { |q| q['person_id'] }
-            report["facility_#{key}"] = data.select { |q| q['access_type'] == 'Health facility'}.map { |q| q['person_id'] }
+          r = report.deep_dup
+          (start_date.month..end_date.month).each_with_index do |month, index|
+            r.each_key do |key|
+              report[key] ||= {}
+              r[key].each_key do |k|
+                data = r[key][k]
+                report[key]["month_#{index + 1}"] ||= {}
+                report[key]["month_#{index + 1}"]["community_#{k}"] = data.select { |q| q['access_type'] == 'Community' && q['encounter_datetime'].month == month }.map { |q| q['person_id'] }
+                report[key]["month_#{index + 1}"]["health_facility_#{k}"] = data.select { |q| q['access_type'] == 'Health facility' && q['encounter_datetime'].month == month}.map { |q| q['person_id'] }
+                report[key].delete(k)
+              end
+            end
           end
           report
         end
 
         def filter_gender(key, data)
-          report.merge!({
+          report.merge!({ result_given_to_client: {
                           "#{key}_male": data.select { |q| q['gender'] == 'M' },
                           "#{key}_female": data.select { |q| q['gender'] == 'F' }
-                        })
+                      }})
         end
 
         def result_given_to_client
-          data = connection.select_all(ObsValueScope.call(model: query, name: 'result_given',
-                                                          concept_id: HIV_GROUP)).to_hash
+          data = connection.select_all(ObsValueScope.call(model: query, name: 'result_given', concept_id: HIV_GROUP)).to_hash
           filter_gender('new_negative', filter_hash(data, 'result_given', NEW_NEGATIVE))
           filter_gender('new_positive', filter_hash(data, 'result_given', NEW_POSITIVE))
           filter_gender('confirmatory_positive', filter_hash(data, 'result_given', CONFIRMATORY_POSITIVE))
           report.merge!({
-                          new_exposed_infant: filter_hash(data, 'result_given', NEW_EXPOSED_INFANT),
-                          new_inconclusive: filter_hash(data, 'result_given', NEW_INCONCLUSIVE),
-                          confirmatory_inconclusive: filter_hash(data, 'result_given', CONFIRMATORY_INCONCLUSIVE),
+                          result_given_to_client: {
+                            new_exposed_infant: filter_hash(data, 'result_given', NEW_EXPOSED_INFANT),
+                            new_inconclusive: filter_hash(data, 'result_given', NEW_INCONCLUSIVE),
+                            confirmatory_inconclusive: filter_hash(data, 'result_given', CONFIRMATORY_INCONCLUSIVE)
+                          }
                         })
         end
 
@@ -88,34 +97,51 @@ module HtsService
             ).group('person.person_id')
           ).to_hash
           report.merge!({
-                          single_negative: filter_hash(data, 'test_one', HIV_NEGATIVE),
-                          single_positive: filter_hash(data, 'test_one', HIV_POSITIVE),
-                          one_and_two_positive: filter_hash(data, %w[test_one test_two], HIV_POSITIVE),
-                          one_and_two_negative: filter_hash(data, %w[test_one test_two], HIV_NEGATIVE),
-                          one_and_two_disc: data.select do |q|
-                            q['test_one'] == HIV_POSITIVE && q['test_two'] == HIV_NEGATIVE && q['test_three'] == HIV_POSITIVE || q['test_one'] == HIV_POSITIVE && q['test_two'] == HIV_POSITIVE && q['test_three'] == HIV_NEGATIVE
-                          end
+                          outcome_summary: {
+                            single_neg: filter_hash(data, 'test_one', HIV_NEGATIVE),
+                            single_pos: filter_hash(data, 'test_one', HIV_POSITIVE),
+                            one_and_two_neg: filter_hash(data, %w[test_one test_two], HIV_NEGATIVE),
+                            one_and_two_pos: filter_hash(data, %w[test_one test_two], HIV_POSITIVE),
+                            one_and_two_disc: data.select do |q|
+                              q['test_one'] == HIV_POSITIVE && q['test_two'] == HIV_NEGATIVE && q['test_three'] == HIV_POSITIVE || q['test_one'] == HIV_POSITIVE && q['test_two'] == HIV_POSITIVE && q['test_three'] == HIV_NEGATIVE
+                            end
+                          }
                         })
         end
 
         def partner_present
-          data = connection.select_all(ObsValueScope.call(model: query, name: 'partner_present',
-                                                          concept_id: PARTENER_PRESENT, value: 'value_text')).to_hash
+          data = connection.select_all(
+            ObsValueScope.call(
+              model: query,
+              name: 'partner_present',
+              concept_id: PARTENER_PRESENT,
+              value: 'value_text'
+            )
+          ).to_hash
           report.merge!({
-                          partner_present: filter_hash(data, 'partner_present', 'Yes'),
-                          partner_not_present: filter_hash(data, 'partner_present', 'No')
+                          partner_present: {
+                            present: filter_hash(data, 'partner_present', 'Yes'),
+                            not_present: filter_hash(data, 'partner_present', 'No')
+                         }
                         })
         end
 
         def last_tested
-          data = connection.select_all(ObsValueScope.call(model: query, name: 'last_tested',
-                                                          concept_id: LAST_TESTED)).to_hash
+          data = connection.select_all(
+            ObsValueScope.call(
+              model: query,
+              name: 'last_tested',
+              concept_id: LAST_TESTED
+            )
+          ).to_hash
           report.merge!({
-                          last_never_tested: filter_hash(data, 'last_tested', HIV_NEVER_TESTED),
-                          last_negative: filter_hash(data, 'last_tested', HIV_NEGATIVE),
-                          last_positive: filter_hash(data, 'last_tested', HIV_POSITIVE),
-                          last_exposed_infant: filter_hash(data, 'last_tested', HIV_EXPOSED_INFANT),
-                          last_inconclusive: filter_hash(data, 'last_tested', HIV_INVALID_OR_INCONCLUSIVE),
+                          last_test:{
+                            never_tested: filter_hash(data, 'last_tested', HIV_NEVER_TESTED),
+                            negative: filter_hash(data, 'last_tested', HIV_NEGATIVE),
+                            positive: filter_hash(data, 'last_tested', HIV_POSITIVE),
+                            exposed_infant: filter_hash(data, 'last_tested', HIV_EXPOSED_INFANT),
+                            inconclusive: filter_hash(data, 'last_tested', HIV_INVALID_OR_INCONCLUSIVE)
+                          }
                         })
         end
 
@@ -125,23 +151,28 @@ module HtsService
           data = connection.select_all(query_two.group('person.person_id')).to_hash
 
           report.merge!({
-                          pitc: data.select { |q|
+                          access_type: {
+                            pitc: data.select { |q|
                                   ['ANC first visit', 'Inpatient', 'STI', 'PMTCT FUP', 'Peadiatric',
                                    'VMMC', 'Malnutrition', 'TB', 'OPD', 'Other PITC'].include?(q['test_location']) },
-                          frs: filter_hash(data, 'test_location', 'Index'),
-                          other: data.select { |q| %w[VCT Mobile Other].include?(q['test_location']) },
-                          twenty_five_plus: data.select { |q| birthdate_to_age(q['birthdate']) > 25 },
-                          zero_to_eleven_months: data.select { |q| birthdate_to_age(q['birthdate']) < 1 },
-                          one_to_fourteen_years: data.select { |q| (1..14).include?(birthdate_to_age(q['birthdate'])) },
-                          fiveteen_to_twenty_four_years: data.select { |q| (15..24).include?(birthdate_to_age(q['birthdate'])) },
-                          male: filter_hash(data, 'gender', 'M'),
-                          fnp: data.select { |q| [NOT_PREGNANT, BREASTFEEDING].include?(q['status']) },
-                          fp: filter_hash(data, 'status', PREGNANT_WOMAN)
+                            frs: filter_hash(data, 'test_location', 'Index'),
+                            other: data.select { |q| %w[VCT Mobile Other].include?(q['test_location']) },
+                          },
+                          age_group: {
+                            twenty_five_plus: data.select { |q| birthdate_to_age(q['birthdate']) > 25 },
+                            zero_to_eleven_months: data.select { |q| birthdate_to_age(q['birthdate']) < 1 },
+                            one_to_fourteen_years: data.select { |q| (1..14).include?(birthdate_to_age(q['birthdate'])) },
+                            fiveteen_to_twenty_four_years: data.select { |q| (15..24).include?(birthdate_to_age(q['birthdate'])) },
+                          },
+                          sex: {
+                            male: filter_hash(data, 'gender', 'M'),
+                            fnp: data.select { |q| [NOT_PREGNANT, BREASTFEEDING].include?(q['status']) },
+                            fp: filter_hash(data, 'status', PREGNANT_WOMAN)
+                          }
                         })
         end
 
         def filter_hash(data, key, value)
-
           return data.select { |q| q[key[0]] == value && q[key[1]] == value } if key.is_a?(Array)
 
           data.select { |q| q[key] == value }
@@ -173,7 +204,8 @@ module HtsService
         person.person_id,
         person.gender,
         person.birthdate,
-        prev_test.value_coded
+        prev_test.value_coded,
+        encounter.encounter_datetime
           SQL
         end
       end
