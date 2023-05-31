e347db5d4bb6126fbad2fd99fba67341915d3fb2
Roy Chanunkha <roychanunkha@gmail.com>
2023-05-31 17:05:14 +0200

    schema and swagger updates

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -86,6 +86,210 @@ RSpec.configure do |config|
             items: { type: :integer },
             example: [1, 2, 3],
             description: 'Array of patient identifiers record ids'
+          },
+          patient_identifier_type: {
+            type: :object,
+            properties: {
+              patient_identifier_type_id: { type: :integer },
+              name: { type: :string },
+              description: { type: :string },
+              format: { type: :string },
+              check_digit: { type: :integer },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              required: { type: :integer },
+              format_description: { type: :string },
+              validator: { type: :string },
+              retired: { type: :integer },
+              retired_by: { type: :integer },
+              date_retired: { type: :string },
+              retire_reason: { type: :string },
+              uuid: { type: :string }
+            }
+          },
+          patient_identifier: {
+            type: :object,
+            properties: {
+              patient_identifier_id: { type: :integer },
+              patient_id: { type: :integer },
+              identifier: { type: :string },
+              identifier_type: { type: :string },
+              preferred: { type: :boolean },
+              location_id: { type: :integer },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              voided: { type: :boolean },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              uuid: { type: :string },
+              type: { "$ref": '#/components/schemas/patient_identifier_type' }
+            }
+          },
+          person_attribute_type: {
+            type: :object,
+            properties: {
+              person_attribute_type_id: { type: :integer },
+              name: { type: :string },
+              description: { type: :string },
+              format: { type: :string },
+              foreign_key: { type: :string },
+              searchable: { type: :integer },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              changed_by: { type: :integer },
+              date_changed: { type: :string },
+              retired: { type: :integer },
+              retired_by: { type: :integer },
+              date_retired: { type: :string },
+              retire_reason: { type: :string },
+              edit_privilege: { type: :string },
+              uuid: { type: :string },
+              sort_weight: { type: :number }
+            }
+          },
+          person_attribute: {
+            type: :object,
+            properties: {
+              person_attribute_id: { type: :integer },
+              person_id: { type: :integer },
+              value: { type: :string },
+              person_attribute_type_id: { type: :integer },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              changed_by: { type: :integer },
+              date_changed: { type: :string },
+              voided: { type: :integer },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              uuid: { type: :string },
+              type: { "$ref": '#/components/schemas/person_attribute_type' }
+            }
+          },
+          person_address: {
+            type: :object,
+            properties: {
+              person_address_id: { type: :integer },
+              person_id: { type: :integer },
+              preferred: { type: :integer },
+              address1: { type: :string },
+              address2: { type: :string },
+              city_village: { type: :string },
+              state_province: { type: :string },
+              postal_code: { type: :string },
+              country: { type: :string },
+              latitude: { type: :string },
+              longitude: { type: :string },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              voided: { type: :integer },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              county_district: { type: :string },
+              neighborhood_cell: { type: :string },
+              region: { type: :string },
+              subregion: { type: :string },
+              township_division: { type: :string },
+              uuid: { type: :string }
+            }
+          },
+          person_name: {
+            type: :object,
+            properties: {
+              person_name_id: { type: :integer },
+              preferred: { type: :integer },
+              person_id: { type: :integer },
+              prefix: { type: :string },
+              given_name: { type: :string },
+              middle_name: { type: :string },
+              family_name_prefix: { type: :string },
+              family_name: { type: :string },
+              family_name2: { type: :string },
+              family_name_suffix: { type: :string },
+              degree: { type: :string },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              voided: { type: :integer },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              changed_by: { type: :integer },
+              date_changed: { type: :string },
+              uuid: { type: :string }
+            }
+          },
+          person: {
+            type: :object,
+            properties: {
+              person_id: { type: :integer },
+              gender: { type: :string },
+              birthdate: { type: :string },
+              birthdate_estimated: { type: :integer },
+              dead: { type: :integer },
+              death_date: { type: :string },
+              cause_of_death: { type: :string },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              changed_by: { type: :integer },
+              date_changed: { type: :string },
+              voided: { type: :integer },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              uuid: { type: :string },
+              names: { type: :array, items: { "$ref": '#/components/schemas/person_name' } },
+              addresses: { type: :array, items: { "$ref": '#/components/schemas/person_address' } },
+              patient_identifiers: { type: :array, items: { "$ref": '#/components/schemas/person_identifier' } },
+              person_attributes: { type: :array, items: { "$ref": '#/components/schemas/person_attribute' } }
+            }
+          },
+          tpt_status: {
+            type: :object,
+            properties: {
+              tpt: { type: :string },
+              completed: { type: :boolean },
+              tb_treatment: { type: :boolean },
+              tpt_init_date: { type: :string },
+              tpt_complete_date: { type: :string }
+            }
+          },
+          merge_audit: {
+            type: :object,
+            properties: {
+              id: { type: :integer },
+              primary_id: { type: :integer },
+              secondary_id: { type: :integer },
+              merge_type: { type: :string },
+              secondary_previous_merge_id: { type: :integer },
+              creator: { type: :integer },
+              voided: { type: :boolean },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              created_at: { type: :string },
+              updated_at: { type: :string }
+            }
+          },
+          patient: {
+            type: :object,
+            properties: {
+              patient_id: { type: :integer },
+              tribe: { type: :string },
+              creator: { type: :integer },
+              date_created: { type: :string },
+              changed_by: { type: :integer },
+              date_changed: { type: :string },
+              voided: { type: :integer },
+              voided_by: { type: :integer },
+              date_voided: { type: :string },
+              void_reason: { type: :string },
+              art_start_date: { type: :string },
+              tpt_status: { "$ref": '#/components/schemas/tpt_status' },
+              merge_history: { type: :array, items: { "$ref": '#/components/schemas/merge_audit' } },
+              person: { "$ref": '#/components/schemas/person' }
+            }
           }
         }
       },
