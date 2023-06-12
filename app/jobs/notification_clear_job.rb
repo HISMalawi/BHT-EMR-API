3603c6e2a2649fbde53d4964f0e172f2fdc4a9f3
Roy Chanunkha <roychanunkha@gmail.com>
2023-06-12 10:32:31 +0200

    added a job to clear notifications

---
+++ app/jobs/notification_clear_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# this job is used to clear notifications after a certain period of time has elapsed
+class NotificationClearJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    lab = User.find_by(username: 'lab_daemon')
+    NotificationAlert.where('date_to_expire < ?', Time.now).each do |alert|
+      # update the notification alert to read
+      alert.update!(alert_read: true, changed_by: lab)
+      # update the notification alert recipient to cleared
+      alert.notification_alert_recipients.update_all(cleared: true)
+    end
+  end
+end
