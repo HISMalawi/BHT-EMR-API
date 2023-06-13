184786a3d4f0ea4eeb0ee6d19fd1951b855a58e2
Roy Chanunkha <roychanunkha@gmail.com>
2023-06-13 09:54:19 +0200

    added swagger documentation

--- spec/requests/api/v1/clinic_reports_spec.rb
+++ spec/requests/api/v1/clinic_reports_spec.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 
-TAGS_NAME = 'Clinic Reports'.freeze
+TAGS_NAME = 'Clinic Reports'
 
+# rubocop:disable Metrics/BlockLength
 describe 'Clinic Reports API', type: :request, swagger_doc: 'v1/swagger.yaml' do
   path '/api/v1/programs/1/reports/clinic_tx_rtt' do
     get 'Retrieve CLINIC TX RTT report' do
@@ -107,4 +110,22 @@ describe 'Clinic Reports API', type: :request, swagger_doc: 'v1/swagger.yaml' do
       end
     end
   end
+
+  path 'api/v1/programs/1/reports/lims_electronic_results' do
+    get 'Retrieve LIMS ELECTRONIC RESULTS report' do
+      tags TAGS_NAME
+      description 'This shows LIMS ELECTRONIC RESULTS report'
+      consumes 'application/json'
+      produces 'application/json'
+      security [api_key: []]
+      parameter name: :start_date, in: :query, type: :string
+      parameter name: :end_date, in: :query, type: :string
+
+      response '200', 'LIMS ELECTRONIC RESULTS Report found' do
+        schema type: :array, items: { '$ref' => '#/components/schemas/lims_electronic_result' }
+        run_test!
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
