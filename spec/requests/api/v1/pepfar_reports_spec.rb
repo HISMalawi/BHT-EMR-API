5a7b51b72f1b4fddf953e9e3f61a2d4cdf984f45
bryan <brianmsyamboza@gmail.com>
2023-06-30 13:10:42 +0200

    updated swagger docs

--- spec/requests/api/v1/pepfar_reports_spec.rb
+++ spec/requests/api/v1/pepfar_reports_spec.rb
@@ -15,20 +15,17 @@ describe 'Pepfar Reports API', type: :request, swagger_doc: 'v1/swagger.yaml' do
 
       response '200', 'TX_TB Report found' do
         schema type: :object, properties: {
-          age_group: { type: :object, properties: {
+          age_group: { 
+          gender: { type: :object, properties: {
             tx_curr: { type: :array, items: { type: :integer } },
             sceen_pos_new: { type: :array, items: { type: :integer } },
             sceen_neg_new: { type: :array, items: { type: :integer } },
             started_tb_new: { type: :array, items: { type: :integer } },
-            specimens_sent: { type: :array, items: { type: :integer } },
-            smear_test_only: { type: :array, items: { type: :integer } },
-            gene_xpert_test_only: { type: :array, items: { type: :integer } },
-            lam: { type: :array, items: { type: :integer } },
-            other: { type: :array, items: { type: :integer } },
             sceen_pos_prev: { type: :array, items: { type: :integer } },
             sceen_neg_prev: { type: :array, items: { type: :integer } },
             started_tb_prev: { type: :array, items: { type: :integer } }
           } }
+        }
         }
         run_test!
       end
