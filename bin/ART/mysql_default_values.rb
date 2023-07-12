a2c39f59daed60362e22970fbbbd3236690c2961
Roy Chanunkha <roychanunkha@gmail.com>
2023-07-12 15:03:11 +0200

    changes to date strings

--- bin/ART/mysql_default_values.rb
+++ bin/ART/mysql_default_values.rb
@@ -6,23 +6,23 @@
 tables = ActiveRecord::Base.connection.tables
 # loop through the tables
 tables.each do |table|
-    # get the table columns and loop through them
-    ActiveRecord::Base.connection.columns(table).each do |column|
-        # check if the column has a default value
-        if column.default
-            # check if the column data type is a date then use current_timestamp()
-            if column.type == :date
-                puts "ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;"
-                ActiveRecord::Base.connection.execute("ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;")
-            # check if the column data type is a datetime then use current_timestamp()
-            elsif column.type == :datetime
-                puts "ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;"
-                ActiveRecord::Base.connection.execute("ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;")
-            # check if the column data type is a time then use current_timestamp()
-            else
-                # just print the table name and the column name
-                puts "#{table} #{column.name} #{column.type} #{column.default}"
-            end
-        end
+  # get the table columns and loop through them
+  ActiveRecord::Base.connection.columns(table).each do |column|
+    # check if the column has a default value
+    if column.default
+      # check if the column data type is a date then use current_timestamp()
+      if column.type == :date
+        puts "ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;"
+        ActiveRecord::Base.connection.execute("ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;")
+      # check if the column data type is a datetime then use current_timestamp()
+      elsif column.type == :datetime
+        puts "ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;"
+        ActiveRecord::Base.connection.execute("ALTER TABLE #{table} MODIFY #{column.name} DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;")
+      # check if the column data type is a time then use current_timestamp()
+      else
+        # just print the table name and the column name
+        puts "#{table} #{column.name} #{column.type} #{column.default}"
+      end
     end
+  end
 end
