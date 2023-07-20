4553b1067c5d737afa60209bae9bd63df662a6c1
bryan <brianmsyamboza@gmail.com>
2023-07-20 09:56:05 +0200

    retry job if exception occures

--- app/jobs/ait_intergration_job.rb
+++ app/jobs/ait_intergration_job.rb
@@ -1,5 +1,8 @@
 class AITIntergrationJob < ApplicationJob
     queue_as :default
+    rescue_from(Exception) do |_exception|
+        retry_job wait: 5.minutes, queue: :default
+    end
 
     def perform(patient_id)
         HTSService::AITIntergration::AITIntergrationService.new(patient_id).sync
