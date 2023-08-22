8fec669547ff8ae35756ea344dd0b848702d8614
Roy Chanunkha <roychanunkha@gmail.com>
2023-08-22 16:53:56 +0200

    adding the first query

--- app/services/art_service/reports/clinic/stock_card_report.rb
+++ app/services/art_service/reports/clinic/stock_card_report.rb
@@ -12,12 +12,29 @@ module ARTService
 
         def find_report
           # TODO: Implement this
+          stock_card_report
         end
 
         private
 
         def stock_card_report
           ActiveRecord::Base.connection.select_all <<~SQL
+            SELECT
+                pbi.drug_id AS drug_id,
+                COALESCE(dc.short_name, dc.name, d.name, 'Unkown') AS drug_name,
+                SUM(pbi.current_quantity)/SUM(pbi.pack_size) AS current_quantity,
+                SUM(pbi.delivered_quantity)/SUM(pbi.pack_size) AS delivered_quantity,
+                SUM(ABS(po.quantity))/SUM(pbi.pack_size) AS dispensed_quantity,
+                pbi.pack_size
+            FROM pharmacy_batch_items AS pbi
+            INNER JOIN drug AS d ON d.drug_id = pbi.drug_id
+            LEFT JOIN drug_cms AS dc ON dc.drug_inventory_id = d.drug_id
+            LEFT JOIN pharmacy_obs AS po ON po.batch_item_id = pbi.id
+                AND po.voided = 0
+                AND po.pharmacy_encounter_type = 3 -- Pharmacy dispensing
+                AND po.transaction_date BETWEEN #{@start_date} AND #{@end_date}
+            WHERE pbi.delivery_date <= #{@end_date}
+            GROUP BY pbi.drug_id
           SQL
         end
       end
