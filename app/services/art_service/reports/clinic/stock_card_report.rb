576e9ac2a6faa7fbad700e36f17b0c5723b76f74
Roy Chanunkha <roychanunkha@gmail.com>
2023-08-23 14:31:14 +0200

    updating the stock report logic

--- app/services/art_service/reports/clinic/stock_card_report.rb
+++ app/services/art_service/reports/clinic/stock_card_report.rb
@@ -32,22 +32,31 @@ module ARTService
               AND po.voided = 0
               AND po.pharmacy_encounter_type = 3 -- Pharmacy dispensing
               AND po.transaction_date BETWEEN #{@start_date} AND #{@end_date}
+              AND po.dispensation_obs_id IS NOT NULL
             LEFT JOIN (
                 SELECT
                     drug_id,
-                    MIN(transaction_date) AS min_transaction_date,
-                    MAX(transaction_date) AS max_transaction_date,
+                    MAX(transaction_date) AS transaction_date,
                     pack_size
                 FROM pharmacy_stock_balances
                 WHERE transaction_date < #{@end_date} -- Opening balance can be anything less than end_date
                 GROUP BY drug_id, pack_size
-            ) AS psb_min_max ON pbi.drug_id = psb_min_max.drug_id AND pbi.pack_size = psb_min_max.pack_size
+            ) AS psb_max ON pbi.drug_id = psb_max.drug_id AND pbi.pack_size = psb_max.pack_size
+            LEFT JOIN (
+              SELECT
+                  drug_id,
+                  MAX(transaction_date) AS transaction_date,
+                  pack_size
+              FROM pharmacy_stock_balances
+              WHERE transaction_date <= #{@start_date} -- Opening balance can be anything less than end_date
+              GROUP BY drug_id, pack_size
+            ) AS psb_min ON pbi.drug_id = psb_min.drug_id AND pbi.pack_size = psb_min.pack_size
             LEFT JOIN pharmacy_stock_balances AS psb_opening ON
                 pbi.drug_id = psb_opening.drug_id AND pbi.pack_size = psb_opening.pack_size
-                AND psb_opening.transaction_date = psb_min_max.min_transaction_date
+                AND psb_opening.transaction_date = psb_min.transaction_date
             LEFT JOIN pharmacy_stock_balances AS psb_closing ON
                 pbi.drug_id = psb_closing.drug_id AND pbi.pack_size = psb_closing.pack_size
-                AND psb_closing.transaction_date = psb_min_max.max_transaction_date
+                AND psb_closing.transaction_date = psb_max.transaction_date
             WHERE pbi.voided = 0
             GROUP BY pbi.drug_id, pbi.pack_size
           SQL
