83e34cf9fa5b2799aa162eab2c0e2c3723743fb5
Roy Chanunkha <roychanunkha@gmail.com>
2023-09-29 09:26:32 +0200

    reworked the dashboard

--- app/services/aetc_service/reports/clinic/dashboard_stats.rb
+++ app/services/aetc_service/reports/clinic/dashboard_stats.rb
@@ -1,91 +1,71 @@
 # frozen_string_literal: true
 
 module AetcService
-  # A hash of encounter types and their display names
-  class DashboardStats
-    ENCOUNTERS = {
-      social_history: 'SOCIAL HISTORY',
-      outpatient_reception: 'PATIENT REGISTRATION',
-      vitals: 'VITALS',
-      presenting_complaints: 'PRESENTING COMPLAINTS',
-      outpatient_diagnosis: 'OUTPATIENT DIAGNOSIS',
-      prescription: 'PRESCRIPTION',
-      dispensing: 'DISPENSING',
-      treatment: 'TREATMENT'
-    }.freeze
+  module Clinic
+    # A hash of encounter types and their display names
+    class DashboardStats
+      ENCOUNTERS = [
+        'SOCIAL HISTORY',
+        'PATIENT REGISTRATION',
+        'VITALS',
+        'PRESENTING COMPLAINTS',
+        'OUTPATIENT DIAGNOSIS',
+        'PRESCRIPTION',
+        'DISPENSING',
+        'TREATMENT'
+      ].freeze
 
-    # An array of report groups
-    REPORT_GROUP = %i[me facility].freeze
-
-    # Initializes a new instance of the DashboardStats class
-    #
-    # @param date [Date] The date to generate the report for
-    def initialize(date)
-      @program = Program.find_by_name 'AETC PROGRAM'
-      @date = date || Date.today
-    end
-
-    # Generates a report of dashboard stats
-    #
-    # @return [Hash] A hash of dashboard stats
-    def find_report
-      build_report
-      tranform_report
-    end
-
-    private
-
-    # Initializes the report structure
-    #
-    # @return [Hash] A hash representing the report structure
-    def initialize_report_structure
-      ENCOUNTERS.transform_values do |_value|
-        REPORT_GROUP.each_with_object({}) do |group, sub_report|
-          sub_report[group] = {}
-        end
+      # Initializes a new instance of the DashboardStats class
+      #
+      # @param start_date [Date] The start date to generate the report for
+      # @param end_date [Date] The end date to limit the result
+      def initialize(start_date, end_date)
+        @program = Program.find_by_name 'AETC PROGRAM'
+        @start_date = start_date.to_date.beginning_of_day
+        @end_date = end_date.to_date.end_of_day
       end
-    end
 
-    # Builds the report
-    #
-    # @return [void]
-    def build_report
-      @report = initialize_report_structure
-      ENCOUNTERS.each do |encounter, name|
-        REPORT_GROUP.each do |group|
-          result = count_encounters name, group
-          @report[encounter][group] = result
-          @report[encounter][:total] = 0 unless @report[encounter][:total]
-          @report[encounter][:total] += result
-        end
+      # Generates a report of dashboard stats
+      #
+      # @return [Hash] A hash of dashboard stats
+      def find_report
+        patient_providers_encounters
       end
-    end
 
-    # Transforms the report
-    #
-    # @return [void]
-    def tranform_report
-      @report.map do |encounter, sub_report|
-        {
-          name: ENCOUNTERS[encounter],
-          me: sub_report[:me],
-          facility: sub_report[:facility],
-          total: sub_report[:total]
-        }
+      private
+
+      # Get the patient providers encounter for the specified date
+      #
+      # @return [Array] An array of encounters
+      def patient_providers_encounters
+        ActiveRecord::Base.connection.select_all <<~SQL
+          SELECT et.name, COALESCE(individual_clinic.total_encounters, 0) AS me, COALESCE(clinic.total_encounters, 0) AS facility, COALESCE(clinic.total_encounters, 0) + COALESCE(individual_clinic.total_encounters, 0) AS total
+          FROM encounter_type et
+          LEFT JOIN (
+            SELECT e.encounter_type, count(*) as total_encounters
+            FROM encounter e
+            WHERE e.encounter_datetime => '#{@start_date}' AND e.encounter_datetime <= '#{@end_date}'
+            AND e.voided = 0
+            AND e.program_id = #{@program.id}
+            GROUP BY e.encounter_type
+          ) AS clinic ON clinic.encounter_type = et.encounter_type_id
+          LEFT JOIN (
+            SELECT e.encounter_type, count(*) as total_encounters
+            FROM encounter e
+            WHERE e.encounter_datetime BETWEEN '#{@start_date}' AND '#{@end_date}'
+            AND e.voided = 0
+            AND e.program_id = #{@program.id}
+            AND e.provider_id = #{User.current.id}
+            GROUP BY e.encounter_type
+          ) AS individual_clinic ON individual_clinic.encounter_type = et.encounter_type_id
+          WHERE et.retired = 0
+          AND et.encounter_type_id IN ('#{encounter_type_ids.join("','")}')
+        SQL
       end
-    end
 
-    # Counts the number of encounters of a given type and group on a given date
-    #
-    # @param encounter [String] The name of the encounter type to count
-    # @param group [Symbol] The group to count encounters for (:me or :facility)
-    # @return [Integer] The number of encounters of the given type and group on the given date
-    def count_encounters(encounter, group)
-      filter = { encounter_type: EncounterType.find_by_name(encounter) }
-      filter[:program] = @program
-      filter[:provider] = User.current.person if group == :me
-      filter[:encounter_datetime] = @date.beginning_of_day..@date.end_of_day
-      Encounter.where(filter).count
+      def encounter_type_ids
+        EncounterType.where(name: ENCOUNTERS).pluck(:encounter_type_id)
+      end
     end
   end
 end
