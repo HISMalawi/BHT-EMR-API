404f583283fa4cdcc77a20066aa6b912fb122e97
Roy Chanunkha <roychanunkha@gmail.com>
2023-10-03 09:38:55 +0200

    added diagnosis specific report

---
+++ app/services/aetc_service/reports/clinic/diagnosis_specific_report.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module AetcService
+  module Reports
+    module Clinic
+      # This report gives a list of areas with patients with a specific diagnosis
+      class DiagnosisSpecificReport
+        include ModelUtils
+        attr_reader :start_date, :end_date, :diagnosis
+
+        def initialize(start_date:, end_date:, **kwargs)
+          @start_date = start_date.to_date.beginning_of_day
+          @end_date = end_date.to_date.end_of_day
+          @diagnosis = kwargs[:diagnosis]
+        end
+
+        def fetch_report
+          process_data || []
+        end
+
+        private
+
+        def data
+          ActiveRecord::Base.connection.select_all <<~SQL
+            SELECT pa.county_district as address, GROUP_CONCAT(DISTINCT e.patient_id) as patient_ids
+            FROM obs o
+            INNER JOIN encounter e ON e.encounter_id = o.encounter_id
+                AND e.voided = 0
+                AND e.program_id = #{program('AETC PROGRAM').program_id}
+                AND e.encounter_datetime >= '#{start_date}' AND e.encounter_datetime <= '#{end_date}'
+                AND e.encounter_type = #{encounter_type('OUTPATIENT DIAGNOSIS').encounter_type_id}
+            INNER JOIN patient p ON p.patient_id = e.patient_id AND p.voided = 0
+            INNER JOIN person_address pa ON pa.person_id = p.patient_id AND pa.voided = 0
+            WHERE o.concept_id IN (#{concept('PRIMARY DIAGNOSIS').concept_id}, #{concept('SECONDARY DIAGNOSIS').concept_id})
+                AND o.voided = 0
+                AND o.value_coded = #{concept(diagnosis).concept_id}
+                AND o.obs_datetime >= '#{start_date}' AND o.obs_datetime <= '#{end_date}'
+            GROUP BY pa.county_district
+          SQL
+        end
+
+        def process_data
+          report_data = []
+          data.each do |d|
+            report_data << {
+              address: d['address'],
+              patient_ids: d['patient_ids'].split(',')
+            }
+          end
+          report_data
+        end
+      end
+    end
+  end
+end
