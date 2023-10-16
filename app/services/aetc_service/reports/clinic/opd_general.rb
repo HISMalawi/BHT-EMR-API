49955a430a314bec1d387b3d1c4a85ddb8a9164e
Roy Chanunkha <roychanunkha@gmail.com>
2023-10-16 20:19:13 +0200

    init on opd general

---
+++ app/services/aetc_service/reports/clinic/opd_general.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module AETCService
+    module Reports
+        module Clinic
+            class OpdGeneral
+                # to be implemented
+            end
+        end
+    end
+end
