5f806ef7dc680b0f352556e3e380f0948202eda2
Roy Chanunkha <roychanunkha@gmail.com>
2023-10-20 11:40:42 +0200

    fixing cxca scrn for pepfar to only include ART clients

--- app/services/cxca_service/reports/pepfar/cxca_scrn.rb
+++ app/services/cxca_service/reports/pepfar/cxca_scrn.rb
@@ -7,8 +7,9 @@ module CXCAService
         attr_reader :start_date, :end_date, :report
 
         include Utils
+        include ModelUtils
 
-        CxCa_PROGRAM = program 'CxCa program'
+        CxCa_PROGRAM = 'CxCa program'
 
         TX_GROUPS = {
           first_time_screened: ['initial screening', 'referral'],
@@ -39,7 +40,7 @@ module CXCAService
         private
 
         def init_report
-          query = fetch_query.to_hash
+          query = fetch_query.to_a
           pepfar_age_groups.collect do |age_group|
             row = {}
             row['age_group'] = age_group
@@ -55,26 +56,43 @@ module CXCAService
         end
 
         def fetch_query
-          Person.connection.select_all(
-            Person.joins(patient: :encounters)
-              .where(encounter: { program_id: CxCa_PROGRAM.id, encounter_datetime: @start_date..@end_date })
-              .joins(<<~SQL)
-                LEFT JOIN obs reason_for_visit ON reason_for_visit.person_id = person.person_id
-                AND reason_for_visit.voided = 0
-                AND reason_for_visit.concept_id = #{concept('Reason for visit').concept_id}
-                LEFT JOIN obs treatment ON treatment.person_id = person.person_id
-                AND treatment.voided = 0
-                AND treatment.concept_id = #{concept('Screening results').concept_id}
-                LEFT JOIN concept_name reason_name ON reason_name.concept_id = reason_for_visit.value_coded
-                AND reason_name.voided = 0
-                LEFT JOIN concept_name screening_name ON screening_name.concept_id = treatment.value_coded
-                AND screening_name.voided = 0
-                AND screening_name.name IS NOT NULL
-              SQL
-              .group('person.person_id')
-              .select("disaggregated_age_group(person.birthdate, DATE('#{@end_date.to_date}')) AS age_group, person.person_id, reason_name.name AS reason_for_visit, screening_name.name AS treatment")
-              .to_sql
-          )
+          ActiveRecord::Base.connection.select_all <<~SQL
+            SELECT
+              p.person_id,
+              disaggregated_age_group(p.birthdate, '#{@end_date}') age_group,
+              reason_name.name reason_for_visit,
+              screening_name.name treatment
+            FROM person p
+            INNER JOIN (
+              SELECT e.patient_id, DATE(MAX(e.encounter_datetime)) AS last_visit_date
+              FROM encounter e
+              WHERE e.program_id = #{program(CxCa_PROGRAM).id}
+                AND e.encounter_datetime >= '#{@start_date}'
+                AND e.encounter_datetime <= '#{@end_date}'
+                AND e.voided = 0
+              GROUP BY e.patient_id
+            ) AS last_visit ON last_visit.patient_id = p.person_id
+            LEFT JOIN obs reason_for_visit ON reason_for_visit.person_id = last_visit.patient_id
+              AND reason_for_visit.voided = 0
+              AND reason_for_visit.concept_id = #{concept('Reason for visit').concept_id}
+              AND DATE(reason_for_visit.obs_datetime) = last_visit.last_visit_date
+            LEFT JOIN obs treatment ON treatment.person_id = last_visit.patient_id
+              AND treatment.voided = 0
+              AND treatment.concept_id = #{concept('Screening results').concept_id}
+              AND DATE(treatment.obs_datetime) = last_visit.last_visit_date
+            LEFT JOIN concept_name reason_name ON reason_name.concept_id = reason_for_visit.value_coded
+              AND reason_name.voided = 0
+            LEFT JOIN concept_name screening_name ON screening_name.concept_id = treatment.value_coded
+              AND screening_name.voided = 0
+            INNER JOIN (
+              SELECT o.patient_id
+              FROM orders o
+              INNER JOIN drug_order d ON d.order_id = o.order_id AND d.drug_inventory_id = (SELECT drug_id FROM arv_drug) AND d.quantity > 0
+              GROUP BY o.patient_id
+            ) in_art ON in_art.patient_id = p.person_id
+            WHERE p.voided = 0 AND LEFT(p.gender, 1) = 'F'
+            GROUP BY p.person_id
+          SQL
         end
       end
     end
