510df4f22ea5c456afa81d72bb9e5fc9e21a2de4
Daniel Justin <danieljmuleke@gmail.com>
2023-10-26 11:11:18 +0200

    Implemented spine attendance report

---
+++ app/services/spine_service/reports/clinic/attendance_report.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SpineService
+  module Reports
+    module Clinic
+      class AttendanceReport
+        include ModelUtils
+        attr_reader :start_date, :end_date
+
+        def initialize(start_date:, end_date:, **kwargs)
+          @start_date = start_date.to_date.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S')
+          @end_date = end_date.to_date.end_of_day.strftime('%Y-%m-%d %H:%M:%S')
+        end
+
+        def fetch_report
+          program = Program.find_by_name 'Spine Program'
+          data = Encounter.where('encounter_datetime BETWEEN ? AND ? AND program_id = ? AND voided = 0', @start_date, @end_date, program.program_id.to_s)
+            .group('patient_id, DATE(encounter_datetime)')
+            .select("patient_id, DATE_FORMAT(encounter_datetime,'%Y-%m-%d') enc_date")
+          
+          results = (data || []).map{|e| e.patient_id}
+          results
+        end
+      end
+    end
+  end
+end
