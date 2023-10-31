cbbe960b87fc81ff462412c8ed27be50dd204e22
anthony-kakatera <anthonyckakatera@gmail.com>
2023-10-31 16:15:04 +0200

    Added UpdateDHIS2 report. Not implemented

---
+++ app/services/aetc_service/reports/clinic/update_dhis2.rb
@@ -0,0 +1,10 @@
+module AetcService
+    module Reports
+        module Clinic
+            class UpdateDHIS2
+                # To be implemented
+                
+            end
+        end
+    end
+end
