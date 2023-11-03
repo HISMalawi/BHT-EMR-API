913822e08eed2e895e24dbf0bc63743299ba21f0
Daniel Justin <danieljmuleke@gmail.com>
2023-11-03 11:29:44 +0200

    Refactor: retrieve needed data only

--- app/services/spine_service/reports/clinic/diagnosis_report.rb
+++ app/services/spine_service/reports/clinic/diagnosis_report.rb
@@ -15,17 +15,13 @@ module SpineService
         def fetch_report
           primary_diagnosis = 6542
           secondary_diagnosis = 6543
-          cellphoneNumberId = 12
+
           data = Encounter.joins("INNER JOIN obs ON obs.encounter_id = encounter.encounter_id AND obs.concept_id IN(#{primary_diagnosis}, #{secondary_diagnosis}) AND obs.voided = 0")
                           .joins("INNER JOIN person p ON p.person_id = encounter.patient_id AND p.voided = 0")
-                          .joins("LEFT JOIN person_name n ON n.person_id = encounter.patient_id AND n.voided = 0")
-                          .joins("LEFT JOIN person_attribute z ON z.person_id = encounter.patient_id AND z.person_attribute_type_id = #{cellphoneNumberId}  AND z.voided = 0")
-                          .joins("LEFT JOIN person_address a ON a.person_id = encounter.patient_id  AND a.voided = 0")
                           .joins("INNER JOIN concept_name c ON c.concept_id = obs.value_coded  AND c.voided = 0")
                           .where('encounter_datetime BETWEEN ? AND ? AND encounter_type = ?', @start_date, @end_date, encounter_type('Outpatient diagnosis').id)
                           .group('obs.person_id, obs.value_coded, DATE(obs.obs_datetime)')
-                          .select("encounter.encounter_type, n.given_name, n.family_name, n.person_id, obs.value_coded, p.gender,
-                                  a.state_province district, a.township_division ta, a.city_village village, z.value,
+                          .select("encounter.encounter_type, p.person_id, obs.value_coded, p.gender,
                                   opd_disaggregated_age_group(p.birthdate,'#{end_date}') as age_group, c.name")
 
           create_diagnosis_hash(data)
