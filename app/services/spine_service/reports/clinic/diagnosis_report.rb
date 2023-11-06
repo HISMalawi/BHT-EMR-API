30fadececa72bf17e1c6321c441c15efc66a2034
Roy Chanunkha <roychanunkha@gmail.com>
2023-11-06 17:00:03 +0200

    code fixes

--- app/services/spine_service/reports/clinic/diagnosis_report.rb
+++ app/services/spine_service/reports/clinic/diagnosis_report.rb
@@ -7,19 +7,16 @@ module SpineService
         include ModelUtils
         attr_reader :start_date, :end_date
 
-        def initialize(start_date:, end_date:, **kwargs)
+        def initialize(start_date:, end_date:, **_kwargs)
           @start_date = start_date.to_date.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S')
           @end_date = end_date.to_date.end_of_day.strftime('%Y-%m-%d %H:%M:%S')
         end
 
         def fetch_report
-          primary_diagnosis = 6542
-          secondary_diagnosis = 6543
-
-          data = Encounter.joins("INNER JOIN obs ON obs.encounter_id = encounter.encounter_id AND obs.concept_id IN(#{primary_diagnosis}, #{secondary_diagnosis}) AND obs.voided = 0")
-                          .joins("INNER JOIN person p ON p.person_id = encounter.patient_id AND p.voided = 0")
-                          .joins("INNER JOIN concept_name c ON c.concept_id = obs.value_coded  AND c.voided = 0")
-                          .where('encounter_datetime BETWEEN ? AND ? AND encounter_type = ?', @start_date, @end_date, encounter_type('Outpatient diagnosis').id)
+          data = Encounter.joins("INNER JOIN obs ON obs.encounter_id = encounter.encounter_id AND obs.concept_id IN(#{concept('Primary diagnosis').id}, #{concept('Secondary diagnosis').id}) AND obs.voided = 0")
+                          .joins('INNER JOIN person p ON p.person_id = encounter.patient_id AND p.voided = 0')
+                          .joins('INNER JOIN concept_name c ON c.concept_id = obs.value_coded AND c.voided = 0')
+                          .where('encounter_datetime BETWEEN ? AND ? AND encounter_type = ?', @start_date, @end_date, encounter_type('diagnosis').id)
                           .group('obs.person_id, obs.value_coded, DATE(obs.obs_datetime)')
                           .select("encounter.encounter_type, p.person_id, obs.value_coded, p.gender,
                                   opd_disaggregated_age_group(p.birthdate,'#{end_date}') as age_group, c.name")
@@ -30,25 +27,26 @@ module SpineService
         def create_diagnosis_hash(data)
           records = {}
           (data || []).each do |record|
-            age_group = record['age_group'].blank? ? "Unknown" : record['age_group']
-            gender = (record['gender'].match(/f/i) ? "F" : (record['gender'].match(/m/i) ? "M" : "Unknown")) rescue "Unknown"
+            age_group = record['age_group'].blank? ? 'Unknown' : record['age_group']
+            gender = begin
+              (if record['gender'].match(/f/i)
+                 'F'
+               else
+                 (record['gender'].match(/m/i) ? 'M' : 'Unknown')
+               end)
+            rescue StandardError
+              'Unknown'
+            end
             patient_id = record['person_id']
             diagnosis = record['name']
 
-            if records[diagnosis].blank?
-              records[diagnosis] = {}
-            end
+            records[diagnosis] = {} if records[diagnosis].blank?
 
-            if records[diagnosis][gender].blank?
-              records[diagnosis][gender] = {}
-            end
+            records[diagnosis][gender] = {} if records[diagnosis][gender].blank?
 
-            if records[diagnosis][gender][age_group].blank?
-              records[diagnosis][gender][age_group] = []
-            end
+            records[diagnosis][gender][age_group] = [] if records[diagnosis][gender][age_group].blank?
 
             records[diagnosis][gender][age_group] << patient_id
-
           end
 
           records
