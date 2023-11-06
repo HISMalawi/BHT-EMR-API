ff09dbb2755be1ceb15df73efe5ed99eafc9d8f3
Roy Chanunkha <roychanunkha@gmail.com>
2023-11-06 11:29:45 +0200

    adding more to the migrations

--- bin/add_person_id_to_user_table.rb
+++ bin/add_person_id_to_user_table.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+def update_admin_password_and_salt
+  user = User.find_by_username('admin')
+  salt = SecureRandom.base64
+  pass = Digest::SHA1.hexdigest("test#{salt}")
+  user.update!(password: pass, salt: salt)
+end
+
 def add_global_property
   GlobalProperty.create!(property: 'current_health_center_id', property_value: 614) if GlobalProperty.where(property: 'current_health_center_id').blank?
   GlobalProperty.create!(property: 'current_health_center_name', property_value: 'Queen Elizabeth Central Hospital') if GlobalProperty.where(property: 'current_health_center_name').blank?
@@ -29,6 +36,7 @@ end
 
 # wrap in a transaction
 ActiveRecord::Base.transaction do
+  update_admin_password_and_salt
   add_global_property
   process_users
 end
