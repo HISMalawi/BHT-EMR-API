c7c550e8c7e279f24391bf52cdbb9902fbf42690
Roy Chanunkha <roychanunkha@gmail.com>
2023-11-09 16:43:43 +0200

    fixes on dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   mount Lab::Engine => '/'
-  mount Radiology::Engine => '/'
+  # mount Radiology::Engine => '/'
   mount EmrOhspInterface::Engine => '/'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
