81ab3fdaa4006d0aa99ca2c7f942d26b44d8a74a
bryan <brianmsyamboza@gmail.com>
2023-12-19 13:24:12 +0200

    Update dump metadata script
    with the recent developments, metadata is being managed on a seperate server, therefore, the script to dump this metadata into project files has also been updated to enable easy pushing of up-to-date of this metadata

--- bin/dump_metadata.sh
+++ bin/dump_metadata.sh
@@ -3,20 +3,12 @@
 BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_PATH="`dirname $BIN_PATH`"
 
-USERNAME=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['username']"`
-PASSWORD=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['password']"`
-DATABASE=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['database']"`
-HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['host']"`
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['metadata']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['metadata']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['metadata']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['metadata']['host']"`
 
 METADATA_FILE=${ROOT_PATH}/db/sql/openmrs_metadata_1_7.sql
-
-if [ -n `mysqldump --version | cut -d ' ' -f 4 | grep -P '8\.\d+\.\d+.*'` ]; then
-  ARGS='--disable-column-statistics'
-else
-  ARGS=''
-fi
-
-set -x
 
 mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $ARGS $DATABASE \
   concept concept_name concept_set concept_answer concept_class concept_datatype \
@@ -24,4 +16,4 @@ mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $ARGS $DATABASE \
   concept_numeric concept_proposal concept_proposal_tag_map concept_set_derived concept_source \
   concept_state_conversion concept_synonym concept_word encounter_type patient_identifier_type \
   order_type person_attribute_type program program_workflow program_workflow_state \
-  relationship_type drug privilege location role
+  relationship_type drug privilege location role > $METADATA_FILE
