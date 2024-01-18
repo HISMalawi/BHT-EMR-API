16817b66385932bb6a33e51b450e13f0b193bc1f
Roy Chanunkha <roychanunkha@gmail.com>
2024-01-18 10:53:10 +0200

    queries lints;

--- app/queries/tb_queries/patient_states_query.rb
+++ app/queries/tb_queries/patient_states_query.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
 
-  class TbQueries::PatientStatesQuery
+module TbQueries
+  class PatientStatesQuery
     STATES = {
       'TREATMENT_COMPLETE' => 93,
       'TREATMENT_FAILED' => 99,
@@ -14,11 +16,11 @@
 
     NORMAL_TREATMENT_LENGTH_IN_DAYS = 168
 
-    def initialize (relation = PatientState.includes(:patient_program))
+    def initialize(relation = PatientState.includes(:patient_program))
       @relation = relation
     end
 
-    def relapse (ids, start_date, end_date)
+    def relapse(ids, start_date, end_date)
       states = @relation.where('patient_program.patient_id': ids,
                                state: STATES['RELAPSE'],
                                'patient_state.date_created': start_date..end_date)
@@ -28,7 +30,7 @@
       states.map { |state| state.patient_program.patient_id }
     end
 
-    def any_relapse (start_date, end_date)
+    def any_relapse(start_date, end_date)
       states = @relation.where(state: STATES['RELAPSE'],
                                'patient_state.date_created': start_date..end_date)
 
@@ -37,7 +39,7 @@
       states.map { |state| state.patient_program.patient_id }
     end
 
-    def defaulted (ids)
+    def defaulted(ids)
       states = @relation.where(state: STATES['DEFAULTED'],
                                patient_program: { patient_id: ids },
                                end_date: nil)
@@ -47,7 +49,7 @@
       states.map { |bar| bar.patient_program.patient_id }
     end
 
-    def treatment_failed (ids, start_date, end_date)
+    def treatment_failed(ids, start_date, end_date)
       states = @relation.where(state: STATES['TREATMENT_FAILED'],
                                patient_program: { patient_id: ids },
                                'patient_state.date_created': start_date..end_date)
@@ -57,9 +59,9 @@
       states.map { |bar| bar.patient_program.patient_id }
     end
 
-    def any (state, patients, start_date, end_date)
+    def any(state, patients, start_date, end_date)
       states = @relation.where('patient_program.patient_id': patients,
-                               state: state,
+                               state:,
                                'patient_state.date_created': start_date..end_date)
 
       return [] if states.empty?
@@ -67,35 +69,34 @@
       states.map { |foo| foo.patient_program.patient_id }
     end
 
-    def in_art_treatment (start_date, end_date)
-      states = @relation.where(state: STATES['ART_TREATMENT'],
-                               'patient_state.date_created': start_date..end_date)
+    def in_art_treatment(start_date, end_date)
+      @relation.where(state: STATES['ART_TREATMENT'],
+                      'patient_state.date_created': start_date..end_date)
     end
 
-    def in_tb_treatment (start_date, end_date)
-      states = @relation.where(state: STATES['CURRENTLY_IN_TREATMENT'],
-                               'patient_state.date_created': start_date..end_date)
+    def in_tb_treatment(start_date, end_date)
+      @relation.where(state: STATES['CURRENTLY_IN_TREATMENT'],
+                      'patient_state.date_created': start_date..end_date)
     end
 
-    def other_previous_treatment ()
+    def other_previous_treatment
       states = @relation.where(state: STATES['CURRENTLY_IN_TREATMENT'],
                                end_date: nil)\
                         .where('DATEDIFF(NOW(), patient_state.date_created) > ?', NORMAL_TREATMENT_LENGTH_IN_DAYS)\
                         .or(@relation.where(state: STATES['UNKNOWN'],
-                                           end_date: nil))
+                                            end_date: nil))
 
       return [] if states.empty?
 
       states.map do |foo|
-        if (foo.patient_program.nil?)
-          next
-        end
+        next if foo.patient_program.nil?
+
         foo.patient_program.patient_id
       end
     end
 
-    def still_open (state, patients)
-      states = @relation.where(state: state,
+    def still_open(state, patients)
+      states = @relation.where(state:,
                                patient_program: { patient_id: patients },
                                end_date: nil)
 
@@ -104,3 +105,4 @@
       states.map { |bar| bar.patient_program.patient_id }
     end
   end
+end
