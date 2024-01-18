2a70e6014d9714711fb68628a337aec420151f1b
Roy Chanunkha <roychanunkha@gmail.com>
2024-01-18 13:53:25 +0200

    updating loading yaml file

--- bin/dump_drug_cms.sh
+++ bin/dump_drug_cms.sh
@@ -3,10 +3,11 @@
 BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_PATH="`dirname $BIN_PATH`"
 
-USERNAME=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['username']"`
-PASSWORD=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['password']"`
-DATABASE=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['database']"`
-HOST=`ruby -ryaml -e "puts YAML::load_file('$ROOT_PATH/config/database.yml')['development']['host']"`
+USERNAME=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['database']"`
+HOST=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['host']"`
+PORT=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['port']"`
 
 METADATA_FILE=${ROOT_PATH}/db/sql/drug_cms_metadata.sql
 
@@ -18,4 +19,4 @@ fi
 
 set -x
 
-mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST $ARGS $DATABASE drug_cms
+mysqldump -u $USERNAME --password=$PASSWORD --host=$HOST --port=$PORT $ARGS $DATABASE drug_cms
