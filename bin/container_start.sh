1c65738e29374b3952334fda787501293bfeefbd
Roy Chanunkha <roychanunkha@gmail.com>
2024-02-20 12:07:04 +0200

    using docker compose for setting up dev container

--- bin/container_start.sh
+++ bin/container_start.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-source ~/.bashrc
+# we need to check for configuration files and create them if they don't exist
+if [ ! -f config/database.yml ]; then
+    cp config/database.yml.example config/database.yml
+
+    # in config/database.yml replace 3308 with 3306
+    sed -i 's/3308/3306/g' config/database.yml
+    sed -i 's/your_scoped_username/root/g' config/database.yml
+    sed -i 's/your_secret_password/root/g' config/database.yml
+fi
+
+# application.yml
+if [ ! -f config/application.yml ]; then
+    cp config/application.yml.example config/application.yml
+fi
+
+# ait.yml
+if [ ! -f config/ait.yml ]; then
+    cp config/ait.yml.example config/ait.yml
+fi
+
+# cable.yml
+if [ ! -f config/cable.yml ]; then
+    cp config/cable.yml.example config/cable.yml
+fi
+
+# storage.yml
+if [ ! -f config/storage.yml ]; then
+    cp config/storage.yml.example config/storage.yml
+fi
+
+if [ ! -f config/secrets.yml ]; then
+    bash bin/setup_production_mode.sh
+fi
 
 rm Gemfile.lock
 
--- bin/post_container_setup.sh
+++ bin/post_container_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mysqld_safe &
-
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 curl -sSL https://get.rvm.io | bash -s stable
