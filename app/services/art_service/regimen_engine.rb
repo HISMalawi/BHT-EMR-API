7a3adec67fc7cd4e0d317ffb607f12b5d4cd2861
Roy Chanunkha <roychanunkha@gmail.com>
2024-03-28 09:15:25 +0200

    fix: adding keyword arguments for regimens

--- app/services/art_service/regimen_engine.rb
+++ app/services/art_service/regimen_engine.rb
@@ -51,13 +51,13 @@ module ArtService
       ingredients.collect { |ingredient| ingredient_to_drug(ingredient) }
     end
 
-    def find_regimens_by_patient(patient, lpv_drug_type: 'tabs')
+    def find_regimens_by_patient(patient:, lpv_drug_type: 'tabs')
       use_tb_dosage = use_tb_patient_dosage?(dtg_drugs.first, patient)
-      find_regimens(patient.weight, use_tb_dosage:,
-                                    lpv_drug_type:)
+      find_regimens(patient_weight: patient.weight, use_tb_dosage:,
+                    lpv_drug_type:)
     end
 
-    def find_regimens(patient_weight, use_tb_dosage: false, lpv_drug_type: 'tabs')
+    def find_regimens(patient_weight:, use_tb_dosage: false, lpv_drug_type: 'tabs')
       ingredients = MohRegimenIngredient.where(
         '(CAST(min_weight AS DECIMAL(4, 1)) <= :weight
          AND CAST(max_weight AS DECIMAL(4, 1)) >= :weight AND ingredient_active = :active)',
