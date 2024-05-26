9d6bea597d212e7cd8b52c97c84bf3dd512e1f88
dominickasanga <dominickasanga@gmail.com>
2024-05-26 12:50:20 +0200

    modified:   app/controllers/api/v1/administer_vaccine_controller.rb

--- app/controllers/api/v1/administer_vaccine_controller.rb
+++ app/controllers/api/v1/administer_vaccine_controller.rb
@@ -3,23 +3,25 @@ module Api
     class AdministerVaccineController < ApplicationController
         
         def administer_vaccine
-          encounter_id, drug_orders = params.require(%i[encounter_id drug_orders program_id])
+          encounter_id, drug_orders, program_id = params.require(%i[encounter_id drug_orders program_id])
 
 
           encounter = Encounter.find(encounter_id)
 
-          unless encounter.type.name = 'IMMUNIZATION'
+          unless encounter.type.name = 'IMMUNIZATION RECORD'
             return render json: { errors: "Not an immunization encounter ##{encounter.encounter_id}"},
                           status: :bad_request
           end
 
           
-          orders = DrugOrderService.create_drug_orders(encounter, drug_orders)
+          orders = DrugOrderService.create_drug_orders(encounter: , drug_orders:)
 
           program = Program.find(program_id)
           provider = params[:provider_id] ? Person.find(params[:provider_id]) : User.current.person
 
-          render json: DispensationService.create(program, dispensations, provider), status: :created
+          # missing dispensations
+
+          # render json: DispensationService.create(program, dispensations, provider), status: :created
           
           render json: orders, status: :created
         end
