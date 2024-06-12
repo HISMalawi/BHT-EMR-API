d1a23bd60a17d4aea171241c4fab9263bbae5cbd
pamzey <mvumapatrick@gmail.com>
2024-06-12 11:06:13 +0200

    added fix to screening method

--- app/services/cxca_service/reports/clinic/reason_for_not_screening_report.rb
+++ app/services/cxca_service/reports/clinic/reason_for_not_screening_report.rb
@@ -9,7 +9,7 @@ module CxcaService
         include Utils
         include ModelUtils
 
-        def initialize(start_date:, end_date:)
+        def initialize(start_date:, end_date:, **kwargs)
           @start_date = start_date.to_date.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S')
           @end_date = end_date.to_date.end_of_day.strftime('%Y-%m-%d %H:%M:%S')
         end
