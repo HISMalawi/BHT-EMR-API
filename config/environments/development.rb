3c3acc744e859f6ed12440de476adeaee1865a3b
dominickasanga <dominickasanga@gmail.com>
2024-06-19 18:46:29 +0200

    modified:   config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.hosts << "apps.linmalawi.org"
+  config.hosts << ""
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
