033bd57d3f5bb8bfe9794c1a40cae34d554ec45e
Senzenjani <smanjawira@gmail.com>
2024-06-24 14:56:57 +0200

    Fetching vaccinated stats for women, men and child

--- app/services/immunization_service/reports/stats/immunization_dashboard.rb
+++ app/services/immunization_service/reports/stats/immunization_dashboard.rb
@@ -14,14 +14,14 @@ module ImmunizationService
                         total_missed_doses:,
                         total_vaccinated_today:,
                         total_children_vaccinated_today:,
-                        total_women_vaccincated_today:,
-                        total_men_vaccincated_today:
+                        total_women_vaccinated_today:,
+                        total_men_vaccinated_today:
                     }
                 end
 
                 def base_query
                     Observation.joins(concept: :concept_names, 
-                                encounter: %i[program type])
+                                encounter: %i[program type], person: [])
                                .where( program: { program_id: 33 })
                 end
 
@@ -46,15 +46,32 @@ module ImmunizationService
                 end
 
                 def total_children_vaccinated_today
+                    cutoff_date = 18.years.ago.to_date
 
+                    base_query.where(
+                        encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                        concept_name: { name: "Batch Number"},
+                        obs: { obs_datetime: @start_date..@end_date },
+                        person: { birthdate: cutoff_date..Date.today}
+                    ).select(:concept_id).count
                 end
 
-                def total_women_vaccincated_today
-
+                def total_women_vaccinated_today
+                    base_query.where(
+                        encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                        concept_name: { name: "Batch Number"},
+                        obs: { obs_datetime: @start_date..@end_date },
+                        person: { gender: "F" }
+                    ).select(:concept_id).count
                 end
 
-                def total_men_vaccincated_today
-
+                def total_men_vaccinated_today
+                    base_query.where(
+                        encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                        concept_name: { name: "Batch Number"},
+                        obs: { obs_datetime: @start_date..@end_date },
+                        person: { gender: "M" }
+                    ).select(:concept_id).count
                 end
 
             end
