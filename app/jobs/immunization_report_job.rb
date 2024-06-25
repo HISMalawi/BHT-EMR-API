cd998d2b070b3f6df40629ddd4125d84f3d2f9be
Senzenjani <smanjawira@gmail.com>
2024-06-25 10:20:06 +0200

    Added immunization report job

---
+++ app/jobs/immunization_report_job.rb
@@ -0,0 +1,11 @@
+# app/jobs/immunization_report_job.rb
+class ImmunizationReportJob < ApplicationJob
+  queue_as :default
+
+  def perform(start_date, end_date)
+    dashboard = ImmunizationService::Reports::Stats::ImmunizationDashboard.new(start_date: start_date, end_date: end_date)
+    data = dashboard.data
+
+    ActionCable.server.broadcast("immunization_report", data)
+  end
+end
