7292e8252fb98081d42518a409707b806c50ca8e
Senzenjani <smanjawira@gmail.com>
2024-06-25 13:44:41 +0200

    Fixed logic error in coming up with number of women and men vaccinated

--- app/services/immunization_service/reports/stats/immunization_dashboard.rb
+++ app/services/immunization_service/reports/stats/immunization_dashboard.rb
@@ -34,7 +34,7 @@ module ImmunizationService
                 end
 
                 def  total_due_for_vaccination_today
-
+                    
                 end
 
                 def total_missed_doses
@@ -42,8 +42,15 @@ module ImmunizationService
                 end
 
                 def total_vaccinated_today
-
+                    current_date = Date.current
+                  
+                    base_query.where(
+                      encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                      concept_name: { name: "Batch Number" },
+                      obs: { obs_datetime: current_date.beginning_of_day..current_date.end_of_day }
+                    ).select(:concept_id).count
                 end
+                  
 
                 def total_children_vaccinated_today
                     cutoff_date = 18.years.ago.to_date
@@ -57,23 +64,27 @@ module ImmunizationService
                 end
 
                 def total_women_vaccinated_today
+                    eighteen_years_ago = 18.years.ago.to_date
+                  
                     base_query.where(
-                        encounter_type: { name: "IMMUNIZATION RECORD" }, 
-                        concept_name: { name: "Batch Number"},
-                        obs: { obs_datetime: @start_date..@end_date },
-                        person: { gender: "F" }
+                      encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                      concept_name: { name: "Batch Number" },
+                      obs: { obs_datetime: Date.current.beginning_of_day..Date.current.end_of_day },
+                      person: { gender: "F", birthdate: eighteen_years_ago..Float::INFINITY }
                     ).select(:concept_id).count
                 end
-
+                  
                 def total_men_vaccinated_today
+                    eighteen_years_ago = 18.years.ago.to_date
+                  
                     base_query.where(
-                        encounter_type: { name: "IMMUNIZATION RECORD" }, 
-                        concept_name: { name: "Batch Number"},
-                        obs: { obs_datetime: @start_date..@end_date },
-                        person: { gender: "M" }
+                      encounter_type: { name: "IMMUNIZATION RECORD" }, 
+                      concept_name: { name: "Batch Number" },
+                      obs: { obs_datetime: Date.current.beginning_of_day..Date.current.end_of_day },
+                      person: { gender: "M", birthdate: eighteen_years_ago..Float::INFINITY }
                     ).select(:concept_id).count
                 end
-
+                  
             end
         end
     end
