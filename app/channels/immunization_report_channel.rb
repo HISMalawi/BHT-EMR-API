66cfd1f423575f448a3a92294937e96709d18cd1
Senzenjani <smanjawira@gmail.com>
2024-06-26 16:00:32 +0200

    Removed fetch data action and changed immunization report to immunization report channel

--- app/channels/immunization_report_channel.rb
+++ app/channels/immunization_report_channel.rb
@@ -1,18 +1,10 @@
 class ImmunizationReportChannel < ApplicationCable::Channel
 
     def subscribed
-        stream_from "immunization_report"
+        stream_from "immunization_report_channel"
     end
 
     def unsubscribed
         # Any cleanup needed when channel is unsubscribed
     end 
-
-    def fetch_data(data)
-        start_date = data['start_date']
-        end_date = data['end_date']
-        ImmunizationReportJob.perform_later(start_date, end_date)
-    end
-
-
 end 
