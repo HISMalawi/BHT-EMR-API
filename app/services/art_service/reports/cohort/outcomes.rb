59951884e8e9c6494fbcd185b06c642d5235c875
Roy Chanunkha <roychanunkha@gmail.com>
2024-06-29 14:50:06 +0200

    chore: handling patient death state

--- app/services/art_service/reports/cohort/outcomes.rb
+++ app/services/art_service/reports/cohort/outcomes.rb
@@ -243,7 +243,7 @@ module ArtService
             SELECT tesd.patient_id, 'Patient died', MAX(ps.start_date), 1
             FROM temp_earliest_start_date tesd
             INNER JOIN patient_program pp ON pp.patient_id = tesd.patient_id AND pp.program_id = 1 AND pp.voided = 0
-            INNER JOIN patient_state ps ON ps.patient_program_id = pp.patient_program_id AND ps.state = 3 AND ps.voided = 0 AND ps.start_date <= #{end_date}
+            INNER JOIN patient_state ps ON ps.patient_program_id = pp.patient_program_id AND ps.state = 3 AND ps.voided = 0 AND ps.start_date <= DATE(#{start ? start_date : end_date}) #{start ? '- INTERVAL 1 DAY' : ''}
             WHERE tesd.patient_id NOT IN (SELECT patient_id FROM temp_patient_outcomes#{start ? '_start' : ''} WHERE step = 1)
             AND tesd.date_enrolled < DATE(#{start ? start_date : end_date}) #{start ? '' : '+ INTERVAL 1 DAY'}
             GROUP BY tesd.patient_id
