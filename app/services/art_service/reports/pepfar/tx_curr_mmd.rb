aecdb1acf69b0e412ed9ef507d8038aa0bbcc353
Roy Chanunkha <roychanunkha@gmail.com>
2024-07-03 15:08:09 +0200

    fix: tx curr mmd

--- app/services/art_service/reports/pepfar/tx_curr_mmd.rb
+++ app/services/art_service/reports/pepfar/tx_curr_mmd.rb
@@ -54,7 +54,7 @@ module ArtService
                 disaggregated_age_group(tesd.birthdate, '#{@end_date}') age_group,
                 TIMESTAMPDIFF(DAY, tcm.start_date, MIN(tcm.expiry_date)) prescribed_days
             FROM temp_earliest_start_date tesd
-            INNER JOIN temp_patient_outcomes tpo ON tpo.patient_id = tesd.patient_id AND tpo.cum_outcome = 'On antiretrovirals'
+            INNER JOIN temp_patient_outcomes tpo ON tpo.patient_id = tesd.patient_id AND tpo.pepfar_cum_outcome = 'On antiretrovirals'
             INNER JOIN temp_current_medication tcm ON tcm.patient_id = tesd.patient_id
             WHERE tesd.date_enrolled <= '#{@end_date}'
             GROUP BY tesd.patient_id;
