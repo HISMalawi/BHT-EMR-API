54cca4a14d7bd01be035705234c87f4e5ecbe4fb
Roy Chanunkha <roychanunkha@gmail.com>
2024-07-03 09:06:46 +0200

    fix: cxca reports

--- app/services/cxca_service/reports/pepfar/cxca_tx.rb
+++ app/services/cxca_service/reports/pepfar/cxca_tx.rb
@@ -19,7 +19,7 @@ module CxcaService
 
         TREATMENTS = %i[thermocoagulation cryotherapy leep].freeze
 
-        def initialize(start_date:, end_date:)
+        def initialize(start_date:, end_date:, **_kwargs)
           @start_date = start_date.to_date.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S')
           @end_date = end_date.to_date.end_of_day.strftime('%Y-%m-%d %H:%M:%S')
           @report = {}
