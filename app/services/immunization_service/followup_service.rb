631c50aeffb12fb6725116d6ef0106036681cc2c
Senzenjani <smanjawira@gmail.com>
2024-07-24 14:39:05 +0200

    Added immunization followup service

---
+++ app/services/immunization_service/followup_service.rb
@@ -0,0 +1,14 @@
+module ImmunizationService
+  class Followup
+
+    def initialize
+
+    end
+
+    # Fetch all missed immunization or milestones 
+    def fetch_missed_immunizations
+        
+    end
+    
+  end
+end
