68e38d77b49b4275812499fba1898891c8b737b9
Senzenjani <smanjawira@gmail.com>
2024-07-24 19:57:55 +0200

    Modified name controller to use the new service reference

--- app/controllers/api/v1/vaccine_schedule_controller.rb
+++ app/controllers/api/v1/vaccine_schedule_controller.rb
@@ -3,7 +3,7 @@ require './app/services/immunization_service/vaccine_schedule_service'
 class Api::V1::VaccineScheduleController < ApplicationController
   def vaccine_schedule
     patient = Person.find(immunization_schedule_params[:patient_id].to_i)
-    vaccine_schedule = VaccineScheduleService.vaccine_schedule(patient)
+    vaccine_schedule = ImmunizationService::VaccineScheduleService.vaccine_schedule(patient)
     render json: vaccine_schedule, status: :ok
   rescue StandardError => e
     render json: { error: e.message }, status: :unprocessable_entity
