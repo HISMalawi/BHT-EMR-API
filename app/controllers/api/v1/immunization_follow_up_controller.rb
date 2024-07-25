0c9defe66c9a4c1df792f5bee82e006717e00ea6
Senzenjani <smanjawira@gmail.com>
2024-07-25 16:08:35 +0200

    Updated controller to be passing location when getting missed immunizations

--- app/controllers/api/v1/immunization_follow_up_controller.rb
+++ app/controllers/api/v1/immunization_follow_up_controller.rb
@@ -2,7 +2,7 @@
  class Api::V1::ImmunizationFollowUpController < ApplicationController
       
     def missed_immunizations
-      missed_visits = service.fetch_missed_immunizations
+      missed_visits = service.fetch_missed_immunizations(User.current.location_id)
 
       unless missed_visits.empty?
         render json: missed_visits, status: :ok
