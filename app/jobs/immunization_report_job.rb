8e7c3107010ef3e959f9bfddcc22cee182439334
Senzenjani <smanjawira@gmail.com>
2024-07-25 14:57:36 +0200

    Moved broadcasting of data to dashboard job

--- app/jobs/immunization_report_job.rb
+++ app/jobs/immunization_report_job.rb
@@ -2,10 +2,12 @@
 class ImmunizationReportJob < ApplicationJob
   queue_as :default
 
-  def perform(start_date, end_date)
-    dashboard = ImmunizationService::Reports::Stats::ImmunizationDashboard.new(start_date: start_date, end_date: end_date)
+  def perform(start_date, end_date, location_id)
+    dashboard = ImmunizationService::Reports::Stats::ImmunizationDashboard.new(start_date: start_date, end_date: end_date, location_id: location_id)
     data = dashboard.data
 
-    ActionCable.server.broadcast('immunization_report_channel', data)
+    immunization_cache = ImmunizationCacheDatum.find_or_initialize_by(name: "dashboard_stats")
+    immunization_cache.update!(value: data)
+    
   end
 end
