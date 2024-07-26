b221a8fc052038dc91ae626544f870761ab93f29
Senzenjani <smanjawira@gmail.com>
2024-07-26 12:05:43 +0200

    Fetching missed immunizattion from  immunization cached datum via end-point

--- app/controllers/api/v1/immunization_follow_up_controller.rb
+++ app/controllers/api/v1/immunization_follow_up_controller.rb
@@ -2,18 +2,13 @@
  class Api::V1::ImmunizationFollowUpController < ApplicationController
       
     def missed_immunizations
-      missed_visits = service.fetch_missed_immunizations(User.current.location_id)
+      missed_visits = ImmunizationCacheDatum.where(:name=>"missed_immunizations")
 
       unless missed_visits.empty?
         render json: missed_visits, status: :ok
       else
         render json: {}, status: :not_found
       end
-    end
-
-    private 
-    def service
-      ImmunizationService::FollowUp.new()
     end
 
 end
