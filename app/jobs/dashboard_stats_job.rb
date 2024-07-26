4de221471a572d25533fd50996a1f68e8d79e690
Senzenjani <smanjawira@gmail.com>
2024-07-26 15:44:40 +0200

    Removed returning of missed immunization cached data from web socket only returning dashboard_stats

--- app/jobs/dashboard_stats_job.rb
+++ app/jobs/dashboard_stats_job.rb
@@ -3,15 +3,9 @@ class DashboardStatsJob < ApplicationJob
   queue_as :default
 
   def perform
-    dashboard_stats = ImmunizationCacheDatum.where(:name => "dashboard_stats")
-    missed_visits = ImmunizationCacheDatum.where(:name=>"missed_immunizations")
+    dashboard_stats = ImmunizationCacheDatum.where(:name => "dashboard_stats").pick(:value)
 
-    data = {
-        dashboard_stats: dashboard_stats,
-        missed_visits: missed_visits
-    }
-
-    ActionCable.server.broadcast('immunization_report_channel', data)
+    ActionCable.server.broadcast('immunization_report_channel', dashboard_stats)
   end
 
 end
