0bb974c20abac0d1e39f8dceba3cb8042f9df877
Senzenjani <smanjawira@gmail.com>
2024-07-26 15:47:29 +0200

    Added logic overdue logic

--- app/jobs/immunization_report_job.rb
+++ app/jobs/immunization_report_job.rb
@@ -3,11 +3,34 @@ class ImmunizationReportJob < ApplicationJob
   queue_as :default
 
   def perform(start_date, end_date, location_id)
-    dashboard = ImmunizationService::Reports::Stats::ImmunizationDashboard.new(start_date: start_date, end_date: end_date, location_id: location_id)
-    data = dashboard.data
+    dashboard_stats = dashboard_service(start_date, end_date, location_id)
+    dashboard_stats = dashboard_stats.data
+
+    missed_visits = followup_service.fetch_missed_immunizations(location_id)
+
+    dashboard_stats[:under_five_overdue] = missed_visits[:under_five_count]
+    dashboard_stats[:over_five_overdue] = missed_visits[:over_five_count]
 
     immunization_cache = ImmunizationCacheDatum.find_or_initialize_by(name: "dashboard_stats")
-    immunization_cache.update!(value: data)
+    immunization_cache.update!(value: dashboard_stats)
+
+
+    immunization_cache = ImmunizationCacheDatum.find_or_initialize_by(name: "missed_immunizations")
+    immunization_cache.update!(value: missed_visits)
     
   end
+
+  private 
+
+  def dashboard_service(start_date, end_date, location_id)
+    ImmunizationService::Reports::Stats::ImmunizationDashboard.new(start_date: start_date,
+                                                                   end_date: end_date, 
+                                                                   location_id: location_id)
+  end
+
+  def followup_service
+    ImmunizationService::FollowUp.new
+  end
+
+
 end
