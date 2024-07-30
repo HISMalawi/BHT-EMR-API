5f268c200223cfafd6e35a0826a41bdea7972ae3
Senzenjani <smanjawira@gmail.com>
2024-07-30 16:52:21 +0200

    Removed unwanted and renamed data to match the labels displayed on dashboard

--- app/services/immunization_service/reports/stats/immunization_dashboard.rb
+++ app/services/immunization_service/reports/stats/immunization_dashboard.rb
@@ -16,14 +16,10 @@ module ImmunizationService
                         total_client_registered:,
                         total_male_registered:,
                         total_female_registered:,
-                        total_vaccinated:,
-                        total_due_for_vaccination_today:,
-                        total_missed_doses:,
-                        total_vaccinated_today:,
-                        total_children_vaccinated_today:,
-                        total_women_vaccinated_today:,
-                        total_men_vaccinated_today:,
-                        vaccination_counts_by_month:
+                        total_vaccinated_this_year:,
+                        total_female_vaccinated_this_year:,
+                        total_male_vaccinated_this_year:,
+                        vaccination_counts_by_month:,
                     }
                 end
 
@@ -66,7 +62,7 @@ module ImmunizationService
                     .count(:person_id)
                 end
 
-                def total_vaccinated
+                def total_vaccinated_this_year
                     base_query
                       .where(
                         encounter_type: { name: "IMMUNIZATION RECORD" },
@@ -76,61 +72,26 @@ module ImmunizationService
                       .distinct
                       .count(:person_id)
                 end
-                  
-
-                def  total_due_for_vaccination_today
-                    
-                end
-
-                def total_missed_doses
-
-                end
-
-                def total_vaccinated_today
-                    base_query
-                      .where(
-                        encounter_type: { name: "IMMUNIZATION RECORD" },
-                        concept_name: { name: "Batch Number" },
-                        obs: { obs_datetime: @current_date.beginning_of_day..@current_date.end_of_day }
-                      )
-                      .distinct
-                      .count(:person_id)
-                  end
-                  
-                def total_children_vaccinated_today
+                         
+                def total_female_vaccinated_this_year
                     base_query
                         .where(
                         encounter_type: { name: "IMMUNIZATION RECORD" },
                         concept_name: { name: "Batch Number" },
-                        obs: { obs_datetime: @current_date.beginning_of_day..@current_date.end_of_day }
-                        )
-                        .where('TIMESTAMPDIFF(YEAR, person.birthdate, CURDATE()) < 18')
-                        .distinct
-                        .count(:person_id)
-                end
-                  
-                def total_women_vaccinated_today
-                    base_query
-                        .where(
-                        encounter_type: { name: "IMMUNIZATION RECORD" },
-                        concept_name: { name: "Batch Number" },
-                        obs: { obs_datetime: @current_date.beginning_of_day..@current_date.end_of_day },
+                        obs: { obs_datetime: @start_date..@end_date },
                         person: { gender: "F" }
-                        )
-                        .where('TIMESTAMPDIFF(YEAR, person.birthdate, CURDATE()) >= 18')
-                        .distinct
+                        ).distinct
                         .count(:person_id)
                 end
                   
-                def total_men_vaccinated_today
+                def total_male_vaccinated_this_year
                     base_query
                         .where(
                         encounter_type: { name: "IMMUNIZATION RECORD" },
                         concept_name: { name: "Batch Number" },
-                        obs: { obs_datetime: @current_date.beginning_of_day..@current_date.end_of_day },
+                        obs: { obs_datetime: @start_date..@end_date },
                         person: { gender: "M" }
                         )
-                        .where('TIMESTAMPDIFF(YEAR, person.birthdate, CURDATE()) >= 18')
                         .distinct
                         .count(:person_id)
                 end
@@ -160,8 +121,6 @@ module ImmunizationService
                     
                     { months: months.reverse, vaccinations: vaccinations.reverse }
                 end
-
-                
             end
         end
     end
