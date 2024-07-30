189c55e6fd6ade07970fa7bfdfd505de3613e5fa
Senzenjani <smanjawira@gmail.com>
2024-07-30 16:49:35 +0200

    Appending due stats to dashbord stats value

--- app/jobs/immunization_report_job.rb
+++ app/jobs/immunization_report_job.rb
@@ -2,6 +2,8 @@
 class ImmunizationReportJob < ApplicationJob
   queue_as :default
 
+  sidekiq_options unique: :until_executed
+
   def perform(start_date, end_date, location_id)
     dashboard_stats = dashboard_service(start_date, end_date, location_id)
     dashboard_stats = dashboard_stats.data
@@ -10,6 +12,9 @@ class ImmunizationReportJob < ApplicationJob
 
     dashboard_stats[:under_five_overdue] = missed_visits[:under_five_count]
     dashboard_stats[:over_five_overdue] = missed_visits[:over_five_count]
+    dashboard_stats[:due_today_count] = missed_visits[:due_today_count]
+    dashboard_stats[:due_this_week_count] = missed_visits[:due_this_week_count]
+    dashboard_stats[:due_this_month_count] = missed_visits[:due_this_month_count]
 
     immunization_cache = ImmunizationCacheDatum.find_or_initialize_by(name: "dashboard_stats")
     immunization_cache.update!(value: dashboard_stats)
