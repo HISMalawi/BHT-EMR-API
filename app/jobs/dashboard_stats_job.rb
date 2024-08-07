dd6bae4123cd29148d71167127137ffffc5fd0cc
Fredrick Chiyenda <fchiyenda@gmail.com>
2024-08-07 23:48:44 +0200

    Added job uniquness until it is executing

--- app/jobs/dashboard_stats_job.rb
+++ app/jobs/dashboard_stats_job.rb
@@ -2,8 +2,6 @@ class DashboardStatsJob < ApplicationJob
 
   queue_as :default
 
-  sidekiq_options unique: :until_executed
-
   def perform(location_id)
     dashboard_stats = ImmunizationCacheDatum.where(name: "dashboard_stats", 
                                             location_id: location_id).pick(:value)
