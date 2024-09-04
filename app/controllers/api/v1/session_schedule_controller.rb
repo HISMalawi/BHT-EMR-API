3704a975a46837b6fefb9609f57eec2d8f912434
Senzenjani <smanjawira@gmail.com>
2024-09-04 15:22:06 +0200

    Created end-point for saving immunization session schedule

---
+++ app/controllers/api/v1/session_schedule_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::SessionScheduleController < ApplicationController
+
+    def index
+        
+    end 
+
+    def show 
+
+    end
+
+    def create
+      params = get_schedule_session_params 
+
+    end
+
+    def update
+
+    end 
+
+    def destroy
+
+    end
+
+    def get_schedule_session_params
+
+    end
+
+    private 
+
+    def get_schedule_session_params
+
+    end 
+end
