6d7278fa0160a9eb200290be20f82b34f741eb24
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-09-30 00:34:49 +0200

    validate stage name

--- app/controllers/api/v1/stages_controller.rb
+++ app/controllers/api/v1/stages_controller.rb
@@ -1,35 +1,32 @@
 module Api
-    module V1
-      class StagesController < ApplicationController
+  module V1
+    class StagesController < ApplicationController
 
-      
-        # GET /api/v1/stag pes
-        def index
-          stages = Stage.includes(:patient).all
+      def index
+        stages = Stage.includes(:patient).all
+        stages_with_names = stages.map do |stage|
+          stage.as_json.merge(
+            fullName: stage.patient.name
+          )
+        end
+        render json: stages_with_names, status: :ok
+      end
 
-          stages_with_names = stages.map do |stage|
-          
-            stage.as_json.merge(
-                fullName: stage.patient.name
-            )
-          end
-          render json: stages_with_names, status: :ok
+      def create
+        stage = Stage.new(stage_params)
+        
+        if stage.save
+          render json: { message: 'Stage created successfully', stage: stage }, status: :created
+        else
+          render json: { errors: stage.errors.full_messages }, status: :unprocessable_entity
         end
-  
-        def create
-          stage = Stage.new(stage_params)
-          if stage.save
-            render json: { message: 'Stage created successfully', stage: stage }, status: :created
-          else
-            render json: { errors: stage.errors.full_messages }, status: :unprocessable_entity
-          end
-        end
-  
-        private
-        def stage_params
-          params.permit(:patient_id, :stage, :arrivalTime, :visit_id, :status)
-        end
+      end
+
+      private
+
+      def stage_params
+        params.require(:stage).permit(:patient_id, :stage, :arrivalTime, :visit_id, :status)
       end
     end
   end
-  
+end
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,6 +1,11 @@
 class Stage < ApplicationRecord
-    validates :patient_id, :stage, :arrivalTime, :visit_id, :status, presence: true
-
     belongs_to :visit 
     belongs_to :patient 
+  
+    VALID_STAGES = %w[VITALS CONSULTATION DISPENSATION].freeze
+
+    validates :stage, inclusion: { in: VALID_STAGES, message: "%{value} is not a valid stage" }
+    
+    validates :patient_id, :arrivalTime, :visit_id, :status, presence: true
 end
+  
