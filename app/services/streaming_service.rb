27292de7ab7d4fa5ee5f4bf636139e0b1ba29e20
Daniel Justin <danieljmuleke@gmail.com>
2024-11-14 09:20:03 +0200

    chore: implemented method to generate visit data for streaming service

--- app/services/streaming_service.rb
+++ app/services/streaming_service.rb
@@ -2,25 +2,33 @@ class StreamingService
 
   attr_accessor :patient, :program_id, :date
 
-
   def initialize(patient_id:, program_id:, date:)
     @patient = Patient.find(patient_id)
     @program_id = program_id
     @date = date
   end
 
+  def generate_visit_data
+    Encounter.where(patient_id: @patient.patient_id, program_id: @program_id)\
+            .where('encounter_datetime BETWEEN ? AND ?', *TimeUtils.day_bounds(@date))\
+            .includes(
+              %i[type location program observations],
+              patient: [
+                :patient_identifiers, 
+                person: %i[
+                  names 
+                  person_attributes
+                ]
+              ],
+              provider: [:names],
+              orders: [:drug_order]
+            )
+  end
+
   def stream_complete_visit
     puts "Running stream complete visit for patient: #{@patient.name}"
-    encounters =  patient.encounters.to_json
-    raise encounters.inspect
-
-    # encounters
-      # obs
-    # patientiddemographics
-    # site data {id and ip addr}
-    # orders
-      # lab
-      # drug
+    encounters =  generate_visit_data
+    # raise encounters.inspect
   end
 
   def stream_patient
