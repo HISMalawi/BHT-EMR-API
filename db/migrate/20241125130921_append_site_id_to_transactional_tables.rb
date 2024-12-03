4c45ffe449e51e8ce399c4f77a464e079176324d
Brian M <brianmsyamboza@gmail.com>
2024-12-03 09:42:45 +0200

    update script

--- db/migrate/20241125130921_append_site_id_to_transactional_tables.rb
+++ db/migrate/20241125130921_append_site_id_to_transactional_tables.rb
@@ -3,12 +3,11 @@
 class AppendSiteIdToTransactionalTables < ActiveRecord::Migration[7.0]
   TRANSACTIONAL_TABLES = %w[users person_name drug_ingredients drug_order encounter orders obs patient_program
                             patient_state person_address pharmacies pharmacy_batch_items pharmacy_batches pharmacy_stock_balances pharmacy_stock_verifications relationship
-                            global_property patient person patient_identifier report_object reporting_report_design reporting_report_design_resource user_property user_role
-                            ].freeze
+                            global_property patient person patient_identifier report_object reporting_report_design reporting_report_design_resource user_property user_role].freeze
 
   def up
     ActiveRecord::Base.connection.execute <<~SQL
-      SET sql_mode = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+      SET sql_mode = 'ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
     SQL
 
     TRANSACTIONAL_TABLES.each do |t|
@@ -19,7 +18,6 @@ class AppendSiteIdToTransactionalTables < ActiveRecord::Migration[7.0]
   end
 
   def current_health_center_id
-    Location.current_health_center&.id  
+    Location.current_health_center&.id
   end
 end
-
