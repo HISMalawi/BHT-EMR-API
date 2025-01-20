832a7017526fca3a6a99fd11e0f4bf9e719e1920
Brian M <brianmsyamboza@gmail.com>
2025-01-20 08:57:35 +0200

    add dissagregation

--- app/services/art_service/reports/pepfar/tx_hiv_htn.rb
+++ app/services/art_service/reports/pepfar/tx_hiv_htn.rb
@@ -15,6 +15,7 @@ module ArtService
 
         def initialize(start_date:, end_date:, **kwargs)
           super(start_date:, end_date:, **kwargs)
+          ArtService::Reports::MaternalStatus.new(start_date:, end_date:, **kwargs).process_data
         end
 
         def find_report
@@ -28,24 +29,29 @@ module ArtService
           find_report
         end
 
-        def init_report
-          @report = pepfar_age_groups.each_with_object({}) do |age_group, report|
-            report[age_group] = %w[M F].each_with_object({}) do |gender, gender_sub_report|
-              gender_sub_report[gender] = {
+        def indicators
+          {
                 tx_curr: [],
                 ever_diagnosed_htn: [],
                 screened_for_htn: [],
                 newly_diagnosed_htn: [],
                 controlled_htn: [],
               }
+        end
+
+        def init_report
+          @report = (pepfar_age_groups - ['Unknown']).each_with_object({}) do |age_group, report|
+            report[age_group] = %w[M F].each_with_object({}) do |gender, gender_sub_report|
+              gender_sub_report[gender] = indicators
             end
           end
+          process_aggreggation_rows
         end
 
         # [
-        #   {
-        #     "patient_id": 1256,
-        #     "systolic": 160.0,
+          #   {
+            #     "patient_id": 1256,
+            #     "systolic": 160.0,
         #     "diastolic": 100.0,
         #     "date_screened_for_htn": "2024-12-05",
         #     "diagnosed": 1,
@@ -53,28 +59,61 @@ module ArtService
         #   },
         # ]
         def map_results(patients:)
-          patients.each do |p|
-            id = p['patient_id']
 
-            @report[p['age_group']][p['gender']][:tx_curr] << id
-            @report[p['age_group']][p['gender']][:screened_for_htn] << id
-            @report[p['age_group']][p['gender']][:ever_diagnosed_htn] << id if p['diagnosed'] == 1
-            @report[p['age_group']][p['gender']][:newly_diagnosed_htn] << id if p['diagnosed'] == 1 && p['date_diagnosed'] > start_date
-            @report[p['age_group']][p['gender']][:controlled_htn] << id if p['systolic'] < SYSTOLIC_THRESHOLD\
-             && p['diastolic'] < DIASTOLIC_THRESHOLD
+          patients.each do |p|
+            next if p['age_group'] == 'Unknown'
+
+            id = p['patient_id']
+            diagonised = p["diagonised"]
+            date_diagnosed = p["date_diagnosed"]
+            systolic = p["systolic"]
+            diastolic = p["diastolic"]
+            maternal_status = p["maternal_status"]
+            gender = p["gender"]
+            age_group = p["age_group"]
+
+            @report[age_group][gender][:tx_curr] << id
+            @report["All"][maternal_status][:tx_curr] << id
+
+            @report[age_group][gender][:screened_for_htn] << id
+            @report["All"][maternal_status][:screened_for_htn] << id
+
+            if diagonised == 1
+              @report[age_group][gender][:ever_diagnosed_htn] << id 
+              @report['All'][maternal_status][:ever_diagnosed_htn] << id
+            end
+            if diagonised == 1 && date_diagnosed > start_date
+              @report[age_group][gender][:newly_diagnosed_htn] << id 
+              @report["All"][maternal_status][:newly_diagnosed_htn] << id
+            end
+            if systolic < SYSTOLIC_THRESHOLD && diastolic < DIASTOLIC_THRESHOLD
+              @report[age_group][gender][:controlled_htn] << id 
+              @report["All"][maternal_status][:controlled_htn] << id
+            end
+          end
+
+        end
+
+        def process_aggreggation_rows     
+          @report["All"] = {}
+          @report['All']['Male'] = indicators
+          
+          %w[Male FP FNP FBf].each do |key|
+            @report['All'][key] = indicators
           end
         end
 
         def screened_for_htn
           ActiveRecord::Base.connection.select_all <<~SQL
-                                                                                                                                          SELECT tesd.patient_id,
+                                                                                                                                      SELECT tesd.patient_id,
               disaggregated_age_group(tesd.birthdate, DATE('#{end_date.to_date}')) age_group,
               LEFT(tesd.gender, 1) AS gender,
               systolic.value_numeric AS systolic,
               diastolic.value_numeric AS diastolic,
               DATE(vitals.encounter_datetime) AS date_screened_for_htn,
-              IF (diagnosed.patient_id IS NOT NULL, 1, 0) AS diagnosed,
-              DATE(diagnosed.date_diagonised) AS date_diagnosed
+              IF (diagnosed.patient_id IS NOT NULL, 1, 0) AS diagonised,
+              DATE(diagnosed.date_diagonised) AS date_diagnosed,
+              IF (ms.maternal_status IS NOT NULL, ms.maternal_status, 'Male') AS maternal_status
             FROM temp_earliest_start_date tesd
             INNER JOIN temp_patient_outcomes tpo
               ON tpo.patient_id = tesd.patient_id
@@ -103,6 +142,7 @@ module ArtService
               AND date_diagnosied.voided = 0
               AND date_diagnosied.concept_id = #{concept("Hypertension diagnosis date").id}
             ) diagnosed ON diagnosed.patient_id = tesd.patient_id
+            LEFT JOIN temp_maternal_status ms ON ms.patient_id = tesd.patient_id
             GROUP BY tesd.patient_id
           SQL
         end
