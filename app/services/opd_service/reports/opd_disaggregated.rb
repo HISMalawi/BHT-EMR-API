ae106f15930df648ab075320bbdfe1c40bc21ad7
Daniel Justin <danieljmuleke@gmail.com>
2025-02-24 13:39:47 +0200

    add site_id filter to opd disaggregated report queries

--- app/services/opd_service/reports/opd_disaggregated.rb
+++ app/services/opd_service/reports/opd_disaggregated.rb
@@ -76,6 +76,7 @@ module OpdService
               INNER JOIN encounter reg_date ON reg_date.patient_id = e.patient_id
                   AND reg_date.encounter_type = #{EncounterType.find_by_name('PATIENT REGISTRATION').encounter_type_id}
                   AND reg_date.voided = 0
+                  AND reg_date.site_id = #{Location.current.location_id}
               LEFT JOIN (
                 SELECT e.patient_id,
                        hiv_status.value_text hiv_status,
@@ -85,7 +86,7 @@ module OpdService
                 FROM encounter e
                 INNER JOIN obs hiv_status ON hiv_status.encounter_id = e.encounter_id
                   AND hiv_status.voided = 0
-                  AND hiv_status.concept_id = #{concept('HIV Status').concept_id}
+                  AND hiv_status.site_id = #{Location.current.location_id}
                 INNER JOIN obs tst_date ON tst_date.encounter_id = e.encounter_id
                     AND tst_date.concept_id = #{concept('HIV test date').concept_id}
                     AND tst_date.voided = 0
@@ -96,14 +97,14 @@ module OpdService
                   AND date_started_art.concept_id = #{concept('Date antiretrovirals started').concept_id}
                   AND date_started_art.voided = 0
                   AND e.encounter_datetime >= '#{start_date}' AND e.encounter_datetime <= '#{end_date}'
-              ) AS hiv_obs ON hiv_obs.patient_id = p.patient_id
+              ) AS hiv_obs ON hiv_obs.patient_id = p.patient_id AND hiv_obs.site_id = #{Location.current.location_id}
               LEFT JOIN (
                 SELECT obs.person_id#{' '}
                 FROM obs
                   WHERE obs.voided = 0
                   AND obs.concept_id = #{concept('Routine TB Screening').concept_id}
                   AND obs.obs_datetime >= '#{start_date}' AND obs.obs_datetime <= '#{end_date}'
-              ) AS screened ON screened.person_id = e.patient_id
+              ) AS screened ON screened.person_id = e.patient_id AND screened.site_id = #{Location.current.location_id}
               LEFT JOIN (
                 SELECT brest.value_coded as breastfeeding,
                        brest.person_id,
@@ -116,8 +117,8 @@ module OpdService
                 AND brest.voided = 0
                 AND preg.voided = 0
                 AND brest.obs_datetime >= '#{start_date}' AND brest.obs_datetime <= '#{end_date}'
-              ) AS preg_status ON preg_status.person_id = p.patient_id
-            WHERE e.program_id = #{Program.find_by_name('OPD program').program_id}
+              ) AS preg_status ON preg_status.person_id = p.patient_id AND preg_status.site_id = #{Location.current.location_id}
+            WHERE e.program_id = #{Program.find_by_name('OPD program').program_id} AND e.site_id = #{Location.current.location_id}
             AND reg_date.encounter_datetime >= '#{start_date}' AND reg_date.encounter_datetime <= '#{end_date}'
             AND e.voided = 0
             GROUP BY e.patient_id
