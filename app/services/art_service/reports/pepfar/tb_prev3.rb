78608a4bc074cf7da7296723fb31f4fd2499e6df
brian@xps <brianmsyamboza@gmail.com>
2025-04-17 15:29:11 +0200

    tb prev 3 fix

--- app/services/art_service/reports/pepfar/tb_prev3.rb
+++ app/services/art_service/reports/pepfar/tb_prev3.rb
@@ -14,8 +14,8 @@ module ArtService
 
         def initialize(start_date:, end_date:, **kwargs)
           @start_date = ActiveRecord::Base.connection.quote(start_date)
-          @check_date = start_date.to_date - 6.months
-          @cut_off_point = start_date.to_date
+          @check_date = start_date&.to_date - 6.months
+          @cut_off_point = start_date&.to_date
           @end_date = ActiveRecord::Base.connection.quote(end_date)
           @occupation = kwargs[:occupation]
           @dsd = kwargs[:dsd]
@@ -91,8 +91,8 @@ module ArtService
         end
 
         def patient_new_on_art?(patient)
-          tpt_initiation_date = patient['tpt_initiation_date'].to_date
-          art_start_date = patient['art_start_date'].to_date
+          tpt_initiation_date = patient['tpt_initiation_date']&.to_date
+          art_start_date = patient['art_start_date']&.to_date
 
           (tpt_initiation_date >= art_start_date) && (tpt_initiation_date < art_start_date + 180.days)
         end
@@ -101,11 +101,11 @@ module ArtService
           clients = fetch_patients_on_tpt.to_a
           results = []
           clients.each do |client|
-            next if client['tpt_initiation_date'].to_date > cut_off_point
+            next if client['tpt_initiation_date']&.to_date > cut_off_point
 
             result = individual_tpt_report(client['patient_id'])
             next if result.blank?
-            next if result['tpt_initiation_date'].to_date < check_date
+            next if result['tpt_initiation_date']&.to_date < check_date
 
             client['tpt_initiation_date'] = result['tpt_initiation_date']
             client['total_pills_taken'] = result['total_pills_taken']
@@ -256,7 +256,7 @@ module ArtService
           result = client_tpt_dates(patient_id)
           return { start_date: '1900-01-01', end_date: } if result.blank?
 
-          sorted_result = result.sort { |a, b| a['start_date'].to_date <=> b['start_date'].to_date }.reverse
+          sorted_result = result.sort { |a, b| a['start_date']&.to_date <=> b['start_date']&.to_date }.reverse
           return_date = { start_date: sorted_result.last['start_date'], end_date: }
 
           course_interruption = result.first['course'] == '3HP' ? 1 : 2
@@ -368,7 +368,7 @@ module ArtService
 
           return end_date if result.blank?
 
-          sorted_result = result.sort { |a, b| a['start_date'].to_date <=> b['start_date'].to_date }
+          sorted_result = result.sort { |a, b| a['start_date']&.to_date <=> b['start_date']&.to_date }
           return_date = sorted_result.last['end_date']
           course_interruption = result.first['course'] == '3HP' ? 1 : 2
           # use a for loop to check if there is a course interruption
