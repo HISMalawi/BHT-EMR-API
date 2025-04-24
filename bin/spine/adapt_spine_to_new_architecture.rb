53815458e2aee83bbbb75d3263ae5cff55051690
fchiyenda <fchiyenda>
2025-04-24 14:57:45 +0200

    Added missing columns in concept_name

--- bin/spine/adapt_spine_to_new_architecture.rb
+++ bin/spine/adapt_spine_to_new_architecture.rb
@@ -1,5 +1,16 @@
 # Create missing tables
 
+# Add Missing columns to concept_name
+connection = ActiveRecord::Base.connection
+
+unless connection.column_exists?(:concept_name, :concept_name_type)
+  connection.add_column :concept_name, :concept_name_type, :string, limit: 50
+end
+
+unless connection.column_exists?(:concept_name, :locale_preferred)
+  connection.add_column :concept_name, :locale_preferred, :string, limit: 4
+end
+
 sql_files = %i[bypass_migrations moh_regimens_v2021 spine_adaptaion spine_concepts_adaptation]
 
 sql_files.each do |file|
