74a70ccb35469fae1fd155e6146d8c2a9daa69d7
Hopson Gausi <hopgausi20@gmail.com>
2025-05-05 15:19:24 +0200

    Refactor user program initialization script to update user location and role before adding programs

--- bin/add_user_programs.rb
+++ bin/add_user_programs.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-puts 'Adding user programs...'
+puts 'Updating user location...'
 User.find_by(username: 'admin').update(location_id: 'LL040010')
+puts 'User location updated.'
+puts 'Updating user role...'
 UserRole.find_by(user_id: 1).update(role: Role.find_by(role: 'Superuser,Superuser,'))
+puts 'User role updated.'
+puts 'Adding user programs...'
 Program.all.each do |p|
   puts "Adding user program for #{p.name}"
   UserProgram.find_or_create_by!(user_id: 1, program_id: p.id)
 end
+puts 'User programs added.'
