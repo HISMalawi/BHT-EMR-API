9d197d354f8c1ff6a0a08ac89611a3e210aa89df
brian@xps <brianmsyamboza@gmail.com>
2025-07-03 09:53:34 +0200

    update scripts to use primary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = false
 
+
+  config.active_support.to_time_preserves_timezone = :zone
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
