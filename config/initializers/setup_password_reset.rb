1720bd6b4c967258d121d3eb7a6695bf0d7182b0
brian@xps <brianmsyamboza@gmail.com>
2025-07-28 08:39:27 +0200

    remove initializer causing indentation issues in application yml

--- config/initializers/setup_password_reset.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-if File.exist?('config/application.yml') &&
-  File.read('config/application.yml').include?('password_reset')
-  return
-end
-
-cmd = 'echo -e "\npassword_reset:\n  secret_key: CENTRALISED-EMR" >> config/application.yml'
-system(cmd) || return
