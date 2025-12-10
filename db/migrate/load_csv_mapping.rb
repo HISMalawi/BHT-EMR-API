3e87485a5d7ce6fd42d2ac2e14094e47f1b566e4
fchiyenda <fchiyenda>
2025-12-10 13:55:09 +0200

    Index Tempory table for performance

--- db/migrate/load_csv_mapping.rb
+++ db/migrate/load_csv_mapping.rb
@@ -25,8 +25,17 @@ def load_csv_mapping
         location_district VARCHAR(255),
         facility_code VARCHAR(255),
         facility_name VARCHAR(255)
-    );
+        )
+        ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
   SQL
+
+  # Add index on facility_code if it does not exist
+  indexes = ActiveRecord::Base.connection.indexes('temp_facility_x_location_map').map(&:columns)
+  unless indexes.include?(['facility_code'])
+    ActiveRecord::Base.connection.execute <<~SQL
+      CREATE INDEX idx_facility_code ON temp_facility_x_location_map(facility_code);
+    SQL
+  end
 
   # Read CSV and insert mappings
   mappings = []
@@ -39,7 +48,8 @@ def load_csv_mapping
 
     next if location_id.nil? || location_id.zero? || facility_code.nil? || facility_code.empty?
 
-    mappings << { facility_code: facility_code, location_id: location_id, location_name: location_name, location_district: location_district, facility_name: facility_name }
+    mappings << { facility_code: facility_code, location_id: location_id, location_name: location_name,
+                  location_district: location_district, facility_name: facility_name }
   end
 
   puts "Found #{mappings.size} mappings in CSV file"
@@ -51,13 +61,15 @@ def load_csv_mapping
   # Insert in batches
   batch_size = 1000
   unique_mappings.each_slice(batch_size) do |batch|
-    values = batch.map { |m| "(#{ActiveRecord::Base.connection.quote(m[:facility_code])}, #{m[:location_id]}, #{ActiveRecord::Base.connection.quote(m[:location_name])}, #{ActiveRecord::Base.connection.quote(m[:location_district])}, #{ActiveRecord::Base.connection.quote(m[:facility_name])})" }.join(',')
-    
+    values = batch.map do |m|
+      "(#{ActiveRecord::Base.connection.quote(m[:facility_code])}, #{m[:location_id]}, #{ActiveRecord::Base.connection.quote(m[:location_name])}, #{ActiveRecord::Base.connection.quote(m[:location_district])}, #{ActiveRecord::Base.connection.quote(m[:facility_name])})"
+    end.join(',')
+
     ActiveRecord::Base.connection.execute <<~SQL
       INSERT INTO temp_facility_x_location_map (facility_code, location_id, location_name, location_district, facility_name)
       VALUES #{values}
       ON DUPLICATE KEY UPDATE location_id = VALUES(location_id);
-      
+
     SQL
   end
 
@@ -68,10 +80,10 @@ end
 if __FILE__ == $PROGRAM_NAME
   begin
     load_csv_mapping
-    
+
     # Optionally export verification file
     # export_mapping_verification
-    
+
     puts "\nMapping loaded successfully!"
     puts "The temporary table 'temp_facility_x_location_map' is now available for use in migrations."
   rescue StandardError => e
@@ -80,4 +92,3 @@ if __FILE__ == $PROGRAM_NAME
     exit 1
   end
 end
-
