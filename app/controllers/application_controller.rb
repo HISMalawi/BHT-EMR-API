c3338bc0b31844cf498a7d6c00a276b08a8a63c5
brian@xps <brianmsyamboza@gmail.com>
2026-01-22 15:32:40 +0200

    update version lock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < ActionController::API
 
   # Map of clients to their allowed versions
   CLIENT_VERSION_CONFIGURATION = {
-    'EMASTERCARD' => 'v2025.Q4.R1',
-    'POC' => 'v2025.Q4.R1'
+    'EMASTERCARD' => 'v2025.Q4.R2',
+    'POC' => 'v2025.Q4.R2'
   }
 
   # Required by audited gem
