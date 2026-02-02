d4316f307c34923af7d459fe8a3dc4581fbf8fe2
brian@xps <brianmsyamboza@gmail.com>
2026-02-02 10:16:56 +0200

    fix deactivation: exclude lab daemon and superuser

--- bin/deactivate_inactive_users.rb
+++ bin/deactivate_inactive_users.rb
@@ -6,14 +6,17 @@ LOGGER = Logger.new(File.join(Rails.root, 'log', 'cron.log'))
 ActiveRecord::Base.logger = LOGGER
 
 def main
+  superuser = User.joins(:user_roles).where(user_roles: { role: 'Superuser'} ).order(date_changed: :desc).first
+  labdaemon = User.find_by(username: 'lab_daemon')
+
   # get the password expiry days
   password_expiry_days = GlobalProperty.find_by_property('password_reset_interval')&.property_value&.to_i || 90
   LOGGER.info("Deactivating users with no activity in the last #{password_expiry_days} days")
-  User.where(date_changed: ..password_expiry_days.days.ago).update_all(deactivated_on: Time.now)
 
-  # get the latest active superuser and ensure they are not deactivated
-  superuser = User.joins(:user_roles).where(user_roles: { role: 'Superuser'} ).order(date_changed: :desc).first
-  LOGGER.info("Reactivating superuser: #{superuser.username}")
+  User.where(date_changed: ..password_expiry_days.days.ago)
+      .where.not(id: [superuser.id, labdaemon.id])
+      .update_all(deactivated_on: Time.now)
+
   superuser.update!(deactivated_on: nil)
 end
 
