147fdf341ad69b2773713393566c38de22debb67
Andrew Mfune <andiemfune@gmail.com>
2026-02-02 14:56:26 +0200

    update client version for POC to v2025.Q4.R3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::API
   # Map of clients to their allowed versions
   CLIENT_VERSION_CONFIGURATION = {
     'EMASTERCARD' => 'v2025.Q4.R2',
-    'POC' => 'v2025.Q4.R2'
+    'POC' => 'v2025.Q4.R3'
   }
 
   # Required by audited gem
