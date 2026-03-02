a74d17070b0f3faaa4cce12eb5132e7ef0e2ab93
Hopson Gausi <hopgausi20@gmail.com>
2026-03-02 13:59:12 +0200

    Fix: Update order status retrieval to include fallback for lab order status

--- app/services/art_service/reports/lims_results.rb
+++ app/services/art_service/reports/lims_results.rb
@@ -24,7 +24,7 @@ module ArtService
         ActiveRecord::Base.connection.select_all <<~SQL
           SELECT o.start_date AS date_ordered, pn.given_name, pn.family_name, pi.identifier AS arv_number, e.patient_id,
           las.date_received, o.accession_number, CONCAT(COALESCE(res.value_modifier, '='), COALESCE(res.value_text, res.value_numeric)) AS result,
-          cn.name AS test_name, las.acknowledgement_type AS result_delivery_mode, latest_test_status.value_text AS order_status, reason_test.name AS test_reason
+          cn.name AS test_name, las.acknowledgement_type AS result_delivery_mode, COALESCE(latest_test_status.value_text, statuses.value_text) AS order_status, reason_test.name AS test_reason
           FROM orders o
           LEFT JOIN lims_acknowledgement_statuses las ON las.order_id = o.order_id
           INNER JOIN encounter e ON e.encounter_id = o.encounter_id AND e.voided = 0 AND (e.program_id = 1 OR e.program_id = 23) -- HIV PROGRAM AND Laboratory program
@@ -36,6 +36,7 @@ module ArtService
           LEFT JOIN patient_identifier pi ON pi.patient_id = e.patient_id AND pi.voided = 0 AND pi.identifier_type = #{identifier_type}
           LEFT JOIN obs ON obs.person_id = e.patient_id AND obs.voided = 0 AND obs.order_id = o.order_id
             AND obs.concept_id = 7363 -- 'Lab test result'
+          LEFT JOIN obs statuses ON statuses.order_id = o.order_id AND statuses.voided = 0 AND statuses.concept_id = 10682 -- 'lab order status'
           LEFT JOIN obs res ON res.obs_group_id = obs.obs_id AND res.voided = 0 AND res.order_id = o.order_id
           LEFT JOIN obs reason ON reason.order_id = o.order_id AND reason.voided = 0  AND reason.concept_id = 2429 -- 'Reason for test'
           LEFT JOIN concept_name reason_test ON reason_test.concept_id = reason.value_coded AND reason_test.voided = 0
