a68f911468d962c43c942ff00abb52fc062677c3
gonjetso <gonjetsoc@gmail.com>
2026-06-14 00:28:44 +0200

    Add SQL mode setting in migration for defaulter risk score

--- db/migrate/20260403172741_add_defaulter_risk_score_to_patients.rb
+++ db/migrate/20260403172741_add_defaulter_risk_score_to_patients.rb
@@ -1,5 +1,6 @@
 class AddDefaulterRiskScoreToPatients < ActiveRecord::Migration[7.0]
   def change
+    execute "SET sql_mode = ''"
     add_column :patient, :defaulter_risk_score, :float
     add_column :patient, :risk_assessed_at, :datetime
   end
